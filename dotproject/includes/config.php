356843d802b4598bbfc741d1485e8a1d809baeed
ajdonnison <ajdonnison>
2002-07-05 10:45:57 +0000

    New config items to support date format control, admin email address, and to centralise site-specific details.

--- dotproject/includes/config.php
+++ dotproject/includes/config.php
@@ -7,14 +7,23 @@ $db = "demodotproject";
 $dbuser = "demodotproject";
 $dbpass = "yourpassword";
 
-
 //localization file.  Needs to be fleshed out
 $language_file = "english.php";
 
-
+// Web site specific information
 $root_dir = "/wwwroot/demo.dotmarketing.org";				//filesystem web root
 $company_name = "dotmarketing, Inc.";		
 $page_title ="dotproject <dotmarketing inc>";
+$base_url = "http://www.dotmarketing.org/dotproject";
+
+// Admin email used in From address of Task update notifications
+$admin_email = "admin@yourdomain.com";
+
+// Date format, can be one of:
+// 0 = international or yyyy-mm-dd
+// 1 = UK and Australia or dd/mm/yyyy
+// 2 = US or mm/dd/yyyy
+$date_format = 0;
 
 
 //File parsers to return indexing information about uploaded files
