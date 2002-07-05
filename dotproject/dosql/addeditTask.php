55fa8006f152ce204b597c6c648c2d3994218db4
ajdonnison <ajdonnison>
2002-07-05 10:33:54 +0000

    Edit task no longer returns spurious database error message. Initial assignments of users now works. Blank screen now fixed. Email is now sent to all assignees, except the editor, for updates, adds, deletes and edits.

--- dotproject/dosql/addeditTask.php
+++ dotproject/dosql/addeditTask.php
@@ -7,7 +7,9 @@ if(empty($task_status ))$task_status =0;
 if(empty($task_order ))$task_order =0;
 if(empty($task_client_publish))$task_client_publish =0;
 $doassingsql  = 0;
+$doassignemail = 0;
 $message = "";
+$mail_type = "";
 
 
 if(empty($task_milestone))$task_milestone = 0;
@@ -21,10 +23,13 @@ if($del){
 	$delsql2 = "delete from user_tasks where task_id = $task_id";
 	mysql_query($delsql);
 	$message = "Task Deleted";
+	$mail_type = "Deleted";
 }
 else if($task_id ==0 && isset($task_name)){
 //insert a new task
 	$task_duration = ($duration * $dayhour);
+	$task_start_date = toDate($task_start_date);
+	$tast_end_date = toDate($task_end_date);
 	
 	
 	
@@ -34,19 +39,27 @@ else if($task_id ==0 && isset($task_name)){
 	('$task_name','$task_parent','$task_milestone','$task_project','$task_start_date','$task_end_date','$task_duration','$task_status ','$task_priority ','$task_precent_complete','$task_description','$task_target_budget ','$task_related_url','$task_order ','$task_client_publish', '$task_owner')";
 	//echo $tsql;
 	mysql_query($tsql);
+	if (mysql_error())
+		$sql = $tsql;
 	//$message.= mysql_error() ."<BR>";
 	$id = mysql_insert_id();
 	if($task_parent == 0){
 		$tpsql = "update tasks set task_parent = " . $id . " where task_id = " . $id;
 		mysql_query($tpsql);
+		if (mysql_error())
+			$sql = $tpsql;
 		//$message.= mysql_error() ."<BR>";
 	}
 	$tosql  ="insert into user_tasks (user_id, task_id, user_type) values ($user_cookie, $id, -1)";
 	mysql_query($tosql);
+	if (mysql_error())
+		$sql = $tosql;
 	//$message.= mysql_error() ."<BR>";
 
+	$task_id = $id;
 	$doassingsql = 1;	
 	$doassignemail = 1;
+	$mail_type = "Added";
 
 } // Update existing task
 else if($task_id > 0){
@@ -101,6 +114,8 @@ else if($task_id > 0){
 	}
 	
 	$task_duration = ($duration * $dayhour);
+	$task_start_date = toDate($task_start_date);
+	$task_end_date = toDate($task_end_date);
 	$tsql = "
 	update tasks set
 	task_name='$task_name',
@@ -122,24 +137,109 @@ else if($task_id > 0){
 	task_owner = '$task_owner'
 	where task_id = $task_id";
 	mysql_query($tsql);
+	if (mysql_error())
+		$sql = $tsql;
 	//$message.= mysql_error() ."<BR>";
 
 	$doassingsql = 1;	
 	$doassignemail = 1;
-
-
+	$mail_type = "Edited";
 }
 
 if($doassingsql){
 	$cleansql = "delete from user_tasks where task_id = " .$task_id . " and user_type = 0";
 	mysql_query($cleansql);
+	if (mysql_error())
+		$sql = $cleansql;
 	//$message.= mysql_error() ."<BR>";
 	$assigees = explode(",", $hassign);
 	for($x = 0; $x < count($assigees); $x++){
 		if(intval($assigees[$x]) > 0){
-			$asql = "insert into user_tasks (user_id, task_id) values ($assigees[$x], $task_id)";
+			$asql = "replace user_tasks (user_id, task_id) values ($assigees[$x], $task_id)";
 			mysql_query($asql);
+			if (mysql_error())
+				$sql = $asql;
 				//$message.= mysql_error() ."<BR>";
+		}
+	}
+}
+
+if ($doassignemail) {
+	$csql = "select user_email, user_first_name, user_last_name
+	from users where users.user_id = $user_cookie";
+	$query = mysql_query($csql);
+	if (mysql_error())
+		$sql = $csql;
+	$editor = mysql_fetch_array($query);
+
+	$usql = "select
+	tasks.task_id,
+	task_name,
+	task_description,
+	creator.user_email as creator_email,
+	creator.user_first_name as creator_first_name,
+	creator.user_last_name as creator_last_name,
+	owner.user_email as owner_email,
+	owner.user_first_name as owner_first_name,
+	owner.user_last_name as owner_last_name,
+	assignee.user_id as assignee_id,
+	assignee.user_email as assignee_email,
+	assignee.user_first_name as assignee_first_name,
+	assignee.user_last_name as assignee_last_name
+		from tasks
+		left join user_tasks on user_tasks.task_id = tasks.task_id
+		left join users creator on creator.user_id = tasks.task_owner
+		left join users owner on owner.user_id = tasks.task_creator
+		left join users assignee on assignee.user_id = user_tasks.user_id
+	where tasks.task_id = $task_id";
+
+	$query = mysql_query($usql);
+	if (mysql_error())
+		$sql = $usql;
+	// For each user, email them an update, similar to the update that
+	// ticketsmith uses.
+	$row_count = mysql_num_rows($query);
+	$mail_header = "From: " . $admin_email . "\r\n"
+	. "Content-Type: text/html\r\n"
+	. "Content-Transfer-Encoding: 8bit\r\n"
+	. "Mime-Version: 1.0\r\n"
+	. "X-Mailer: Dotproject";
+	$subject = "Task $task_id $mail_type";
+	$mail_body = "<head><title>$subject</title></head>\n"
+	. "<body>\n"
+	. "<table bgcolor='#ffffff' cellpadding=4 cellspacing=1>\n"
+	. "<tr bgcolor='#eeeeee'><th colspan=2>$subject</th></tr>\n"
+	. "<tr><td>Task ID</td><td><a href='"
+	. $base_url
+	. "/index.php?m=tasks&a=view&task_id=$task_id'>$task_id</a></td></tr>\n";
+	for ($i = 0; $i < $row_count; $i++) {
+		$row = mysql_fetch_array($query);
+		if ($row['assignee_id'] != $user_cookie) {
+			$mail_text = $mail_body
+			. "<tr><td>Title</td><td>"
+			. $row['task_name']
+			. "&nbsp;</tr>\n<tr><td>Description</td><td>"
+			. str_replace(chr(10), "<BR>", $row['task_description'])
+			. "&nbsp;</td></tr>\n<tr><td>Created by</td><td><a href='mailto:"
+			. $row['creator_email']
+			. "'>"
+			. $row['creator_first_name']
+			. "&nbsp;"
+			. $row['creator_last_name' ]
+			. "</a></tr>\n<tr><td>Owned by</td><td><a href='mailto:"
+			. $row['owner_email']
+			. "'>"
+			. $row['owner_first_name']
+			. "&nbsp;"
+			. $row['owner_last_name']
+			. "</a></tr>\n<tr><td>$mail_type by</td><td><a href='mailto:"
+			. $editor['user_email']
+			. "'>"
+			. $editor['user_first_name']
+			. "&nbsp;"
+			. $editor['user_last_name']
+			. "</a></tr>\n</table></body>\n";
+			mail($row['assignee_email'], $subject, $mail_text, $mail_header);
 		}
 	}
 }
