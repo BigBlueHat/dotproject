e05c2e65ef37738c162bd4d45f1703ac048a59cb
eddieajau <eddieajau>
2002-08-07 23:44:01 +0000

    Added link to new view user page

--- dotproject/modules/admin/index.php
+++ dotproject/modules/admin/index.php
@@ -1,46 +1,50 @@
-<?php 
+<?php
 //User Managagement
 
 //set defaults
-if(empty($message))$message = "";
-if(empty($orderby))$orderby = "user_username";
+$message = isset($HTTP_GET_VARS['message']) ? $HTTP_GET_VARS['message'] : '';
+$orderby = isset($HTTP_GET_VARS['orderby']) ? $HTTP_GET_VARS['orderby'] : 'user_username';
 
+// check permissions to read
+$psql = "
+select count(*)
+from permissions
+where permission_user = $user_cookie and
+	permission_value <> 0
+	and (permission_grant_on = 'all'
+		or (permission_grant_on = 'admin' and (permission_item = -1))
+	)
+";
 
-
-$usql = "
-select  distinct(user_id), user_username, user_last_name, user_first_name, permission_user, user_email
-from users
-left join permissions on users.user_id = permissions.permission_user and permission_value <> 0 
-order by " . $orderby;
-$urow = mysql_query($usql);
-
-
-
-?>
-
-
-
-<SCRIPT language="javascript">
-
-
-
-
-
-
-function delMe(x, y){
-if(confirm("Are you sure you want\nto delete user/group " + y + "?"))
-	{
-	top.location="./index.php?m=admin&a=dosql&del=1&user_id=" + x;
-	} 
-
+$prc = mysql_query($psql);
+$perm = mysql_fetch_array($prc);
+if ($perm[0] == 0) {
+	echo '<script language="javascript">
+	window.location="./index.php?m=admin&a=viewuser&user_id='.$user_cookie.'&message=ACCESS DENIED: You have insufficient permissions to view the administration module.";
+	</script>
+';
 }
 
+$usql = "
+select distinct(user_id), user_username, user_last_name, user_first_name, permission_user, user_email
+from users
+left join permissions on users.user_id = permissions.permission_user and permission_value <> 0
+order by " . $orderby;
+$urow = mysql_query($usql);
+?>
+
+<SCRIPT language="javascript">
+function delMe( x, y ) {
+	if (confirm( "Are you sure you want\nto delete user " + y + "?" )) {
+		top.location="./index.php?m=admin&a=dosql&del=1&user_id=" + x;
+	}
+}
 </script>
 
 <TABLE width="95%" border=0 cellpadding="0" cellspacing=1>
 	<TR>
 	<TD valign="top"><img src="./images/icons/admin.gif" alt="" border="0" width=42 height=42></td>
-		<TD nowrap><span class="title">User/Group Management</span></td>
+		<TD nowrap><span class="title">User Management</span></td>
 		<TD valign="top" align="right" width="100%"><!-- <input type="button" class=button value="add group" onClick="javascript:window.location='./index.php?m=admin&a=addeditgroup';">--><input type="button" class=button value="add user" onClick="javascript:window.location='./index.php?m=admin&a=addedituser';"></td>
 	</tr>
 </TABLE>
@@ -61,7 +65,7 @@ if(confirm("Are you sure you want\nto delete user/group " + y + "?"))
 				<?php while($row = mysql_fetch_array($urow)){?>
 					<?php if(intval(@$row["permission_user"]) !=0){echo "<TR bgcolor=#f4efe3>";} else { echo "<TR bgcolor=#eeeeee>";}?>
 					<TD width="60" class="smallNorm" align="right"><A href="./index.php?m=admin&a=addedituser&user_id=<?php echo $row["user_id"];?>"><span  class="smallNorm">edit</a> |</span> <A href="javascript:delMe(<?php echo $row["user_id"];?>, '<?php echo $row["user_first_name"] . " " . $row["user_last_name"];?>')"><span  class="smallNorm">del</span></a>&nbsp; </td>
-					<TD><?php echo $row["user_username"];?></td>
+					<TD><A href="./index.php?m=admin&a=viewuser&user_id=<?php echo $row["user_id"];?>"><?php echo $row["user_username"];?></a></td>
 					<TD><?php echo $row["user_last_name"];?>, <?php echo $row["user_first_name"];?></td>
 					<TD><A href="mailto:<?php echo $row["user_email"];?>"><?php echo $row["user_email"];?></a></td>
 					<TD><?php if(intval(@$row["permission_user"]) !=0){echo "Yes";} else { echo "No";}?> </td>
