e1c2d0e427ea6c59ad82cb1c4621ff5c12f5b270
eddieajau <eddieajau>
2002-08-07 23:36:42 +0000

    Added cookie for user full name

--- dotproject/logincheck.php
+++ dotproject/logincheck.php
@@ -3,7 +3,7 @@ include "./includes/config.php";
 include "./includes/db_connect.php";
 
 //Check Login
-$psql = "select user_id
+$psql = "select user_id, user_first_name, user_last_name
 					from users,  permissions
 					where user_username = '$username'
 					and user_password = password('$password') 
@@ -13,17 +13,14 @@ $psql = "select user_id
 $prc = mysql_query($psql);
 
 //Pull record, write bad login if exists
-if(!$row = mysql_fetch_array($prc))
-	{
+if (!$row = mysql_fetch_array($prc)) {
 	$message  = "Login Failed.";
 	include "./includes/login.php";
 	die;
-	}
+}
 
-
-setcookie("user_cookie", $row[0]);
-
-
+setcookie( "user_cookie", $row[0] );
+setcookie( "user_full_name", $row[1].' '.$row[2] );
 ?>
 <script language="JavaScript">
 window.location = "./index.php"
