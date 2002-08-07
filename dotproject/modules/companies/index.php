16920555e8ab099bd634702254466acf0b31d75e
eddieajau <eddieajau>
2002-08-07 23:48:40 +0000

    Fixed project counts

--- dotproject/modules/companies/index.php
+++ dotproject/modules/companies/index.php
@@ -1,38 +1,21 @@
-<?php 
+<?php
 //Companies
 
-//First pull perms
-$psql = "select permission_value, permission_item 
-	from 
-		permissions, users 
-	where 
-		permission_user = user_id and 
-		permission_value <> 0 and
-		(permission_grant_on = 'companies' or permission_grant_on = 'all')
-	order by permission_value";
-
-$perm =mysql_query($psql);
-$pullperm = mysql_fetch_array($perm, MYSQL_NUM);
-$pullperm[0]=1; // JBF - force use of else clause below
-if($pullperm[0] < 0)
-	{
-	$csql = "select company_id, company_name, count(project_id)  as countp, user_first_name, user_last_name 
-		from companies 
-		left join projects on companies.company_id = projects.project_company
-		left join users on companies.company_owner = users.user_id 
-		group by company_id
-		order by company_name";
-	}
-else
-	{
-	$csql = "select company_id, company_name, count(projects.project_id) as countp, count(projects2.project_id) as inactive, user_first_name, user_last_name
-	from permissions, companies
-	left join projects on companies.company_id = projects.project_company and projects.project_active <> 0
-	left join users on companies.company_owner = users.user_id 
-	left join projects as projects2 on companies.company_id = projects2.project_company and projects2.project_active = 0
-	where (permission_value <> 0 and (permission_grant_on = 'all' or (permission_grant_on = 'companies' and permission_item = company_id)) and (permission_user = $user_cookie))
-	group by company_id order by company_name";
-	}
+$csql = "
+select company_id, company_name,
+	count(distinct projects.project_id) as countp, count(distinct projects2.project_id) as inactive,
+	user_first_name, user_last_name
+from permissions, companies
+left join projects on companies.company_id = projects.project_company and projects.project_active <> 0
+left join users on companies.company_owner = users.user_id
+left join projects as projects2 on companies.company_id = projects2.project_company and projects2.project_active = 0
+where
+	permission_user = $user_cookie
+	and permission_value <> 0 
+	and (permission_grant_on = 'all' or permission_grant_on = 'companies' or permission_item = company_id)
+group by company_id
+order by company_name
+";
 $cos =mysql_query($csql);
 
 $usql = "Select user_first_name, user_last_name from users where user_id = $user_cookie";
@@ -40,22 +23,12 @@ $urc = mysql_query($usql);
 $urow = mysql_fetch_array($urc);
 ?>
 
-<SCRIPT language="javascript">
-function delMe(x, y){
-if(confirm("Are you sure you want\nto delete user " + y + "?"))
-	{
-	parent.mainframe.location="./users.cfm?a=del&user_id=" + x;
-	} 
-
-}
-
-</script>
 <img src="images/shim.gif" width="1" height="5" alt="" border="0"><br>
 <TABLE width="95%" border=0 cellpadding="0" cellspacing=1>
 	<TR>
 	<TD><img src="./images/icons/money.gif" alt="" border="0"></td>
 		<TD nowrap><span class="title">Clients and Companies</span></td>
-		<TD align="right" width="100%"><input type="button" class=button value="add new company" onClick="javascript:window.location='./index.php?m=companies&a=addedit';"></td>
+		<TD align="right" width="100%"><input type="button" class=button value="new company" onClick="javascript:window.location='./index.php?m=companies&a=addedit';"></td>
 	</tr>
 </TABLE>
 <TABLE width="95%" border=0 cellpadding="0" cellspacing=1>
@@ -72,21 +45,15 @@ if(confirm("Are you sure you want\nto delete user " + y + "?"))
 					<TD class="mboxhdr"><A href="#"><font color="white">Company Name</font></a></td>
 					<TD class="mboxhdr"><A href="#"><font color="white">Active Projects</font></a></td>
 					<TD class="mboxhdr"><A href="#"><font color="white">Archived Projects</font></a></td>
-
 				</tr>
-				
-				
-				
-<?php  while($row = mysql_fetch_array($cos)){?>
+<?php while($row = mysql_fetch_array($cos)){?>
 				<TR>
-				
-				
 					<TD width="60" class="smallNorm" align="right" valign="bottom">&nbsp; </td>
-					<TD><A href="./index.php?m=companies&a=addedit&company_id=<?php echo $row["company_id"];?>"><?php echo $row["company_name"];?></A></td>
+					<TD><A href="./index.php?m=companies&a=view&company_id=<?php echo $row["company_id"];?>"><?php echo $row["company_name"];?></A></td>
 					<TD><?php echo $row["countp"];?></td>
 					<TD><?php echo @$row["inactive"];?></td>
 				</tr>
-	<?php }?>
+<?php }?>
 			</TABLE>
 		</td>
 	</tr>
