56bdcf20be59f326bdef95056fefe840456500f0
eddieajau <eddieajau>
2002-08-07 23:42:59 +0000

    Display current user, added about link

--- dotproject/includes/header.php
+++ dotproject/includes/header.php
@@ -3,57 +3,44 @@
 <html>
 <head>
 <script language="JavaScript">
-function doBtn(){
+function doBtn() {
 	var oEl = event.srcElement;
 	var doit = event.type;
 	
-	while( -1 == oEl.className.indexOf( "Btn" ) )
-	{
+	while (-1 == oEl.className.indexOf( "Btn" )) {
 		oEl = oEl.parentElement;
-		if( !oEl ) return;
+		if (!oEl)
+			return;
 	}
-	
-		if(doit == "mouseover" || doit == "mouseup")
-	{
-		oEl.className="clsBtnOn";
-	}	
-	else if(doit == "mousedown")
-	{
-		oEl.className="clsBtnDown";
-	}
-	else
-	{
-	oEl.className="clsBtnOff";
+	if (doit == "mouseover" || doit == "mouseup") {
+		oEl.className = "clsBtnOn";
+	} else if (doit == "mousedown") {
+		oEl.className = "clsBtnDown";
+	} else {
+		oEl.className = "clsBtnOff";
 	}
 }
-
 function tboff(){
 	var oEl = event.srcElement;
 	var doit = event.type;
-	oEl.className="topBtnOff";
+	oEl.className = "topBtnOff";
 }
-
-
 </script>
-
-
-
-
-
-
 <title><?php echo $page_title;?></title>
-<link rel="STYLESHEET" type="text/css" href="./style/main.css">
+<link rel="stylesheet" type="text/css" href="./style/main.css">
 </head>
 <body bgcolor="#ffffff" topmargin="0" leftmargin="0" marginheight=0 marginwidth=0 background="images/bground.gif">
 <TABLE width="100%" cellpadding=3 cellspacing=0 bgcolor="#cccccc" style="border: outset #eeeeee 2px;">
 	<TR>
-		<TD nowrap width="100%"><span id="smallCompanyTitle"><?php echo $company_name;?></SPAN></TD>
-		<TD align="right">
-			<TABLE cellpadding=1 cellspacing=1 bgcolor="#bbbbbb" width="200">
+		<TD nowrap width="33%"><span id="smallCompanyTitle"><?php echo $company_name;?></SPAN></TD>
+		<TD nowrap width="34%"><span id="smallCompanyTitle">Current user: <?php echo "$user_full_name"; ?></SPAN></TD>
+		<TD nowrap width="33%" align="right">
+			<TABLE cellpadding=1 cellspacing=1 width="200">
 				<TR>
-					<TD class="topBtnOff" nowrap bgcolor="#cccccc" align="center"  onmouseover="doBtn();" onmouseout="tboff();" onmousedown="doBtn();" onmouseup="doBtn();"><A href="./index.php?m=admin&a=addedituser&user_id=<?php echo $user_cookie;?>" onmouseover="doBtn();"><?php echo ptranslate("My Info");?></a></TD>
+					<TD class="topBtnOff" nowrap bgcolor="#cccccc" align="center"  onmouseover="doBtn();" onmouseout="tboff();" onmousedown="doBtn();" onmouseup="doBtn();"><A href="./index.php?m=admin&a=viewuser&user_id=<?php echo $user_cookie;?>" onmouseover="doBtn();"><?php echo ptranslate("My Info");?></a></TD>
 					<TD class="topBtnOff" nowrap bgcolor="#cccccc" align="center"  onmouseover="doBtn();" onmouseout="tboff();" onmousedown="doBtn();" onmouseup="doBtn();"><A href="./index.php?logout=-1" onmouseover="doBtn();"><?php echo ptranslate("Logout");?></a></TD>
 					<TD class="topBtnOff" nowrap bgcolor="#cccccc" align="center"  onmouseover="doBtn();" onmouseout="tboff();" onmousedown="doBtn();" onmouseup="doBtn();"><?php echo ptranslate("Help");?></TD>
+					<TD class="topBtnOff" nowrap bgcolor="#cccccc" align="center"  onmouseover="doBtn();" onmouseout="tboff();" onmousedown="doBtn();" onmouseup="doBtn();"><A href="./index.php?m=help&a=about" onmouseover="doBtn();"><?php echo ptranslate("About");?></a></TD>
 				</TR>
 			</TABLE>
 	</TR>
