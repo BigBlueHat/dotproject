fa251ac0d4015808a55472a21b51d9fe98380051
eddieajau <eddieajau>
2002-08-09 00:57:10 +0000

    Initial upload

---
+++ dotproject/modules/help/access_denied.php
@@ -0,0 +1,23 @@
+<img src="images/shim.gif" width="1" height="5" alt="" border="0"><br>
+<TABLE width="95%" border=0 cellpadding="0" cellspacing=1>
+	<TR>
+	<TD><img src="./images/icons/dp.gif" alt="" border="0"></td>
+		<TD nowrap width="100%"><span class="title">Access Denied</span></td>
+	</tr>
+</TABLE>
+
+
+
+<p><TABLE width="95%" border=0 cellpadding="5" cellspacing=0>
+<tr valign=top>
+	<td width=50%>
+		<p>You have attemped to access an with without the sufficient permissions to do so.
+
+		<p>Please contact the DotProject System Administrator.
+
+	</td>
+	<td width=50%>
+		<p>
+	</td>
+</tr>
+</table>
