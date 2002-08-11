0404ad35d11ef50d6d9bf2e4f8e1f5aced163495
eddieajau <eddieajau>
2002-08-11 09:16:15 +0000

    Formatting changes

--- dotproject/index.php
+++ dotproject/index.php
@@ -2,53 +2,42 @@
 error_reporting( E_PARSE | E_CORE_ERROR | E_WARNING);
 //error_reporting(E_ALL );
 
-
-if(isset($HTTP_POST_VARS["cookie_project"]))
-{
+if (isset( $HTTP_POST_VARS["cookie_project"] )) {
 	setcookie("cookie_project", $HTTP_POST_VARS["cookie_project"]);
 }
 
 header ("Expires: Mon, 26 Jul 1997 05:00:00 GMT");    // Date in the past
-header ("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT"); 
+header ("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
                                                       // always modified
 header ("Cache-Control: no-cache, must-revalidate");  // HTTP/1.1
 header ("Pragma: no-cache");                          // HTTP/1.0
-      
 
 
-if(empty($m))$m = "companies";
-if(isset($HTTP_GET_VARS["m"]))
-	{
-	$m = $HTTP_GET_VARS["m"];
-	}
-	else
-	{
-	$m = $m;
-	}
+$m = isset( $HTTP_GET_VARS['m'] ) ? $HTTP_GET_VARS['m'] :
+	(isset( $HTTP_COOKIE_VARS['m'] ) ? $HTTP_COOKIE_VARS['m'] : 'companies');
+$a = isset( $HTTP_GET_VARS['a'] )? $HTTP_GET_VARS['a'] : 'index';
+
 setcookie("m", $m, time()+234234532523);
-if(empty($a))$a = "index";
 
+if (isset( $mmodule )) {
+	$m="mail";
+}
 
-
-if(isset($mmodule))$m="mail";
 require "./includes/config.php";
 require "./includes/db_connect.php";
 require "./includes/main_functions.php";
 require "./includes/permissions.php";
 @include "./functions/" . $m . "_func.php";
+
 //do some db work if dosql is set
-if(isset($HTTP_POST_VARS["dosql"])) require("./dosql/" . $HTTP_POST_VARS["dosql"] . ".php");
-
-if(isset($return))header("Location: /" . $return);
-
+if (isset( $HTTP_POST_VARS["dosql"]) ) {
+	require("./dosql/" . $HTTP_POST_VARS["dosql"] . ".php");
+}
+if (isset( $return )) {
+	header("Location: /" . $return);
+}
 
 require "./includes/header.php";
-
-
-
-
 require "./modules/" . $m . "/" . $a . ".php";
-
-
 require "./includes/footer.php";
 ?>
