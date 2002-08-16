52ee1c9af9579f4f746cd30dbd604b4415a75e0a
eddieajau <eddieajau>
2002-08-16 02:26:11 +0000

    Parse master cookie

--- dotproject/logincheck.php
+++ dotproject/logincheck.php
@@ -3,24 +3,26 @@ include "./includes/config.php";
 include "./includes/db_connect.php";
 
 //Check Login
-$psql = "select user_id, user_first_name, user_last_name
-					from users,  permissions
-					where user_username = '$username'
-					and user_password = password('$password') 
-					and users.user_id = permissions.permission_user
-					and permission_value <> 0
-					";
+$psql = "select
+user_id, user_first_name, user_last_name, user_company
+from users,  permissions
+where user_username = '$username'
+and user_password = password('$password') 
+and users.user_id = permissions.permission_user
+and permission_value <> 0
+";
+
 $prc = mysql_query($psql);
 
 //Pull record, write bad login if exists
-if (!$row = mysql_fetch_array($prc)) {
+if (!$row = mysql_fetch_array( $prc, MYSQL_NUM )) {
 	$message  = "Login Failed.";
 	include "./includes/login.php";
 	die;
 }
 
 setcookie( "user_cookie", $row[0] );
-setcookie( "user_full_name", $row[1].' '.$row[2] );
+setcookie( "thisuser", implode( '|', $row ) );
 ?>
 <script language="JavaScript">
 window.location = "./index.php"
