b7abac535ee71089f73649cf516d4d862f8299ea
eddieajau <eddieajau>
2002-08-30 04:42:08 +0000

    Updated permissions

--- dotproject/functions/calendar_func.php
+++ dotproject/functions/calendar_func.php
@@ -1,42 +1,85 @@
 <?php
 //Calendar functions
-if(empty($thisMonth)){$thisMonth = date("n", time());}
-if(empty($thisYear)){$thisYear = date("Y", time());}
+if (empty( $thisMonth )) {
+	$thisMonth = date( "n", time() );
+}
+if (empty( $thisYear )) {
+	$thisYear = date( "Y", time() );
+}
 
 $company_id = isset($HTTP_GET_VARS["company_id"]) ? $HTTP_GET_VARS["company_id"] :
-	(isset($HTTP_POST_VARS["company_id"]) ? $HTTP_POST_VARS["company_id"] : '0');
+	(isset($HTTP_POST_VARS["company_id"]) ? $HTTP_POST_VARS["company_id"] : $thisuser_company);
 
 $pwhere = $company_id ? "and project_company = $company_id" : '';
 //----------------------------------
 //Pull added events with start date in this month
 //----------------------------------
 $psql = "
-select event_title, event_id,
-	month(from_unixtime(event_start_date)) as m,
-	DAYOFMONTH(from_unixtime(event_start_date)) as d,
-	year(from_unixtime(event_start_date)) as y
-from events
-where month(from_unixtime(event_start_date)) = $thisMonth
-	and year(from_unixtime(event_start_date)) = $thisYear
+SELECT event_title, event_id,
+	month(from_unixtime(event_start_date)) AS m,
+	DAYOFMONTH(from_unixtime(event_start_date)) AS d,
+	year(from_unixtime(event_start_date)) AS y
+FROM events
+WHERE month(from_unixtime(event_start_date)) = $thisMonth
+	AND year(from_unixtime(event_start_date)) = $thisYear
 ";
 
-$perc = mysql_query($psql);
-while($perow = mysql_fetch_array($perc)){
+$perc = mysql_query( $psql );
+while ($perow = mysql_fetch_array( $perc )) {
 	$events[] = array("color"=>"blue","type"=>"e","title"=>"Event: " . $perow["event_title"], "d"=> $perow["d"],"m"=>$perow["m"], "y" => $perow["y"], "name"=>$perow["event_title"],"id" =>$perow["event_id"]);
 }
 //----------------------------------
 //Pull Starting projects for this month
 //----------------------------------
+// get denied projects
+$dsql = "
+SELECT distinct project_id
+FROM projects, permissions
+WHERE permission_user = $user_cookie
+	AND permission_grant_on = 'projects' 
+	AND permission_item = project_id
+	AND permission_value = 0
+";
+##echo "<pre>$dsql</pre>";
+$drc = mysql_query($dsql);
+$deny = array();
+while ($row = mysql_fetch_array( $drc, MYSQL_NUM )) {
+	$deny[] = $row[0];
+}
+// get allowed projects
+$asql = "
+SELECT distinct project_id
+FROM projects, permissions
+WHERE permission_user = $user_cookie
+	AND permission_value <> 0 
+	AND (
+		(permission_grant_on = 'all')
+		OR (permission_grant_on = 'projects' and permission_item = -1)
+		OR (permission_grant_on = 'projects' and permission_item = project_id)
+	)
+"
+. (count($deny) > 0 ? 'AND project_id NOT IN (' . implode( ',', $deny ) . ')' : '');
+##echo "<pre>$asql</pre>";
+$arc = mysql_query( $asql );
+$pallow = array();
+while ($row = mysql_fetch_array( $arc, MYSQL_NUM )) {
+	$pallow[] = $row[0];
+}
+
+$pwhere .= (count($pallow) > 0 ? ' AND project_id IN (' . implode( ',', $pallow ) . ')' : '');
+
 $psql = "
-select project_name, project_id,
-	month(project_start_date) as m,
+SELECT project_name, project_id,
+	MONTH(project_start_date) as m,
 	DAYOFMONTH(project_start_date) as d,
-	year(project_start_date) as y,
+	YEAR(project_start_date) as y,
 	project_color_identifier as color
-from projects where month(project_start_date) = $thisMonth
-	and year(project_start_date) = $thisYear
+FROM projects
+WHERE MONTH(project_start_date) = $thisMonth
+	AND YEAR(project_start_date) = $thisYear
 	$pwhere
 ";
+##echo "<pre>$psql</pre>";
 $perc = mysql_query( $psql );
 while ($perow = mysql_fetch_array( $perc )) {
 	$events[] = array( "color"=>$perow["color"],"type"=>"p","title"=>"Start: " . $perow["project_name"], "d"=> $perow["d"],"m"=>$perow["m"], "y" => $perow["y"], "name"=>$perow["project_name"],"id" =>$perow["project_id"] );
@@ -46,16 +89,17 @@ while ($perow = mysql_fetch_array( $perc )) {
 //----------------------------------
 $psql = "
 select project_name, project_id,
-	month(project_end_date) as m,
+	MONTH(project_end_date) as m,
 	DAYOFMONTH(project_end_date) as d,
-	year(project_end_date) as y,
+	YEAR(project_end_date) as y,
 	project_color_identifier as color
-from projects where month(project_end_date) = $thisMonth
-	and year(project_end_date) = $thisYear
+FROM projects
+WHERE month(project_end_date) = $thisMonth
+	AND YEAR(project_end_date) = $thisYear
 	$pwhere
 ";
-
-$perc = mysql_query($psql);
+##echo "<pre>$psql</pre>";
+$perc = mysql_query( $psql );
 while ($perow = mysql_fetch_array( $perc )) {
 	$events[] = array("color"=>$perow["color"],"type"=>"p","title"=>"Due: " . $perow["project_name"], "d"=> $perow["d"],"m"=>$perow["m"], "y" => $perow["y"], "name"=>$perow["project_name"],"id" =>$perow["project_id"]);
 }
@@ -125,11 +169,5 @@ function eventsForDate($d, $m, $y){
 		}
 	}
 	return $fillcal;
-
-
-
 }
-
-
-
 ?>
