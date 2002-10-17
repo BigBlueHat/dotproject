9855907a9328dc283259a731a7774a4f930f68b6
eddieajau <eddieajau>
2002-10-17 06:54:08 +0000

    Initial upload

---
+++ dotproject/functions/admin_func.php
@@ -0,0 +1,13 @@
+<?php
+// user types
+$utypes = array(
+	0 => '',
+	1 => 'Administrator',
+	2 => 'CEO',
+	3 => 'Director',
+	4 => 'Branch Manager',
+	5 => 'Manager',
+	6 => 'Supervisor',
+	7 => 'Employee'
+);
+?>
