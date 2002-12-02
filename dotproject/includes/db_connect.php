92594ebf4903eab42a7bd42d1a599c0b47da3197
eddieajau <eddieajau>
2002-12-02 22:43:48 +0000

    Modifications.  See CHANGELOG

--- dotproject/includes/db_connect.php
+++ dotproject/includes/db_connect.php
@@ -30,31 +30,30 @@ db_connect( $dbhost, $db, $dbuser, $dbpass );
 /* DB METHODS */
 
 function db_connect( $host='localhost', $dbname, $user='root', $passwd='', $port='3306' ) {
-
 	mysql_pconnect( "$host:$port", $user, $passwd )
 		 or die( 'FATAL ERROR: Connection to database server failed' );
 
-	if( $dbname ) {
+	if ($dbname) {
 		mysql_select_db( $dbname )
 			or die( "FATAL ERROR: Database not found ($dbname)" );
 	} else {
 		die( "FATAL ERROR: Database name not supplied<br />(connection to database server succesful)" );
 	}
-
 }
 
-function db_error() {
-	return mysql_error();
+function db_error( $errLine='0' ) {
+	$s = '<span class="error">'.mysql_error().'</span>';
+	$s .= ($errLine ? "<br />The SQL error occured at line $errLine" : '');
+	return $s;
 }
 
 function db_errno() {
 	return mysql_errno();
 }
 
-function db_loadObject( $sql, &$object ) {
+function db_loadObject( $sql, &$object, $errLine='0' ) {
 	$hash = array();
-	if( ! db_loadHash( $sql, &$hash ) ) {
-		db_error();
+	if( !db_loadHash( $sql, &$hash, $errLine ) ) {
 		return false;
 	}
 	bindHashToObject( $hash, $object );
@@ -69,49 +68,44 @@ function db_exec( $sql ) {
 	return $cur;
 }
 
-function db_num_rows( $qid )
-{
+function db_num_rows( $qid ) {
 	return mysql_num_rows( $qid );
 }
 
-function db_loadHash( $sql, &$hash )
-{
+function db_loadHash( $sql, &$hash, $errLine='0' ) {
 	$cur = mysql_query( $sql );
-	$cur or die( db_error() );
+	$cur or exit( db_error( $errLine ) );
 	$hash = mysql_fetch_assoc( $cur );
-	mysql_free_result($cur );
-	if( $hash == false  )
+	mysql_free_result( $cur );
+	if ($hash == false)
 		return false;
 	else
-		return true;	
+		return true;
 }
 
-function db_loadHashList( $sql )
-{
+function db_loadHashList( $sql, $errLine='0' ) {
 	$cur = mysql_query( $sql );
-	$cur or die( "Error in DB_loadHashList -" . db_error() . " - SQL=$sql" );
+	$cur or exit( db_error( $errLine ) );
 	$hashlist = array();
-	while( $hash = mysql_fetch_array( $cur ) )
-	{
+	while ($hash = mysql_fetch_array( $cur )) {
 		$hashlist[$hash[0]] = $hash[1];
 	}
-	mysql_free_result($cur);
+	mysql_free_result( $cur );
 	return $hashlist;
 }
 
-
-function db_loadList( $sql, $maxrows = NULL )
-{
+function db_loadList( $sql, $maxrows=NULL, $errLine='0'  ) {
 	$cur = mysql_query( $sql );
-	$cur  or db_error();
+	$cur or exit( '<span class="error">'.db_error().'</span>'.($errLine ? "<br />The SQL error occured at line $errLine" : '') );
 	$list = array();
 	$cnt = 0;
-	while( $hash = mysql_fetch_array( $cur, MYSQL_ASSOC ) ) {
+	while ($hash = mysql_fetch_array( $cur, MYSQL_ASSOC )) {
 		$list[] = $hash;
-		if( $maxrows && $maxrows == $cnt++ )
+		if( $maxrows && $maxrows == $cnt++ ) {
 			break;
+		}
 	}
-	mysql_free_result($cur);
+	mysql_free_result( $cur );
 	return $list;
 }
 
@@ -122,51 +116,53 @@ function db_loadList( $sql, $maxrows = NULL )
 function db_loadObjectList( $sql, $object, $maxrows = NULL )
 {
 	$cur = mysql_query( $sql );
-	if( ! $cur ) die( "DB_loadObjectList : " . db_error() );
+	if (!$cur) {
+		die( "DB_loadObjectList : " . db_error() );
+	}
 	$list = array();
 	$cnt = 0;
-	while( $row = mysql_fetch_array( $cur ) ) 
-	{	
+	while ($row = mysql_fetch_array( $cur )) {
 		$list[] = $object->Load( $row[0] );
-		if( $maxrows && $maxrows == $cnt++ )
+		if( $maxrows && $maxrows == $cnt++ ) {
 			break;
+		}
 	}
-	mysql_free_result($cur);
+	mysql_free_result( $cur );
 	return $list;
 }
 
-function DB_insertArray( $table, &$hash, $verbose=false )
-{
+function DB_insertArray( $table, &$hash, $verbose=false ) {
 	$fmtsql = "insert into $table ( %s ) values( %s ) ";
-	foreach( $hash as $k => $v ) {
-		if( is_array($v) or is_object($v) or $v == NULL ) 
+	foreach ($hash as $k => $v) {
+		if (is_array($v) or is_object($v) or $v == NULL) {
 			continue;
+		}
 		$fields[] = $k;
 		$values[] = "'" . mysql_escape_string( $v ) . "'";
 	}
 	$sql = sprintf( $fmtsql, implode( ",", $fields ) ,  implode( ",", $values ) );
-	
+
 	( $verbose ) && print "$sql<br>\n";
-	
-	if( ! mysql_query( $sql ) )
+
+	if (!mysql_query( $sql )) {
 		return false;
+	}
 	$id = mysql_insert_id();
-	return true;	
+	return true;
 }
 
-function db_updateArray( $table, &$hash, $keyName, $verbose=false )
-{
+function db_updateArray( $table, &$hash, $keyName, $verbose=false ) {
 	$fmtsql = "UPDATE $table SET %s WHERE %s";
-	foreach( $hash as $k => $v ) {
-			
-		if( is_array($v) or is_object($v) or $k[0] == '_' ) // internal or NA field 
+	foreach ($hash as $k => $v) {
+
+		if( is_array($v) or is_object($v) or $k[0] == '_' ) // internal or NA field
 			continue;
 
 		if( $k == $keyName ) { // PK not to be updated
 			$where = "$keyName='" . mysql_escape_string( $v ) . "'";
 			continue;
 		}
-		if( $v == '' ) 
+		if( $v == '' )
 			$val = 'NULL';
 		else
 			$val = "'" . mysql_escape_string( $v ) . "'";
@@ -195,47 +191,43 @@ function db_dateConvert( $src, &$dest, $srcFmt )
 
 function db_datetime( $timestamp = NULL )
 {
-	if( ! $timestamp )
+	if (!$timestamp) {
 		return NULL;
-	if( is_object($timestamp) )
+	}
+	if (is_object($timestamp)) {
 		return $timestamp->toString( '%Y-%m-%d %H:%M:%S');
-	else
+	} else {
 		return strftime( '%Y-%m-%d %H:%M:%S', $timestamp );
+	}
 }
 
-function db_escape( $str )
-{
+function db_escape( $str ) {
 	return mysql_escape_string( $str );
 }
-
 
 /*
  *  copy the hash array content into the object as properties
  *  only existing properties of object are filled. when undefined in hash, properties wont be deleted
  *  @param obj byref the object to fill of any class
- *  @param hash the input array 
+ *  @param hash the input array
  */
 function bindHashToObject( $hash, &$obj, $prefix=NULL )
 {
 	is_array( $hash ) or die( "bindHashToObject : hash expected" );
 	is_object( $obj ) or die( "bindHashToObject : object expected" );
-	if( $prefix ) {
-		foreach( get_object_vars($obj) as $k => $v ) {
-			if( isset($hash[$prefix . $k ]) ) {
+	if ($prefix) {
+		foreach (get_object_vars($obj) as $k => $v) {
+			if (isset($hash[$prefix . $k ])) {
 				$obj->$k = $hash[$k];
 			}
 		}
 	} else {
-		foreach( get_object_vars($obj) as $k => $v ) {
-			if( isset($hash[$k]) ) {
+		foreach (get_object_vars($obj) as $k => $v) {
+			if (isset($hash[$k])) {
 				$obj->$k = $hash[$k];
 			}
 		}
 	}
-	echo "obj="; print_r($obj ); exit;
+	//echo "obj="; print_r($obj); exit;
 }
-
-
-
-
 ?>
