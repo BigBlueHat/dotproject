fc6b069a74452e6aa6b84c13ce5b5582a91f14e7
eddieajau <eddieajau>
2002-12-05 07:03:26 +0000

    Reworked based on new CCompany class functions and messaging techniques

--- dotproject/dosql/company_aed.php
+++ dotproject/dosql/company_aed.php
@@ -1,81 +1,28 @@
 <?php
-if (empty( $company_id )) {
-	$company_id = 0;
-}
-if ($company_owner =="") {
-	$co = $user_cookie;
-} else {
-	$co =$company_owner;
+$del = isset($HTTP_POST_VARS['del']) ? $HTTP_POST_VARS['del'] : 0;
+$isNotNew = @$HTTP_POST_VARS['company_id'];
+
+$company = new CCompany();
+
+if (($msg = $company->bind( $HTTP_POST_VARS ))) {
+	$AppUI->setMsg( $msg, UI_MSG_ERROR );
+	$AppUI->redirect();
 }
 
 if ($del) {
-	$test = "select project_id from projects where project_company = $company_id";
-
-	$testrc = mysql_query($test);
-	if(mysql_num_rows($testrc)) {
-		$message = "You cannot delete a company that has projects associated with it";
+	if (($msg = $company->delete())) {
+		$AppUI->setMsg( $msg, UI_MSG_ERROR );
+		$AppUI->redirect();
 	} else {
-		$sql = "delete from companies where company_id = $company_id";
-		$rsql = mysql_query($sql);
-		$message = mysql_error();
+		$AppUI->setMsg( "Company deleted", UI_MSG_ALERT );
+		$AppUI->redirect( "m=companies" );
 	}
-} else if (($company_id) == 0) {
-	$company_name = $HTTP_POST_VARS["company_name"];
-	$sql =
-	"insert into companies (
-	company_name,
-	company_phone1,
-	company_phone2,
-	company_fax,
-	company_address1,
-	company_address2,
-	company_city,
-	company_state,
-	company_zip,
-	company_primary_url,
-	company_owner,
-	company_description )
-	values
-	(
-	'$company_name',
-	'$company_phone1',
-	'$company_phone2',
-	'$company_fax',
-	'$company_address1',
-	'$company_address2',
-	'$company_city',
-	'$company_state',
-	'$company_zip',
-	'$company_primary_url',
-	$user_cookie,
-	'$company_description')";
-
-	$rsql = mysql_query($sql);
-	$message = mysql_error();
-
 } else {
-	$company_name = $HTTP_POST_VARS["company_name"];
-	$sql = "update companies set
-	company_name='$company_name',
-	company_phone1='$company_phone1',
-	company_phone2='$company_phone2',
-	company_fax='$company_fax',
-	company_address1='$company_address1',
-	company_address2='$company_address2',
-	company_city='$company_city',
-	company_state='$company_state',
-	company_zip='$company_zip',
-	company_primary_url='$company_primary_url',
-	company_owner=$co,
-	company_description= '$company_description'
-	where
-	company_id = $company_id";
-
-	$rsql = mysql_query($sql);
-	$message = mysql_error();
+	if (($msg = $company->store())) {
+		$AppUI->setMsg( $msg, UI_MSG_ERROR );
+	} else {
+		$AppUI->setMsg( "Company ".($isNotNew ? 'updated' : 'inserted'), UI_MSG_OK );
+	}
+	$AppUI->redirect();
 }
-
 ?>
-<script language="javascript">
-	window.location="./index.php?m=companies&message=<?php echo $message;?>";
-</script>
