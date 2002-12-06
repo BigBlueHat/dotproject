87f905829173fac286900139149d50407e774318
eddieajau <eddieajau>
2002-12-06 07:03:15 +0000

    Added pilot multi-lungual support

--- dotproject/includes/login.php
+++ dotproject/includes/login.php
@@ -1,6 +1,11 @@
 <?php
 $return = '';
 setcookie("user_cookie", "");
+$AppUI = new CAppUI;
+$AppUI->locale_warn = true;
+$AppUI->user_locale = $host_locale;
+
+@include_once( "$root_dir/locales/core.php" );
 ?>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
 <html>
@@ -17,32 +22,38 @@ setcookie("user_cookie", "");
 <form action="./logincheck.php" method="post" name="loginform">
 <input type="hidden" name="login" value="<?php echo time();?>">
 <input type="hidden" name="return" value="<?php echo $return;?>">
-
 <tr>
 	<td colspan="2" class="headerfontWhite" bgcolor="gray">
 		<b><?php echo $company_name;?></b>
 	</td>
 </tr>
 <tr>
-	<td bgcolor="#eeeeee" valign="top" align="right" nowrap width="80">
-		Username:
+	<td bgcolor="#eeeeee" align="right" nowrap width="100">
+		<?php echo $AppUI->_('username');?>:
 	</td>
 	<td bgcolor="#eeeeee" align=left class="menufontlight" nowrap>
-		<input type="text" size="25" name="username"class=text>
+		<input type="text" size="25" name="username" class=text>
 	</td>
 </tr>
 <tr>
-	<td bgcolor="#eeeeee" valign="top" align="right"  nowrap>
-		Password:
+	<td bgcolor="#eeeeee" align="right"  nowrap>
+		<?php echo $AppUI->_('password');?>:
 	</td>
-	<td bgcolor="#eeeeee" align=left class="menufontlight" nowrap>
+	<td bgcolor="#eeeeee" align="left" class="menufontlight" nowrap>
 		<input type="password" size="25" name="password" class="text">
-		<p><input type="submit" value="Login" class=button></p>
+	</td>
+</tr>
+<tr>
+	<td bgcolor="#eeeeee" align="center" class="menufontlight" nowrap colspan="2">
+		<input type="submit" value="<?php echo $AppUI->_('login');?>" class="button"></p>
 	</td>
 </tr>
 </table>
 
-<p align="center"><?php echo @$message?></p>
+<p align="center"><?php 
+	echo $AppUI->_( @$message );
+	echo ini_get( 'register_globals') ? '' : '<br><span class="warning">WARNING: dotproject is not supported with register_globals=off</span>';
+?></p>
 
 <table align="center" border="0" width="250" cellpadding="4" cellspacing="0">
 <tr>
@@ -50,7 +61,7 @@ setcookie("user_cookie", "");
 		<br>
 		<ul type="square">
 			<li>
-				<A href="mailto:<?php echo 'admin@' . $site_domain;?>">Help! I've forgotten my username and password!</a>
+				<A href="mailto:<?php echo 'admin@' . $site_domain;?>"><?php echo $AppUI->_('forgotPassword');?></a>
 			</li>
 		</ul>
 	</td>
@@ -59,9 +70,7 @@ setcookie("user_cookie", "");
 	<td align=center>
 		<img src="./images/icons/dp.gif" width="42" height="42" border=0 alt="dotproject">
 		<p>dotproject</p>
-		<p>an open-source
-		<br />project management
-		<br />application</p>
+		<p><?php echo $AppUI->_('openSource');?></p>
 	</td>
 </tr>
 </form>
