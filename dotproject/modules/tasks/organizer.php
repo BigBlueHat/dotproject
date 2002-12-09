e83c2228bf8085024f17d91a4fd7956c1ed30bf8
kripper <kripper>
2002-12-09 10:26:48 +0000

    Added options Consider task's user assignements for two overlapping tasks (a user is defined as essential if there are not more than two other users assigned to this task)

--- dotproject/modules/tasks/organizer.php
+++ dotproject/modules/tasks/organizer.php
@@ -12,13 +12,10 @@
  *	- task dependencies
  * 
  */
-
-	$show_no_end_date_warnings = true;
-	$show_delayed_tasks_warnings = true;
 		
  	$errors = false;
 	$tasks = array();
-	if(!$do) $do="ask";
+	if(!$do) $do="conf";
 	
 	function task_link($task) {
 		return "<a href='index.php?m=tasks&a=view&task_id=" . $task["task_id"] . "'>" . $task["task_name"] . "</a>";
@@ -33,8 +30,8 @@
 	}
 		
 	function log_info($msg) {
-		global $do;
-		if($do == "ask") {
+		global $option_debug;
+		if($option_debug) {
 			echo "$msg<br>";
 		}
 	}	
@@ -58,13 +55,37 @@
 				
 		global $tasks, $do;
 		
+		// don't fixate tasks before now
+		
+		if($time < time()) $time = time();
+		
 		$str_start_date = date("Y-m-d", strtotime(date("Y-m-d", $time) . " + 1 day"));
 		$str_end_date = get_end_date($str_start_date, $tasks[$task_index]["task_duration"]);
 		
-		// check tasks overlapping
+		// Complex SQL explanation:
+		//
+		// Objective: Check tasks overlapping only when
+		// a user is vital for both tasks
+		//
+		// Definition of "vital for one task": when a task is assigned to user and total_users <= 2
+		// (for example: if task is assigned to tree o more users, he is not vital).
+		//  
+		// Thus, a user is vital for both tasks <=>
+		//	- total_users <= 2 for both tasks
+		//	- and he apears in both tasks
+		//
+		// Thus, in both tasks (say 4 and 10), a there will be a vital user <=>
+		//	- "number of tasks with total_users <= 2"
+		//	  = rows("select count(*) as num_users from user_tasks
+		//	  where task_id=4 or task_id=10
+		//	  group by task_id having num_users <= 2") == 2;
+		//
+		//	- and "number of users which appears in both tasks"
+		//	  = rows("select count(*) as frec 
+		//	  from user_tasks where task_id=4 or task_id=10
+		//	  group by user_id having frec = 2") > 0
 				
-		$task = $tasks[$tasks_index];
-		$t1_start = strtotime($str_start_date);
+		$t1_start = strtotime($str_start_date);	
 		$t1_end = strtotime($str_end_date);
 		
 		foreach($tasks as $task2) {
@@ -76,14 +97,31 @@
 				|| ($t1_end >= $t2_start && $t1_end <= $t2_end))
 			) {
 				// tasks are overlapping
-				// log_info("$t1_start - $t1_end<br>$t2_start - $t2_end<br>");
-				log_info("Task can't be set to [$str_start_date - $str_end_date] due to conflicts with task " . task_link($task2) . ".");
-				fixate_task($task_index, $t2_end, $dep_on_task);
-				return;
+				
+				$t1 = $tasks[$task_index]["task_id"];
+				$t2 = $task2["task_id"];
+				
+				$sql1 = "select count(*) as num_users from user_tasks where task_id=$t1 or task_id=$t2 group by task_id having num_users <= 2";
+				$sql2 = "select count(*) as frec from user_tasks where task_id=$t1 or task_id=$t2 group by user_id having frec = 2";
+		
+				$vital = mysql_num_rows(mysql_query($sql1)) == 2 && mysql_num_rows(mysql_query($sql2)) > 0;
+				if($vital) {
+				
+					log_info("Task can't be set to [$str_start_date - $str_end_date] due to conflicts with task " . task_link($task2) . ".");
+					fixate_task($task_index, $t2_end, $dep_on_task);
+					return;
+				} else {
+					log_info("Task conflicts with task " . task_link($task2) . " but there are no vital users.");
+				}
 			}
 		}
 		
 		$tasks[$task_index]["fixed"] = true;
+		
+		// be quite if nothing will be changed
+		
+		if($tasks[$task_index]["task_start_date"] == $str_start_date && $tasks[$task_index]["task_end_date"] = $str_end_date) return;
+		
 		$tasks[$task_index]["task_start_date"] = $str_start_date;
 		$tasks[$task_index]["task_end_date"] = $str_end_date;
 			
@@ -191,7 +229,7 @@
 			
 			if($all_fixed) {
 				// this task depends only on fixated tasks
-				log_info("all dependencies are fixed");				
+				log_info("all dependencies are fixed");
 				fixate_task($i, $latest_end_date, $dep_on_task);
 			} else {
 				log_error("task has not fixed dependencies");
@@ -244,6 +282,35 @@ if($set_dynamic) {
 
 <form name="form" method="post">
 
+<?php
+	// default options
+	if(!isset($option_check_delayed_tasks)) $option_check_delayed_tasks=1;
+	if(!isset($option_no_end_date_warning)) $option_no_end_date_warning=0;
+	if(!isset($option_debug)) $option_debug=0;
+?>
+<table border="0" cellpadding="4" cellspacing="0" width="98%" class="std">
+<tr>
+	<td>
+		<input type=checkbox name=option_check_delayed_tasks value=1 <?php echo $option_check_delayed_tasks?"checked":"" ?>>Check delayed fixed tasks<br>
+		<input type=checkbox name=option_no_end_date_warning value=1 <?php echo $option_no_end_date_warning?"checked":"" ?>>Warn of fixed tasks withoud end dates<br>
+		<?php /*
+		<input type=checkbox name=option_project value=1 <?php echo $option_project?"checked":"" ?>>Organize tasks belonging only to <select name=option_project_id>
+			<?php
+				$sql = "select project_id, project_name from projects";
+				$query = mysql_query($sql);
+				while($project = mysql_fetch_array($query)) {
+					echo "<option value=" . $project["project_id"] . ">" . $project["project_name"] . "</option>";
+				}
+			?>
+		</select><br>
+		*/ ?>
+		<input type=checkbox name=option_debug value=1 <?php echo $option_debug?"checked":"" ?>>Show debug info<br>
+	</td>
+</tr>
+</table>
+<br>
+
+<?php if($do != "conf") { ?>
 <table border="0" cellpadding="4" cellspacing="0" width="98%" class="std">
 <tr>
 	<td>
@@ -252,7 +319,7 @@ if($set_dynamic) {
 			/**** Add tasks to an array and check conflicts ****/
 		
 			// Select tasks without childrens (sub tasks)
-			$sql = "select a.*,!a.task_dynamic as fixed from tasks as a left join tasks as b on a.task_id = b.task_parent and a.task_id != b.task_id where b.task_id IS NULL or b.task_id = b.task_parent order by a.task_priority desc";
+			$sql = "select a.*,!a.task_dynamic as fixed from tasks as a left join tasks as b on a.task_id = b.task_parent and a.task_id != b.task_id where b.task_id IS NULL or b.task_id = b.task_parent order by a.task_priority desc, a.task_order desc";
 			$dtrc = mysql_query( $sql );
 
 			while ($row = mysql_fetch_array( $dtrc, MYSQL_ASSOC )) {
@@ -262,7 +329,7 @@ if($set_dynamic) {
 			        if(!$row["task_dynamic"] && $row["task_end_date"] == "0000-00-00 00:00:00") {
 			        	if($row["task_duration"] != 0) {
 				        	$row["task_end_date"] = get_end_date($row["task_start_date"], $row["task_duration"]);
-				        	if($do=="ask" && $show_no_end_date_warnings) {
+				        	if($do=="ask" && $option_no_end_date_warning) {
 					        	log_warning("Task " . task_link($row) . " has no end date. Using tasks duration instead.",
 					        	"<input type=checkbox name='set_end_date[" . $row["task_id"] . "]' value=1> Set end date to " . formatTime(strtotime($row["task_end_date"])));
 					        }
@@ -285,9 +352,9 @@ if($set_dynamic) {
 				        	// nothing has be done yet
 				        	$end_time = strtotime($row["task_end_date"]);
 				        	if($end_time < time()) {
-				        		if($show_delayed_tasks_warnings) {
+				        		if($option_check_delayed_tasks) {
 					        		log_warning("Task " .task_link($row) . " started on " . formatTime(strtotime($row["task_start_date"])) . " and ended on " . formatTime($end_time) . "." ,
-					        		"<input type=checkbox name=set_dynamic[" . $row["task_id"] . "] value=1 checked> Set as dynamic task<br>" .
+					        		"<input type=checkbox name=set_dynamic[" . $row["task_id"] . "] value=1 checked> Set as dynamic task and reorganize<br>" .
 					        		"<input type=checkbox name=set_priority[" . $row["task_id"] . "] value=1 checked> Set priority to high<br>"				        		
 								);
 							}
@@ -302,7 +369,6 @@ if($set_dynamic) {
 				for($i = 0; $i < count($tasks) ; $i++) {
 					process_dependencies($i);
 				}
-				echo "<input type=hidden name=do value=" . ($do=="ask"?"fixate":"ask") . ">";
 			}
 		?>	
 
@@ -310,13 +376,18 @@ if($set_dynamic) {
 </tr>
 </table>
 <br>
+<?php } ?>
+
 <?php
 if(!$errors) {
-	if($do=="ask") {
+	echo "<input type=hidden name=do value=" . ($do=="ask"?"fixate":"ask") . ">";
+	if($do == "ask") {
 		echo "<font size=2><b>Do you want to accept this changes?</b></font><br>";
 		echo "<input type=button value=accept class=button onClick='javascript:document.form.submit()'>";
-	} else if ($do=="fixate") {
+	} else if ($do == "fixate") {
 		echo "<font size=2><b>Tasks has been reorganized</b></font><br>";
+	} else if ($do == "conf") {
+		echo "<input type=button value=start class=button onClick='javascript:document.form.submit()'>";
 	}
 } else {
 	echo "<font size=2><b>Please correct the above errors</b></font><br>";
