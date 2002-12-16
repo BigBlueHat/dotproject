580c0bfa8b69991fc055a28f806611a3110e6154
eddieajau <eddieajau>
2002-12-16 06:32:01 +0000

    Refashioned icons into an array for printing in a loop

--- dotproject/includes/leftnav.php
+++ dotproject/includes/leftnav.php
@@ -1,95 +1,36 @@
+<?php
+$leftnav = array(
+	array( 'companies', 'money.gif', 'Clients & Companies' ),
+	array( 'projects', 'projects.gif', 'Projects' ),
+	array( 'tasks', 'tasks.gif', 'Tasks' ),
+	array( 'calendar', 'calendar.gif', 'Calendar' ),
+	array( 'files', 'folder.gif', 'Files' ),
+	array( 'contacts', 'contacts.gif', 'Contacts' ),
+	array( 'forums', 'communicate.gif', 'Forums' ),
+	array( 'ticketsmith', 'ticketsmith.gif', 'Tickets' ),
+	array( 'admin', 'admin.gif', 'User Admin' ),
+	array( 'system', 'system.gif', 'System Admin' )
+);
+?>
 <table cellspacing=0 cellpadding=2 border=0 height="600">
 <tr>
 	<td><img src=images/shim.gif width=70 height=3></td>
 	<td rowspan="100"><img src=images/shim.gif width=10 height=100></td>
 </tr>
 <?php
-if (isset( $perms['all'] ) || isset( $perms['companies'] )) { ?>
-<tr>
-	<td align="center" valign=middle class=nav>
-		<table cellspacing=0 cellpadding=0 border=0><tr><td class="clsBtnOff"><A href="./index.php?m=companies"><img src="./images/icons/money.gif"   onmouseover="doBtn();" onmouseout="doBtn();" onmousedown="doBtn();" onmouseup="doBtn();" alt="Companies" border="0" width="30" height="30"></a></td></tr></table>
-		<?php echo $AppUI->_('Clients & Companies');?>
-	</td>
-</tr>
-<?php }
-if (isset( $perms['all'] ) || isset( $perms['projects'] )) { ?>
-<tr>
-	<td align="center" valign=middle class=nav>
-		<table cellspacing=0 cellpadding=0 border=0><tr><td class="clsBtnOff">
-		<A href="./index.php?m=projects"><img src="./images/icons/projects.gif"  onmouseover="doBtn();" onmouseout="doBtn();" onmousedown="doBtn();" onmouseup="doBtn();" alt="Projects" border="0" width="30" height="30"></a>
-		</td></tr></table>
-		<?php echo $AppUI->_('Projects');?>
-	</td>
-</tr>
-<?php }
-if (isset( $perms['all'] ) || isset( $perms['tasks'] )) { ?>
-<tr>
-	<td align="center" valign=middle class=nav>
-		<table cellspacing=0 cellpadding=0 border=0><tr><td class="clsBtnOff"><A href="./index.php?m=tasks"><img src="./images/icons/tasks.gif" alt="Tasks"  onmouseover="doBtn();" onmouseout="doBtn();" onmousedown="doBtn();" onmouseup="doBtn();" border="0" width="30" height="30"></a></td></tr></table>
-		<?php echo $AppUI->_('Tasks');?>
-	</td>
-</tr>
-<?php }
-if (isset( $perms['all'] ) || isset( $perms['calendar'] )) { ?>
-<tr>
-	<td id="clients" align="center" valign=middle class=nav>
-		<table cellspacing=0 cellpadding=0 border=0><tr><td class="clsBtnOff"><A href="./index.php?m=calendar"><img src="./images/icons/calendar.gif"   onmouseover="doBtn();" onmouseout="doBtn();" onmousedown="doBtn();" onmouseup="doBtn();" alt="Calendar" border="0" width="30" height="30"></a></td></tr></table>
-		<?php echo $AppUI->_('Calendar');?>
-	</td>
-</tr>
-<?php }
-if (isset( $perms['all'] ) || isset( $perms['files'] )) { ?>
-<tr>
-	<td align="center" valign=middle class=nav>
-		<table cellspacing=0 cellpadding=0 border=0><tr><td class="clsBtnOff"><A href="./index.php?m=files"><img src="./images/icons/folder.gif" alt="Files" border="0" width="30" height="30" onmouseover="doBtn();" onmouseout="doBtn();" onmousedown="doBtn();" onmouseup="doBtn();" ></a></td></tr></table>
-		<?php echo $AppUI->_('Files');?>
-	</td>
-</tr>
-<?php }
-if (isset( $perms['all'] ) || isset( $perms['contacts'] )) { ?>
-<tr>
-	<td align="center" valign=middle class=nav>
-		<table cellspacing=0 cellpadding=0 border=0><tr><td class="clsBtnOff"><A href="./index.php?m=contacts"><img src="./images/icons/contacts.gif" alt="Contacts" border="0" width="30" height="30" onmouseover="doBtn();" onmouseout="doBtn();" onmousedown="doBtn();" onmouseup="doBtn();" ></a></td></tr></table>
-		<?php echo $AppUI->_('Contacts');?>
-	</td>
-</tr>
-<?php }
-if (isset( $perms['all'] ) || isset( $perms['forums'] )) { ?>
-<tr>
-	<td align="center" valign=middle class=nav>
-		<table cellspacing=0 cellpadding=0 border=0><tr><td class="clsBtnOff"><A href="./index.php?m=forums"><img src="./images/icons/communicate.gif" alt="Users and Communication" border="0" width="30" height="30" onmouseover="doBtn();" onmouseout="doBtn();" onmousedown="doBtn();" onmouseup="doBtn();" ></a></td></tr></table>
-		<?php echo $AppUI->_('Forums');?>
-	</td>
-</tr>
-<?php }
-if (isset( $perms['all'] ) || isset( $perms['ticketsmith'] )) { ?>
-<tr>
-	<td align="center" valign=middle class=nav>
-		<table cellspacing=0 cellpadding=0 border=0><tr><td class="clsBtnOff">
-		<A href="./index.php?m=ticketsmith"><img src="./images/icons/ticketsmith.gif"  onmouseover="doBtn();" onmouseout="doBtn();" onmousedown="doBtn();" onmouseup="doBtn();" alt="Tickets" border="0" width="30" height="30"></a>
-		</td></tr></table>
-		<?php echo $AppUI->_('Tickets');?>
-	</td>
-</tr>
-
-<?php }
-if (isset( $perms['all'] ) || isset( $perms['admin'] )) { ?>
-<tr>
-	<td align="center" valign=middle class=nav>
-		<table cellspacing=0 cellpadding=0 border=0><tr><td class="clsBtnOff"><A href="./index.php?m=admin"><img src="./images/icons/admin.gif" alt="Admin" border="0" width="30" height="30" onmouseover="doBtn();" onmouseout="doBtn();" onmousedown="doBtn();" onmouseup="doBtn();" ></a></td></tr></table>
-		<?php echo $AppUI->_('User Admin');?>		
-	</td>
-</tr>
-
-<?php }
-if (isset( $perms['all'] ) || isset( $perms['system'] )) { ?>
-<tr>
-	<td align="center" valign=middle class=nav>
-		<table cellspacing=0 cellpadding=0 border=0><tr><td class="clsBtnOff"><A href="?m=system"><img src="./images/icons/system.gif" alt="System Administration" border="0" width="30" height="30" onmouseover="doBtn();" onmouseout="doBtn();" onmousedown="doBtn();" onmouseup="doBtn();" ></a></td></tr></table>
-		<?php echo $AppUI->_('System Admin');?>
-	</td>
-</tr>
-<?php }?>
+$s = '';
+foreach ($leftnav as $module) {
+	if (isset( $perms['all'] ) || isset( $perms[$module[0]] )) {
+		$s .= '<td align="center" valign="middle" class="nav">'
+			.'<table cellspacing=0 cellpadding=0 border=0><tr><td class="clsBtnOff">'
+			.'<a href="?m='.$module[0].'">'
+			.'<img src="./images/icons/'.$module[1].'" onmouseover="doBtn();" onmouseout="doBtn();" onmousedown="doBtn();" onmouseup="doBtn();" alt="" border="0" width="30" height="30"></a></td></tr></table>'
+			.$AppUI->_($module[2])
+			.'</td></tr>';
+	}
+}
+echo $s;
+?>
 
 <tr height="100%">
 	<td>&nbsp;<img src=images/shim.gif width=7 height=10></td>
