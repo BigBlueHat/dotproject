a7f87db50dad3aa366d44307d41250204326fd71
eddieajau <eddieajau>
2002-12-16 23:22:21 +0000

    Added comments

--- dotproject/functions/admin_func.php
+++ dotproject/functions/admin_func.php
@@ -1,8 +1,11 @@
 <?php
 // user types
 $utypes = array(
+// DEFAULT USER (nothing special)
 	0 => '',
+// DO NOT CHANGE ADMINISTRATOR INDEX !
 	1 => 'Administrator',
+// you can modify the terms below to suit your organisation
 	2 => 'CEO',
 	3 => 'Director',
 	4 => 'Branch Manager',
@@ -10,6 +13,7 @@ $utypes = array(
 	6 => 'Supervisor',
 	7 => 'Employee'
 );
+
 ##
 ##	NOTE: the user_type field in the users table must be changed to a TINYINT
 ##
