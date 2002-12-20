babcaaff1a589e6edd63b0c05cef75d77e8e2500
eddieajau <eddieajau>
2002-12-20 04:01:15 +0000

    Change help back to about.  Help now linked in context

--- dotproject/includes/header.php
+++ dotproject/includes/header.php
@@ -41,7 +41,7 @@ function tboff(){
 		<tr>
 			<td class="topBtnOff" nowrap bgcolor="#cccccc" align="center"  onmouseover="doBtn();" onmouseout="tboff();" onmousedown="doBtn();" onmouseup="doBtn();"><a href="./index.php?m=admin&a=viewuser&user_id=<?php echo $AppUI->user_id;?>" onmouseover="doBtn();"><?php echo $AppUI->_('My Info');?></a></td>
 			<td class="topBtnOff" nowrap bgcolor="#cccccc" align="center"  onmouseover="doBtn();" onmouseout="tboff();" onmousedown="doBtn();" onmouseup="doBtn();"><a href="./index.php?logout=-1" onmouseover="doBtn();"><?php echo $AppUI->_('Logout');?></a></td>
-			<td class="topBtnOff" nowrap bgcolor="#cccccc" align="center"  onmouseover="doBtn();" onmouseout="tboff();" onmousedown="doBtn();" onmouseup="doBtn();"><?php echo contextHelp( $AppUI->_('Help') );?></td>
+			<td class="topBtnOff" nowrap bgcolor="#cccccc" align="center"  onmouseover="doBtn();" onmouseout="tboff();" onmousedown="doBtn();" onmouseup="doBtn();"><?php echo contextHelp( $AppUI->_('About') );?></td>
 		</tr>
 		</table>
 	</td>
