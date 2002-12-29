f1d6098fb901ab3681fa5c299633d010f8a10dc7
eddieajau <eddieajau>
2002-12-29 12:26:06 +0000

    Minor reformatting

--- dotproject/modules/companies/vw_archived.php
+++ dotproject/modules/companies/vw_archived.php
@@ -9,7 +9,7 @@ SELECT projects.*, users.user_first_name,users.user_last_name
 FROM projects
 LEFT JOIN users ON users.user_id = projects.project_owner
 WHERE project_company = $company_id
-	and project_active = 0
+	AND project_active = 0
 ORDER BY project_name
 ";
 
@@ -24,7 +24,7 @@ if (!($rows = db_loadList( $sql, NULL ))) {
 
 	foreach ($rows as $row){
 		$s .= '<tr><td>';
-		$s .= '<a href="./index.php?m=projects&a=view&project_id='.$row["project_id"].'">'.$row["project_name"].'</a>';
+		$s .= '<a href="?m=projects&a=view&project_id='.$row["project_id"].'">'.$row["project_name"].'</a>';
 		$s .= '<td>'.$row["user_first_name"].'&nbsp;'.$row["user_last_name"].'</td>';
 		$s .= '</tr>';
 	}
