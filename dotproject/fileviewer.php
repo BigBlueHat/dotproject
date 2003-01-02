fa3c1fefad6e947bab35ff01dde892b110ae8458
eddieajau <eddieajau>
2003-01-02 05:36:18 +0000

    Updated for new code-base

--- dotproject/fileviewer.php
+++ dotproject/fileviewer.php
@@ -1,24 +1,31 @@
 <?php
 //file viewer
-include "./includes/config.php";
-include "./includes/db_connect.php";
-include "./includes/main_functions.php";
-include "./includes/permissions.php";
+require "./includes/config.php";
+require "$root_dir/includes/db_connect.php";
+require "$root_dir/classdefs/ui.php";
 
-if($file_id) {
+session_start();
+$AppUI =& $_SESSION['AppUI'];
 
+include "$root_dir/includes/main_functions.php";
+include "$root_dir/includes/permissions.php";
 
-    $query = "select * from files where file_id=$file_id";
-    $result = MYSQL_QUERY($query);
-    $type = MYSQL_RESULT($result,0,"file_type");
-		$size = MYSQL_RESULT($result,0,"file_size");
-		$dname = MYSQL_RESULT($result,0,"file_name");
-		$name = MYSQL_RESULT($result,0,"file_real_filename");
-		$file_project = MYSQL_RESULT($result,0,"file_project");
-		header("Content-length: $size");
-    	header("Content-type: $type");
-		header( "Content-disposition: attachment; filename=$dname" );
-		readfile($root_dir . "/files/" . $file_project . "/" . $name);
+$denyRead = getDenyRead( 'files' );
+if ($denyRead) {
+	$AppUI->redirect( "m=help&a=access_denied" );
+}
 
-};
-?> 
+$file_id = isset($_GET['file_id']) ? $_GET['file_id'] : 0;
+
+if ($file_id) {
+    $sql = "SELECT * FROM files WHERE file_id=$file_id";
+	db_loadHash( $sql, $file );
+	header( "Content-length: {$file['file_size']}" );
+	header( "Content-type: {$file['file_type']}" );
+	header( "Content-disposition: attachment; filename={$file['file_name']}" );
+	readfile( "$root_dir/files/{$file['file_project']}/{$file['file_real_filename']}" );
+} else {
+	$AppUI->setMsg( "fileIdError", UI_MSG_ERROR );
+	$AppUI->redirect();
+}
+?>
