497398df270c3726ef90f9942cbba667c386cff8
eddieajau <eddieajau>
2003-01-03 07:09:35 +0000

    Upgraded db, language support, popup calendar and cosmetic changes

--- dotproject/modules/calendar/addedit.php
+++ dotproject/modules/calendar/addedit.php
@@ -1,39 +1,78 @@
 <?php
-
 // check permissions
 $denyEdit = getDenyEdit( $m );
 
 if ($denyEdit) {
-	echo '<script language="javascript">
-	window.location="./index.php?m=help&a=access_denied";
-	</script>
-';
+	$AppUI->redirect( "m=help&a=access_denied" );
 }
 
-// Add / Edit event
-if(empty( $event_id )) {
-	$event_id = 0;
-}
+require_once( "$root_dir/classdefs/date.php" );
 
-//Pull event information
-$csql = "Select * from events where events.event_id = $event_id";
-$crc = mysql_query( $csql );
-$crow = mysql_fetch_array( $crc );
+$event_id = isset( $_GET['event_id'] ) ? $_GET['event_id'] : 0;
+
+// Pull event information
+$sql = "SELECT * FROM events WHERE event_id = $event_id";
+db_loadHash( $sql, $event );
 
 //Set the starting date and time
-if(is_array($crow)){
-	$sdate = strftime( "%Y-%m-%d", $crow["event_start_date"] );
-	$stime = strftime( "%H:%M", $crow["event_start_date"] );
-	$shour = strftime( "%H", $crow["event_start_date"] );
-	$smin = strftime( "%M", $crow["event_start_date"] );
-	$edate = strftime( "%Y-%m-%d", $crow["event_end_date"] );
-	$etime = strftime( "%H:%M", $crow["event_end_date"] );
-	$ehour = strftime( "%H", $crow["event_end_date"] );
-	$emin = strftime( "%M", $crow["event_end_date"] );
+if(is_array($event)){
+	$sdate = strftime( "%Y-%m-%d", $event["event_start_date"] );
+	$stime = strftime( "%H:%M", $event["event_start_date"] );
+	$shour = strftime( "%H", $event["event_start_date"] );
+	$smin = strftime( "%M", $event["event_start_date"] );
+	$edate = strftime( "%Y-%m-%d", $event["event_end_date"] );
+	$etime = strftime( "%H:%M", $event["event_end_date"] );
+	$ehour = strftime( "%H", $event["event_end_date"] );
+	$emin = strftime( "%M", $event["event_end_date"] );
 
 	$sdate = fromDate( $sdate );
 	$edate = fromDate( $edate );
 }
+
+// format dates
+$df = $AppUI->getPref('SHDATEFORMAT');
+
+$start_date = $event["event_start_date"] ? new CDate( $event["event_start_date"] ) : new CDate();
+$start_date->setFormat( $df );
+$start_time = $start_date->getHours() * 60 + $start_date->getMinutes();
+
+$end_date = $event["event_end_date"] ? new CDate( $event["event_end_date"] ) : new CDate();
+$end_date->setFormat( $df );
+$end_time = $end_date->getHours() * 60 + $end_date->getMinutes();
+
+$recurs =  array (
+	'Never',
+	'Hourly',
+	'Daily',
+	'Weekly',
+	'Bi-Weekly',
+	'Every Month',
+	'Quarterly',
+	'Every 6 months',
+	'Every Year'
+);
+
+$remind = array (
+	"900" => '15 mins',
+	"1800" => '30 mins',
+	"3600" => '1 hour',
+	"7200" => '2 hours',
+	"14400" => '4 hours',
+	"28800" => '8 hours',
+	"56600" => '16 hours',
+	"86400" => '1 day',
+	"172800" => '2 days'
+);
+
+$times = array();
+$t = new CDate();
+$t->setTime( 0,0,0 );
+for ($m=0; $m < 1440; $m+=30) {
+	$t->setTime( 0, $m, 0 );
+	$times[$m] = $t->toString( "%I:%M %p" );
+}
+$crumbs = array();
+//$crumbs["?m=calendar"] = "calendar";
 ?>
 
 
@@ -44,11 +83,11 @@ function submitIt(){
 		alert("Please enter a valid event title");
 		form.event_title.focus();
 	}
-	if (form.sdate.value.length < 1){
+	if (form.event_start_date.value.length < 1){
 		alert("Please enter a start date");
 		form.event_start_date.focus();
 	}
-	if (form.edate.value.length < 1){
+	if (form.event_end_date.value.length < 1){
 		alert("Please enter an end date");
 		form.event_end_date.focus();
 	}
@@ -68,177 +107,112 @@ function delIt(){
 	}
 }
 
-function popCalendar(x){
-	var form = document.changeevent;
+var calendarField = '';
 
-	mm = <?php echo strftime("%m", time());?>;
-	dd = <?php echo strftime("%d", time());?>;
-	yy = <?php echo strftime("%Y", time());?>;
-
-	dar = eval( "document.changeevent." + x + ".value.split('-')" );
-	if (eval( "document.changeevent." + x + ".value.length" ) > 9){
-	if (dar.length == 3) {
-		yy = parseInt(dar[0], 10);
-		mm = parseInt(dar[1], 10);
-		dd = parseInt(dar[2], 10);
-		}
-	}
-	
-	newwin = window.open('./calendar.php?page=events&form=changeevent&field=' + x + '&thisYear=' + yy + '&thisMonth=' + mm + '&thisDay=' + dd, 'calwin', 'width=250, height=220, scollbars=false');
+function popCalendar( field ){
+	calendarField = field;
+	uts = eval( 'document.changeevent.event_' + field + '.value' );
+	window.open( './calendar.php?callback=setCalendar&uts=' + uts, 'calwin', 'top=250,left=250,width=250, height=220, scollbars=false' );
 }
+
+function setCalendar( uts, fdate ) {
+	fld_uts = eval( 'document.changeevent.event_' + calendarField );
+	fld_fdate = eval( 'document.changeevent.' + calendarField );
+	fld_uts.value = uts;
+	fld_fdate.value = fdate;
+}
+
 </script>
-<table border=0 cellpadding="0" cellspacing="0" width="98%">
-	<tr>
-		<td width="44"><img src="./images/icons/calendar.gif" alt="" border="0"></td>
-		<td width="100%"><span class="title">Events</span></td>
-		<td valign="bottom">&nbsp;</td>
-	</tr>
-	<tr>
-		<td colspan=2 nowrap>This page allows you to view and edit an event</td>
-		<td align="right" nowrap><A href="javascript:delIt()">delete event <img align="absmiddle" src="./images/icons/trash.gif" width="16" height="16" alt="Delete this event" border="0"></a></td>
-	</tr>
+<table border="0" cellpadding="0" cellspacing="0" width="98%">
+<tr>
+	<td><img src="./images/icons/calendar.gif" alt="" border="0"></td>
+	<td nowrap>
+		<span class="title"><?php echo $AppUI->_(($event_id > 0) ? "Edit Event" : "New Event" ); ?></span>
+	</td>
+	<td align="right" width="100%">&nbsp;</td>
+	<td nowrap="nowrap" width="20" align="right"><?php echo contextHelp( '<img src="./images/obj/help.gif" width="14" height="16" border="0" alt="'.$AppUI->_( 'Help' ).'">', 'ID_HELP_EVENT_EDIT' );?></td>
+</tr>
+</table>
+
+<table border="0" cellpadding="4" cellspacing="0" width="98%">
+<tr>
+	<td width="50%" nowrap><?php echo breadCrumbs( $crumbs );?></td>
+	<td width="50%" align="right">
+		<a href="javascript:delIt()"><img align="absmiddle" src="./images/icons/trash.gif" width="16" height="16" alt="" border="0"><?php echo $AppUI->_('delete event');?></a>
+	</td>
+</tr>
 </table>
 
 <table cellspacing="1" cellpadding="2" border="0" width="98%" class="std">
 <form name="changeevent" action="?m=calendar" method="post">
-<input type="hidden" name="dosql" value="addeditdel_event">
+<input type="hidden" name="dosql" value="event_aed">
 <input type="hidden" name="del" value="0">
-<input type="hidden" name="event_project" value="0">
 <input type="hidden" name="event_id" value="<?php echo $event_id;?>">
+<input type="hidden" name="event_project" value="0">
 
 <tr>
-	<th colspan="3"><?php if($event_id == 0){echo "Add";}else{echo "Edit";}?> event</th>
+	<td width="33%" align="right" nowrap="nowrap"><?php echo $AppUI->_( 'Event Title' );?>:</td>
+	<td width="20%">
+		<input type="text" class="text" size="25" name="event_title" value="<?php echo @$event["event_title"];?>" maxlength="255">
+	</td>
+	<td width="5%"></td>
 </tr>
 <tr>
-	<td valign="top">
-		<table cellspacing="1" cellpadding="1" border="0" width="360">
-		<tr>
-			<td align="right" nowrap>Event Name:</td>
-			<td>
-				<input type="text" class="text" size="25" name="event_title" value="<?php echo @$crow["event_title"];?>" maxlength="255">
-			</td>
-		</tr>
-		<tr>
-			<td align="right" nowrap>Make private:</td>
-			<td>
-				<input type="checkbox" value="1" name="event_private" <?php echo (@$crow["event_private"] ? 'checked' : '');?>>
-			</td>
-		</tr>
-		</table>
-		<img src="images/shim.gif" width=100 height=10 border=0><br>
-		<table border=0 cellpadding=1 cellspacing=1 bgcolor="silver" width=360>
-		<tr bgcolor="#eeeeee">
-			<td align="right">Start Date/Time:</td>
-			<td nowrap><input type="text" class="text" name="sdate" value="<?php echo @$sdate;?>" maxlength="10" size=12><a href="#" onClick="popCalendar('sdate')"><img src="./images/calendar.gif" width="24" height="12" alt="" border="0"></a></td>
-			<td>
-				<select name="stime">
-<?php
-	$hr=0;
-	$mn="00";
-	while ($hr < 24) {
-		echo "<option value='$hr:$mn'";
-		if ($hr == $shour && $mn == $smin)
-			echo " selected";
-		echo ">";
-		if ($hr == 0)
-			echo "12:$mn";
-		else
-			echo "$hr:$mn";
-		if ($hr < 12)
-			echo " am";
-		else
-		  echo " pm";
-		echo "</option>\n";
-		if ($mn == "00") {
-			$mn = "30";
-		} else {
-			$mn = "00";
-			$hr++;
-		}
-	}
-?>
-				</select>
-			</td>
-		</tr>
-		<tr bgcolor="#eeeeee">
-			<td align="right">End Date:</td>
-			<td nowrap><input type="text" class="text" name="edate" value="<?php echo @$edate;?>" maxlength="10" size=12><a href="#" onClick="popCalendar('edate')"><img src="./images/calendar.gif" width="24" height="12" alt="" border="0"></a></td>
-			<td>
-				<select name="etime">
-<?php
-	$hr=0;
-	$mn="00";
-	while ($hr < 24) {
-		echo "<option value='$hr:$mn'";
-		if ($hr == $ehour && $mn == $emin)
-			echo " selected";
-		echo ">";
-		if ($hr == 0)
-			echo "12:$mn";
-		else
-			echo "$hr:$mn";
-		if ($hr < 12)
-			echo " am";
-		else
-		  echo " pm";
-		echo "</option>\n";
-		if ($mn == "00") {
-			$mn = "30";
-		} else {
-			$mn = "00";
-			$hr++;
-		}
-	}
-?>
-				</select>
-			</td>
-		</tr>
-		<tr bgcolor="#eeeeee">
-			<td align="right">Recurs:</td>
-			<td>
-				<select name="event_recurs">
-					<option>Never
-					<option>Hourly
-					<option>Daily
-					<option>Weekly
-					<option>Bi-Weekly
-					<option>Every Month
-					<option>Quarterly
-					<option>Every 6 months
-					<option>Every Year
-				</select> x</td>
-			<td>
-				<input type="text"  name="event_times_recuring" value="<?php echo @$crow["event_times_recuring"];?>" maxlength="2" size=3> times
-			</td>
-		</tr>
-		</table>
-		<img src="images/shim.gif" width=100 height=10 border=0><br>
-		<table border=0 cellpadding=1 cellspacing=1 bgcolor="silver" width=360>
-		<tr bgcolor="#eeeeee">
-			<td>Remind me:</td>
-			<td><Select name="event_remind">
-				<option value="900">15 mins.
-				<option value="1800">30 mins.
-				<option value="3600">1 hour
-				<option value="7200">2 hours
-				<option value="14400">4 hours
-				<option value="28800">8 hours
-				<option value="56600">16 hours
-				<option value="86400">1 day
-				<option value="172800">2 days
-			
-			
-			</SELECT> in advance</td>
-		</tr>
-		</table>
-	</td>
-	<td valign="top">
-		<b>Description</b><br>
-		<textarea class="textarea" name="event_notes"><?php echo @$crow["event_description"];?></textarea></td>
+	<td align="right" nowrap="nowrap"><?php echo $AppUI->_( 'Make Private' );?>:</td>
+	<td>
+		<input type="checkbox" value="1" name="event_private" <?php echo (@$event["event_private"] ? 'checked' : '');?>>
 	</td>
 </tr>
 <tr>
-<td><input type="button" value="back" class=button onClick="javascript:window.location='./index.php?m=events';"></td>
-<td align="right"><input type="button" value="submit" class=button onClick="submitIt()"></td></tr>
+	<td align="right" nowrap="nowrap"><?php echo $AppUI->_( 'Start Date' );?>:</td>
+	<td nowrap="nowrap">
+		<input type="hidden" name="event_start_date" value="<?php echo $start_date ? $start_date->getTimestamp() : '-1';?>">
+		<input type="text" name="start_date" value="<?php echo $start_date ? $start_date->toString() : '';?>" class="text" disabled="disabled">
+		<a href="#" onClick="popCalendar('start_date')">
+			<img src="./images/calendar.gif" width="24" height="12" alt="<?php echo $AppUI->_('Calendar');?>" border="0">
+		</a>
+	</td>
+	<td align="right" nowrap="nowrap"><?php echo $AppUI->_( 'Time' );?>:</td>
+	<td><?php echo arraySelect( $times, 'start_time', 'size="1" class="text"', $start_time ); ?></td>
+</tr>
+
+<tr>
+	<td align="right" nowrap="nowrap"><?php echo $AppUI->_( 'End Date' );?>:</td>
+	<td nowrap="nowrap">
+		<input type="hidden" name="event_end_date" value="<?php echo $end_date ? $end_date->getTimestamp() : '-1';?>">
+		<input type="text" name="end_date" value="<?php echo $end_date ? $end_date->toString() : '';?>" class="text" disabled="disabled">
+		<a href="#" onClick="popCalendar('end_date')">
+			<img src="./images/calendar.gif" width="24" height="12" alt="<?php echo $AppUI->_('Calendar');?>" border="0">
+		</a>
+	</td>
+	<td align="right" nowrap="nowrap"><?php echo $AppUI->_( 'Time' );?>:</td>
+	<td><?php echo arraySelect( $times, 'end_time', 'size="1" class="text"', $end_time ); ?></td>
+</tr>
+<tr>
+	<td align="right" nowrap="nowrap"><?php echo $AppUI->_( 'Recurs' );?>:</td>
+	<td><?php echo arraySelect( $recurs, 'event_recurs', 'size="1" class="text"', $event['event_recurs'] ); ?></td>
+	<td align="right">x</td>
+	<td>
+		<input type="text"  name="event_times_recuring" value="<?php echo @$event["event_times_recuring"];?>" maxlength="2" size=3> <?php echo $AppUI->_( 'times' );?>
+	</td>
+</tr>
+<tr>
+	<td align="right" nowrap="nowrap"><?php echo $AppUI->_( 'Remind Me' );?>:</td>
+	<td><?php echo arraySelect( $remind, 'event_remind', 'size="1" class="text"', $event['event_remind'] ); ?> <?php echo $AppUI->_( 'in advance' );?></td>
+</tr>
+<tr>
+	<td valign="top" align="right"><?php echo $AppUI->_( 'Description' );?></td>
+	<td align="left" colspan="3">
+		<textarea class="textarea" name="event_description" rows="5" cols="45"><?php echo @$event["event_description"];?></textarea></td>
+	</td>
+</tr>
+<tr>
+	<td colspan="2">
+		<input type="button" value="<?php echo $AppUI->_( 'back' );?>" class="button" onClick="javascript:window.location='./index.php?m=events';">
+	</td>
+	<td align="right" colspan="2">
+		<input type="button" value="<?php echo $AppUI->_( 'submit' );?>" class="button" onClick="submitIt()">
+	</td>
+</tr>
 </form>
 </table>
