8ab8e0506d354a10e0ad524c21ec395ca02d5950
eddieajau <eddieajau>
2003-01-14 03:08:04 +0000

    Added session name Updated message handling

--- dotproject/logincheck.php
+++ dotproject/logincheck.php
@@ -3,6 +3,7 @@ require_once( "./includes/config.php" );
 require_once( "$root_dir/includes/db_connect.php" );
 require_once( "$root_dir/misc/debug.php" );
 require_once( "$root_dir/classdefs/ui.php" );
+session_name( 'dotproject' );
 session_start();
 session_register('AppUI');
 
@@ -16,7 +17,7 @@ $uistyle = $AppUI->getPref( 'UISTYLE' ) ? $AppUI->getPref( 'UISTYLE' ) : $host_s
 
 $ok = $AppUI->login( $username, $password );
 if (!$ok) {
-	$message = 'Login Failed';
+	$AppUI->setMsg( 'Login Failed' );
 	include "$root_dir/style/$uistyle/login.php";
 	die;
 }
