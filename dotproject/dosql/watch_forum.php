cfd0b92cdf889aa1870cf4e04800eeeb872d6110
eddieajau <eddieajau>
2003-01-14 00:42:45 +0000

    Updated message handling

--- dotproject/dosql/watch_forum.php
+++ dotproject/dosql/watch_forum.php
@@ -10,19 +10,21 @@ if ($watch) {
 	// clear existing watches
 	$sql = "DELETE FROM forum_watch WHERE watch_user = $AppUI->user_id AND watch_$watch IS NOT NULL";
 	if (!db_exec($sql)) {
-		$message = 'An error occurred cleaning up the watch table<br>'.db_error();
+		$AppUI->setMsg( db_error(), UI_MSG_ERROR );
 	} else {
 		$sql = '';
 		foreach ($_POST as $k => $v) {
 			if (strpos($k, 'forum_') !== FALSE) {
 				$sql = "INSERT INTO forum_watch (watch_user,watch_$watch) VALUES ($AppUI->user_id,".substr( $k, 6 ).")";
 				if (!db_exec($sql)) {
-					$message .= db_error().'<br>';
+					$AppUI->setMsg( db_error(), UI_MSG_ERROR );
+				} else {
+					$AppUI->setMsg( "Watch updated", UI_MSG_OK );
 				}
 			}
 		}
 	}
 } else {
-	$message = 'Incorrect watch type passed to sql handler.';
+	$AppUI->setMsg( 'Incorrect watch type passed to sql handler.', UI_MSG_ERROR );
 }
 ?>
