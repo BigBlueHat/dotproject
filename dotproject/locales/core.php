73992806bcb6ddbdc77e05d65914559d0044fce9
eddieajau <eddieajau>
2003-01-14 02:55:28 +0000

    Departments module now also includes company module strings

--- dotproject/locales/core.php
+++ dotproject/locales/core.php
@@ -2,6 +2,9 @@
 ob_start();
 	@readfile( "$root_dir/locales/$AppUI->user_locale/common.inc" );
 	@readfile( "$root_dir/locales/$AppUI->user_locale/$m.inc" );
+	if ($m == 'departments') {
+		@readfile( "$root_dir/locales/$AppUI->user_locale/companies.inc" );
+	}
 	eval( "\$GLOBALS['translate']=array(".ob_get_contents()."\n'0');" );
 ob_end_clean();
 ?>
