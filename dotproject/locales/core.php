5c5efe75f757855bef1bd0d0f2735b3d87556f1b
eddieajau <eddieajau>
2003-01-17 12:25:55 +0000

    Added inclusion of styles.inc when in system module

--- dotproject/locales/core.php
+++ dotproject/locales/core.php
@@ -2,8 +2,13 @@
 ob_start();
 	@readfile( "$root_dir/locales/$AppUI->user_locale/common.inc" );
 	@readfile( "$root_dir/locales/$AppUI->user_locale/$m.inc" );
-	if ($m == 'departments') {
+	switch ($m) {
+	case 'departments':
 		@readfile( "$root_dir/locales/$AppUI->user_locale/companies.inc" );
+		break;
+	case 'system':
+		@readfile( "$root_dir/locales/$host_locale/styles.inc" );
+		break;
 	}
 	eval( "\$GLOBALS['translate']=array(".ob_get_contents()."\n'0');" );
 ob_end_clean();
