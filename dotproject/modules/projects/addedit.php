edf601d2ed27d8c853513cd3bb70975f2ffd48b6
kobudo <kobudo>
2003-02-14 21:11:37 +0000

    Sweep and upgrade for XHTML compliance: <input> changed to <input /> <img> changed to <img />
    All attributes quoted and alt= attributes added to <img /> tags.

--- dotproject/modules/projects/addedit.php
+++ dotproject/modules/projects/addedit.php
@@ -108,17 +108,17 @@ function delIt() {
 
 <table cellspacing="0" cellpadding="0" border="0" width="98%">
 <form name="AddEdit" action="./index.php?m=projects&a=dosql" method="post">
-<input type="hidden" name="del" value="0">
-<input type="hidden" name="project_id" value="<?php echo $project_id;?>">
-<input type="hidden" name="project_creator" value="<?php echo $AppUI->user_id;?>">
+<input type="hidden" name="del" value="0" />
+<input type="hidden" name="project_id" value="<?php echo $project_id;?>" />
+<input type="hidden" name="project_creator" value="<?php echo $AppUI->user_id;?>" />
 
 <tr>
-	<td><img src="./images/icons/projects.gif" alt="" border="0"></td>
+	<td><img src="./images/icons/projects.gif" alt="" border="0" /></td>
 	<td nowrap>
 		<h1><?php echo $AppUI->_(($project_id > 0) ? "Edit Project" : "New Project" ); ?></h1>
 	</td>
 	<td align="right" width="100%">&nbsp;</td>
-	<td nowrap="nowrap" width="20" align="right"><?php echo contextHelp( '<img src="./images/obj/help.gif" width="14" height="16" border="0" alt="'.$AppUI->_( 'Help' ).'">', 'ID_HELP_PROJ_EDIT' );?></td>
+	<td nowrap="nowrap" width="20" align="right"><?php echo contextHelp( '<img src="./images/obj/help.gif" width="14" height="16" border="0" alt="'.$AppUI->_( 'Help' ).'" />', 'ID_HELP_PROJ_EDIT' );?></td>
 </tr>
 </table>
 
@@ -126,7 +126,7 @@ function delIt() {
 <tr>
 	<td width="50%" nowrap><?php echo breadCrumbs( $crumbs );?></td>
 	<td width="50%" align="right">
-		<a href="javascript:delIt()"><img align="absmiddle" src="./images/icons/trash.gif" width="16" height="16" alt="" border="0"><?php echo $AppUI->_('delete project');?></a>
+		<a href="javascript:delIt()"><img align="absmiddle" src="./images/icons/trash.gif" width="16" height="16" alt="" border="0" /><?php echo $AppUI->_('delete project');?></a>
 	</td>
 </tr>
 </table>
@@ -138,7 +138,7 @@ function delIt() {
 		<tr>
 			<td align="right" nowrap="nowrap"><?php echo $AppUI->_('Project Name');?></td>
 			<td width="100%">
-				<input type="text" name="project_name" value="<?php echo @$project["project_name"];?>" size="25" maxlength="50" onBlur="setShort();" class="text">
+				<input type="text" name="project_name" value="<?php echo @$project["project_name"];?>" size="25" maxlength="50" onBlur="setShort();" class="text" />
 			</td>
 		</tr>
 		<tr>
@@ -152,33 +152,32 @@ function delIt() {
 			<td width="100%" nowrap="nowrap">
 <?php
 	echo arraySelect( $companies, 'project_company', 'class="text" size="1"', $project["project_company"] );
-?> *
-			</td>
+?> *</td>
 		</tr>
 		<tr>
 			<td align="right" nowrap="nowrap"><?php echo $AppUI->_('Start Date');?></td>
 			<td>
-				<input type="hidden" name="project_start_date" value="<?php echo $start_date->getTimestamp();?>">
-				<input type="text" name="start_date" value="<?php echo $start_date->toString();?>" class="text" disabled="disabled">
+				<input type="hidden" name="project_start_date" value="<?php echo $start_date->getTimestamp();?>" />
+				<input type="text" name="start_date" value="<?php echo $start_date->toString();?>" class="text" disabled="disabled" />
 				<a href="#" onClick="popCalendar('start_date')">
-					<img src="./images/calendar.gif" width="24" height="12" alt="<?php echo $AppUI->_('Calendar');?>" border="0">
+					<img src="./images/calendar.gif" width="24" height="12" alt="<?php echo $AppUI->_('Calendar');?>" border="0" />
 				</a>
 			</td>
 		</tr>
 		<tr>
 			<td align="right" nowrap="nowrap"><?php echo $AppUI->_('Target Finish Date');?></td>
 			<td>
-				<input type="hidden" name="project_end_date" value="<?php echo $end_date ? $end_date->getTimestamp() : '-1';?>">
-				<input type="text" name="end_date" value="<?php echo $end_date ? $end_date->toString() : '';?>" class="text" disabled="disabled">
+				<input type="hidden" name="project_end_date" value="<?php echo $end_date ? $end_date->getTimestamp() : '-1';?>" />
+				<input type="text" name="end_date" value="<?php echo $end_date ? $end_date->toString() : '';?>" class="text" disabled="disabled" />
 				<a href="#" onClick="popCalendar('end_date')">
-					<img src="./images/calendar.gif" width="24" height="12" alt="<?php echo $AppUI->_('Calendar');?>" border="0">
+					<img src="./images/calendar.gif" width="24" height="12" alt="<?php echo $AppUI->_('Calendar');?>" border="0" />
 				</a>
 			</td>
 		</tr>
 		<tr>
 			<td align="right" nowrap="nowrap"><?php echo $AppUI->_('Target Budget');?> $</td>
 			<td>
-				<input type="Text" name="project_target_budget" value="<?php echo @$project["project_target_budget"];?>" maxlength="10" class="text">
+				<input type="Text" name="project_target_budget" value="<?php echo @$project["project_target_budget"];?>" maxlength="10" class="text" />
 			</td>
 		</tr>
 		<tr>
@@ -187,17 +186,17 @@ function delIt() {
 		<tr>
 			<td align="right" nowrap="nowrap"><?php echo $AppUI->_('Actual Finish Date');?></td>
 			<td>
-				<input type="hidden" name="project_actual_end_date" value="<?php echo $actual_end_date ? $actual_end_date->getTimestamp() : '-1';?>">
-				<input type="text" name="actual_end_date" value="<?php echo $actual_end_date ? $actual_end_date->toString() : '';?>" class="text" disabled="disabled">
+				<input type="hidden" name="project_actual_end_date" value="<?php echo $actual_end_date ? $actual_end_date->getTimestamp() : '-1';?>" />
+				<input type="text" name="actual_end_date" value="<?php echo $actual_end_date ? $actual_end_date->toString() : '';?>" class="text" disabled="disabled" />
 				<a href="#" onClick="popCalendar('project_actual_end_date','actual_end_date')">
-					<img src="./images/calendar.gif" width="24" height="12" alt="<?php echo $AppUI->_('Calendar');?>" border="0">
+					<img src="./images/calendar.gif" width="24" height="12" alt="<?php echo $AppUI->_('Calendar');?>" border="0" />
 				</a>
 			</td>
 		</tr>
 		<tr>
 			<td align="right" nowrap="nowrap"><?php echo $AppUI->_('Actual Budget');?> $</td>
 			<td>
-				<input type="text" name="project_actual_budget" value="<?php echo @$project["project_actual_budget"];?>" size="10" maxlength="10" class="text">
+				<input type="text" name="project_actual_budget" value="<?php echo @$project["project_actual_budget"];?>" size="10" maxlength="10" class="text"/>
 			</td>
 		</tr>
 		<tr>
@@ -206,13 +205,13 @@ function delIt() {
 		<tr>
 			<td align="right" nowrap="nowrap"><?php echo $AppUI->_('URL');?></td>
 			<td>
-				<input type="text" name="project_url" value="<?php echo @$project["project_url"];?>" size="40" maxlength="255" class="text">
+				<input type="text" name="project_url" value="<?php echo @$project["project_url"];?>" size="40" maxlength="255" class="text" />
 			</td>
 		</tr>
 		<tr>
 			<td align="right" nowrap="nowrap"><?php echo $AppUI->_('Staging URL');?></td>
 			<td>
-				<input type="Text" name="project_demo_url" value="<?php echo @$project["project_demo_url"];?>" size="40" maxlength="255" class="text">
+				<input type="Text" name="project_demo_url" value="<?php echo @$project["project_demo_url"];?>" size="40" maxlength="255" class="text" />
 			</td>
 		</tr>
 		</table>
@@ -222,19 +221,19 @@ function delIt() {
 		<tr>
 			<td align="right" nowrap="nowrap"><?php echo $AppUI->_('Short Name');?></td>
 			<td colspan="3">
-				<input type="text" name="project_short_name" value="<?php echo @$project["project_short_name"];?>" size="10" maxlength="10" class="text"> *
+				<input type="text" name="project_short_name" value="<?php echo @$project["project_short_name"];?>" size="10" maxlength="10" class="text" /> *
 			</td>
 		</tr>
 		<tr>
 			<td align="right" nowrap="nowrap"><?php echo $AppUI->_('Color Identifier');?></td>
 			<td nowrap="nowrap">
-				<input type="text" name="project_color_identifier" value="<?php echo @$project["project_color_identifier"];?>" size="10" maxlength="6" onBlur="setColor();" class="text"> *
+				<input type="text" name="project_color_identifier" value="<?php echo @$project["project_color_identifier"];?>" size="10" maxlength="6" onBlur="setColor();" class="text" /> *
 			</td>
 			<td nowrap="nowrap">
 				<a href="#" onClick="newwin=window.open('./color_selector.php', 'calwin', 'width=320, height=300, scollbars=false');"><?php echo $AppUI->_('change color');?></a>
 			</td>
 			<td nowrap="nowrap">
-				<span id="test" title="test" style="background:#<?php echo @$project["project_color_identifier"];?>;"><a href="#" onClick="newwin=window.open('./color_selector.php', 'calwin', 'width=320, height=300, scollbars=false');"><img src="./images/shim.gif" border=1 width="40" height="20"></a></span>
+				<span id="test" title="test" style="background:#<?php echo @$project["project_color_identifier"];?>;"><a href="#" onClick="newwin=window.open('./color_selector.php', 'calwin', 'width=320, height=300, scollbars=false');"><img src="./images/shim.gif" border="1" width="40" height="20" /></a></span>
 			</td>
 		</tr>
 		<tr>
@@ -272,10 +271,10 @@ function delIt() {
 </tr>
 <tr>
 	<td>
-		<input class=button type="button" name="<?php echo $AppUI->_('cancel');?>" value="cancel" onClick="javascript:if(confirm('Are you sure you want to cancel.')){location.href = './index.php?m=projects';}">
+		<input class=button type="button" name="<?php echo $AppUI->_('cancel');?>" value="cancel" onClick="javascript:if(confirm('Are you sure you want to cancel.')){location.href = './index.php?m=projects';}" />
 	</td>
 	<td align="right" colspan="2">
-		<input class=button type="Button" name="btnFuseAction" value="<?php echo $AppUI->_('submit');?>" onClick="submitIt();">
+		<input class=button type="Button" name="btnFuseAction" value="<?php echo $AppUI->_('submit');?>" onClick="submitIt();" />
 	</td>
 </tr>
 </form>
