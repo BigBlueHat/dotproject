c4d7b692c57651c14d3f5227b94d9dfca0275420
kobudo <kobudo>
2003-02-14 20:11:09 +0000

    John creates a "config-dist.php" file to replace our config.php file.
    This will help reduce opportunity for overwriting config.php files on
    dotproject upgrades, and make it easier for our development team to
    checkout dotproject (and check in) without killing local connections.

---
+++ dotproject/includes/config-dist.php
@@ -0,0 +1,43 @@
+<?php /* $Id$ */
+//Config File
+
+//db access information [DEFAULT example]
+$dPconfig['dbtype'] = "mysql";
+$dPconfig['dbhost'] = "localhost";
+$dPconfig['dbname'] = "dotproject";
+$dPconfig['dbuser'] = "root";
+$dPconfig['dbpass'] = "";
+
+
+/*
+ Localisation of the host for this dotproject,
+ that is, what language will the login screen be in.
+*/
+$dPconfig['host_locale'] = "en";
+
+// default user interface style
+$dPconfig['host_style'] = "default";
+
+// local settings [DEFAULT example WINDOWS]
+$dPconfig['root_dir'] = "C:/apache/htdocs/dotproject";
+$dPconfig['company_name'] = "My Company";
+$dPconfig['page_title'] = "DotProject";
+$dPconfig['base_url'] = "http://localhost/dotproject";
+$dPconfig['site_domain'] = "dotproject.net";
+
+// enable if you want to be able to see other users's tasks
+$dPconfig['show_all_tasks'] = false;
+// enable if you want to support gantt charts
+$dPconfig['enable_gantt_charts'] = true;
+
+$dPconfig['daily_working_hours'] = 8.0;
+
+// set debug = true to help analyse errors
+$dPconfig['debug'] = false;
+
+//File parsers to return indexing information about uploaded files
+$ft["default"] = "/usr/bin/strings";
+$ft["application/msword"] = "/usr/bin/strings";
+$ft["text/html"] = "/usr/bin/strings";
+$ft["application/pdf"] = "/usr/bin/pdftotext";
+?>
