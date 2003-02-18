fb553135a58ab4987504d0b3267302c801e758e8
eddieajau <eddieajau>
2003-02-18 23:18:30 +0000

    Merged into ./index.php

--- dotproject/logincheck.php
+++
@@ -1,9 +0,0 @@
-<?php /* $Id$ */
-	$username = isset($_POST['username']) ? $_POST['username'] : '';
-	$password = isset($_POST['password']) ? $_POST['password'] : '';
-	$ok = $AppUI->login( $username, $password );
-	if (!$ok) {
-		$AppUI->setMsg( 'Login Failed' );
-		$AppUI->redirect();
-	}
-?>
