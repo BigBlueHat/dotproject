22e24459960f6543c3a6a7567f33752e2f4ed86b
eddieajau <eddieajau>
2003-02-19 23:36:00 +0000

    Fixed wrong field being returned in getReadbaleModule function

--- dotproject/includes/permissions.php
+++ dotproject/includes/permissions.php
@@ -7,11 +7,11 @@ define( 'PERM_EDIT', '-1' );
 define( 'PERM_ALL', '-1' );
 
 function getReadableModule() {
-	$sql = "SELECT mod_name FROM modules WHERE mod_active > 0 ORDER BY mod_ui_order";
+	$sql = "SELECT mod_directory FROM modules WHERE mod_active > 0 ORDER BY mod_ui_order";
 	$modules = db_loadColumn( $sql );
 	foreach ($modules as $mod) {
 		if (!getDenyRead($mod)) {
-			return strtolower($mod);
+			return $mod;
 		}
 	}
 	return null;
