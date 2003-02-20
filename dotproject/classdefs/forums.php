e1bd38218c70bf83040b94cc4f4d0f71287f45b5
kripper <kripper>
2003-02-20 22:53:01 +0000

    Avoid erronous log when adding messages to forum

--- dotproject/classdefs/forums.php
+++ dotproject/classdefs/forums.php
@@ -44,7 +44,10 @@ class CForum {
 		}
 		if( $this->forum_id ) {
 			$ret = db_updateObject( 'forums', $this, 'forum_id', false ); // ! Don't update null values
-			addHistory("Updated forum '" . $this->forum_name . "'");			
+			if(!$this->forum_name) {
+				// when adding messages, this functon is called without first setting 'forum_name'
+				addHistory("Updated forum '" . $this->forum_name . "'");
+			}
 		} else {
 			$this->forum_create_date = db_datetime( time() );
 			$ret = db_insertObject( 'forums', $this, 'forum_id' );
