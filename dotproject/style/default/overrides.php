d1e7ff0aaa3926e36a4ff425676c573b988aaf44
kripper <kripper>
2003-02-22 01:21:50 +0000

    Fixed problem caused by table width, when inserting big images (like the gantt chart) into the TabBox widget.

--- dotproject/style/default/overrides.php
+++ dotproject/style/default/overrides.php
@@ -41,17 +41,18 @@ class CTabBox extends CTabBox_core {
 			echo "</table>\n";
 		} else {
 		// tabbed view
-			$s = '<table width="100%" border="0" cellpadding="0" cellspacing="0"><tr>';
+			$s = '<table width="100%" border="0" cellpadding="0" cellspacing="0">';
+			$s .= '<tr><td><table border="0" cellpadding="0" cellspacing="0">';
 			foreach( $this->tabs as $k => $v ) {
 				$class = ($k == $this->active) ? 'tabon' : 'taboff';
 				$sel = ($k == $this->active) ? 'Selected' : '';
 				$s .= '<td height="28" valign="middle" width="3"><img src="./style/' . $uistyle . '/images/tab'.$sel.'Left.png" width="3" height="28" border="0" alt="" /></td>';
-				$s .= '<td valign="middle" width="1%" nowrap="nowrap"  background="./style/' . $uistyle . '/images/tab'.$sel.'Bg.png">&nbsp;<a href="'.$this->baseHRef.'tab='.$k.'">'.$AppUI->_($v[1]).'</a>&nbsp;</td>';
+				$s .= '<td valign="middle" nowrap="nowrap"  background="./style/' . $uistyle . '/images/tab'.$sel.'Bg.png">&nbsp;<a href="'.$this->baseHRef.'tab='.$k.'">'.$AppUI->_($v[1]).'</a>&nbsp;</td>';
 				$s .= '<td valign="middle" width="3"><img src="./style/' . $uistyle . '/images/tab'.$sel.'Right.png" width="3" height="28" border="0" alt="" /></td>';
 				$s .= '<td width="3" class="tabsp"><img src="./images/shim.gif" height="1" width="3" /></td>';
 			}
-			$s .= '<td width="100%" nowrap="nowrap" class="tabsp">&nbsp;</td>';
-			$s .= '</tr><tr><td width="100%" colspan="'.(count($this->tabs)*4 + 1).'" class="tabox">';
+			$s .= '</table></td></tr>';
+			$s .= '<tr><td width="100%" colspan="'.(count($this->tabs)*4 + 1).'" class="tabox">';
 			echo $s;
 			require $this->baseInc.$this->tabs[$this->active][0].'.php';
 			echo '</td></tr></table>';
