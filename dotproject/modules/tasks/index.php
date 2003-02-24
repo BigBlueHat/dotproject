329e13a5d28e08305ca8f64f5443bfd86a06e405
eddieajau <eddieajau>
2003-02-24 00:43:52 +0000

    Updated titleblock

--- dotproject/modules/tasks/index.php
+++ dotproject/modules/tasks/index.php
@@ -1,20 +1,9 @@
-<?php
-##
-## Task index - standalone wrapper
-##
-
-// check permissions
-$denyRead = getDenyRead( $m );
-$denyEdit = getDenyEdit( $m );
-
-if ($denyRead) {
-	$AppUI->redirect( 'm=help&a=access_denied' );
-}
+<?php /* TASKS $Id$ */
 $AppUI->savePlace();
 
 // act on passed parameters
-if (isset( $_GET['f'] )) {
-	$AppUI->setState( 'TaskIdxFilter', $_GET['f'] );
+if (isset( $_POST['f'] )) {
+	$AppUI->setState( 'TaskIdxFilter', $_POST['f'] );
 }
 $f = $AppUI->getState( 'TaskIdxFilter' ) ? $AppUI->getState( 'TaskIdxFilter' ) : 'my';
 
@@ -23,25 +12,25 @@ if (isset( $_GET['project_id'] )) {
 }
 $project_id = $AppUI->getState( 'TaskIdxProject' ) ? $AppUI->getState( 'TaskIdxProject' ) : 0;
 $AppUI->setState( 'ActiveProject', $project_id );
-?>
 
-<table width="98%" border="0" cellpadding="0" cellspacing=1>
-<tr>
-	<td><img src="./images/icons/tasks.gif" alt="Tasks" border="0" width="44" height="38"></td>
-	<td nowrap width="100%"><h1><?php echo $AppUI->_('Project Tasks');?></h1></td>
-<form name="task_filter" method=GET action="./index.php">
-<input type=hidden name=m value=tasks>
-	<td nowrap align=right>
-<?php
-	echo arraySelect( $filters, 'f', 'size=1 class=text onChange="document.task_filter.submit();"', $f, true );
-?>
-	</td>
-	<td nowrap="nowrap" width="20" align="right"><?php echo contextHelp( '<img src="./images/obj/help.gif" width="14" height="16" border="0" alt="'.$AppUI->_( 'Help' ).'">', 'ID_HELP_TASK_IDX' );?></td>
-</form>
-</tr>
-</table>
 
-<?php
+// setup the title block
+$titleBlock = new CTitleBlock( 'Tasks', 'tasks.gif', $m, 'ID_HELP_TASK_IDX' );
+$titleBlock->addCell( $AppUI->_('Filter') . ':' );
+$titleBlock->addCell(
+	arraySelect( $filters, 'f', 'size=1 class=text onChange="document.taskFilter.submit();"', $f, true ), '',
+	'<form action="?m=tasks" method="post" name="taskFilter">', '</form>'
+);
+$titleBlock->addCell();
+if ($canEdit && $project_id) {
+	$titleBlock->addCell(
+		'<input type="submit" class="button" value="'.$AppUI->_('new task').'">', '',
+		'<form action="?m=tasks&a=addedit&project_id=' . $project_id . '" method="post">', '</form>'
+	);
+}
+$titleBlock->addCrumb( "?m=tasks&a=todo", "my todo" );
+$titleBlock->show();
+
 // include the re-usable sub view
 	$min_view = false;
 	include("{$AppUI->cfg['root_dir']}/modules/tasks/tasks.php");
