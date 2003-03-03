3628e7c850c8a4df1c1b40e2ed81a2a45879d4a4
eddieajau <eddieajau>
2003-03-03 22:44:19 +0000

    Renamed file

---
+++ dotproject/modules/departments/do_dept_aed.php
@@ -0,0 +1,24 @@
+<?php /* DEPARTMENTS $Id$ */
+$del = isset($_POST['del']) ? $_POST['del'] : 0;
+
+$dept = new CDepartment();
+if (($msg = $dept->bind( $_POST ))) {
+	$AppUI->setMsg( $msg, UI_MSG_ERROR );
+	$AppUI->redirect();
+}
+if ($del) {
+	if (($msg = $dept->delete())) {
+		$AppUI->setMsg( $msg, UI_MSG_ERROR );
+	} else {
+		$AppUI->setMsg( "Department deleted", UI_MSG_ALERT );
+	}
+} else {
+	if (($msg = $dept->store())) {
+		$AppUI->setMsg( $msg, UI_MSG_ERROR );
+	} else {
+		$isNotNew = @$_POST['department_id'];
+		$AppUI->setMsg( "Department ".($isNotNew ? 'updated' : 'inserted'), UI_MSG_OK );
+	}
+}
+$AppUI->redirect();
+?>
