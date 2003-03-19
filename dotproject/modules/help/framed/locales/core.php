cbb189e55ec2c58a19572b41609ee113e402bf2b
eddieajau <eddieajau>
2003-03-19 06:22:03 +0000

    Scuttled dotHelp system

--- dotproject/modules/help/framed/locales/core.php
+++
@@ -1,7 +0,0 @@
-<?php
-ob_start();
-	@readfile( "{$AppUI->cfg['root_dir']}/locales/$AppUI->user_locale/common.inc" );
-	@readfile( "{$AppUI->cfg['root_dir']}/locales/$AppUI->user_locale/$m.inc" );
-	eval( "\$GLOBALS['translate']=array(".ob_get_contents()."\n'0');" );
-ob_end_clean();
-?>
