b1d2e161b124cd916d8c86623338d5af24bca1a9
eddieajau <eddieajau>
2003-03-22 00:49:46 +0000

    Initial upload

---
+++ dotproject/modules/ticketsmith/do_ticket_aed.php
@@ -0,0 +1,28 @@
+<?php
+##
+##	Ticketsmith sql handler
+##
+
+$name = isset($HTTP_POST_VARS['name']) ? $HTTP_POST_VARS['name'] : '';
+$email = isset($HTTP_POST_VARS['email']) ? $HTTP_POST_VARS['email'] : '';
+$subject = isset($HTTP_POST_VARS['subject']) ? $HTTP_POST_VARS['subject'] : '';
+$priority = isset($HTTP_POST_VARS['priority']) ? $HTTP_POST_VARS['priority'] : '';
+$description = isset($HTTP_POST_VARS['problem']) ? $HTTP_POST_VARS['problem'] : '';
+
+$description = db_escape($description);
+
+$author = $name . " <" . $email . ">";
+$tsql =
+"INSERT INTO tickets (author,subject,priority,body,timestamp,type) ".
+"VALUES('$author','$subject','$priority','$description',UNIX_TIMESTAMP(),'Open')";
+
+$rc = mysql_query($tsql);
+
+if (!mysql_errno()) {
+	$AppUI->setMsg( mysql_error() );
+	// add code to mail to ticket master
+} else {
+	$AppUI->setMsg( "Ticket added" );
+}
+$AppUI->redirect( "m=ticketsmith" );
+?>
