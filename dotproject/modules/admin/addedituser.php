ec0a7c31af425004dd25c95de6dd763f844ab394
eddieajau <eddieajau>
2003-04-04 05:06:07 +0000

    Fixed broken bread-crumb and name of dosql file

--- dotproject/modules/admin/addedituser.php
+++ dotproject/modules/admin/addedituser.php
@@ -32,7 +32,7 @@ if (!db_loadHash( $sql, $user ) && $user_id > 0) {
 	$titleBlock = new CTitleBlock( $ttl, 'admin.gif', $m, 'ID_HELP_USER_EDIT' );
 	$titleBlock->addCrumb( "?m=admin", "users list" );
 	$titleBlock->addCrumb( "?m=admin&a=viewuser&user_id=$user_id", "view this user" );
-	$titleBlock->addCrumb( "?m=admin&a=permissions&user_id=$user_id", "edit preferences" );
+	$titleBlock->addCrumb( "?m=system&a=addeditpref&user_id=$user_id", "edit preferences" );
 	$titleBlock->show();
 ?>
 <SCRIPT language="javascript">
@@ -110,8 +110,9 @@ function setDept( key, val ) {
 </script>
 
 <table width="100%" border="0" cellpadding="0" cellspacing="1" height="400" class="std">
-<form name="editFrm" action="./index.php?m=admin&a=dosql" method="post">
+<form name="editFrm" action="./index.php?m=admin" method="post">
 	<input type="hidden" name="user_id" value="<?php echo intval($user["user_id"]);?>" />
+	<input type="hidden" name="dosql" value="do_user_aed" />
 
 <tr>
     <td align="right" width="230"><?php echo $AppUI->_('Login Name');?>:</td>
