83407f8a3dc08215dc0f6a511c0d88e0d45d22a6
eddieajau <eddieajau>
2003-04-09 12:25:01 +0000

    Fixed bugs preventing proper update of file information

--- dotproject/modules/files/do_file_aed.php
+++ dotproject/modules/files/do_file_aed.php
@@ -1,7 +1,7 @@
 <?php
 //addfile sql
-$del = isset($_POST['del']) ? $_POST['del'] : 0;
-
+$file_id = dPgetParam( $_POST, 'file_id', 0 );
+$del = dPgetParam( $_POST, 'del', 0 );
 $file = new CFile();
 
 if (($msg = $file->bind( $_POST ))) {
@@ -26,33 +26,39 @@ if ($del) {
 set_time_limit( 600 );
 ignore_user_abort( 1 );
 
+//echo "<pre>";print_r($_POST);echo "</pre>";die;
+
 $upload = null;
 if (isset( $_FILES['formfile'] )) {
 	$upload = $_FILES['formfile'];
 
 	if ($upload['size'] < 1) {
-		$AppUI->setMsg( 'Upload file size is zero.  Process aborted.', UI_MSG_ERROR );
-		$AppUI->redirect();
+		if (!$file_id) {
+			$AppUI->setMsg( 'Upload file size is zero.  Process aborted.', UI_MSG_ERROR );
+			$AppUI->redirect();
+		}
+	} else {
+
+	// store file with a unique name
+		$file->file_name = $upload['name'];
+		$file->file_type = $upload['type'];
+		$file->file_size = $upload['size'];
+		$file->file_date = db_unix2dateTime( time() );
+		$file->file_real_filename = uniqid( rand() );
+
+		$file->moveTemp( $upload );
+		$file->indexStrings();
 	}
-
-// store file with a unique name
-	$file->file_name = $upload['name'];
-	$file->file_type = $upload['type'];
-	$file->file_size = $upload['size'];
-	$file->file_date = db_unix2dateTime( time() );
-	$file->file_real_filename = uniqid( rand() );
-
-	$file->moveTemp( $upload );
-	$file->indexStrings();
 }
 
-$file->file_owner = $AppUI->user_id;
+if (!$file_id) {
+	$file->file_owner = $AppUI->user_id;
+}
 
 if (($msg = $file->store())) {
 	$AppUI->setMsg( $msg, UI_MSG_ERROR );
 } else {
-	$isNotNew = @$_POST['file_id'];
-	$AppUI->setMsg( $isNotNew ? 'updated' : 'added', UI_MSG_OK, true );
+	$AppUI->setMsg( $file_id ? 'updated' : 'added', UI_MSG_OK, true );
 }
 $AppUI->redirect();
 ?>
