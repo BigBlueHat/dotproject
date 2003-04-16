24006ddb7780037eb73e1f3839775c835a129df0
eddieajau <eddieajau>
2003-04-16 04:41:05 +0000

    Fixed percent complete calculation for new task duration type

--- dotproject/modules/projects/view.php
+++ dotproject/modules/projects/view.php
@@ -23,7 +23,7 @@ SELECT
 	company_name,
 	CONCAT(user_first_name, ' ', user_last_name) user_name,
 	projects.*,
-	SUM(t1.task_duration*t1.task_percent_complete)/SUM(t1.task_duration) AS project_percent_complete
+	SUM(t1.task_duration*t1.task_duration_type*t1.task_percent_complete)/SUM(t1.task_duration*t1.task_duration_type) AS project_percent_complete
 FROM projects
 LEFT JOIN companies ON company_id = project_company
 LEFT JOIN users ON user_id = project_owner
