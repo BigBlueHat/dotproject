dacdedd3ebe2df95ad930017d2672c677c9148b2
eddieajau <eddieajau>
2003-04-16 04:54:56 +0000

    Updated message handling

--- dotproject/modules/system/syskeys/do_syskey_aed.php
+++ dotproject/modules/system/syskeys/do_syskey_aed.php
@@ -8,18 +8,18 @@ if (($msg = $syskey->bind( $_POST ))) {
 	$AppUI->redirect();
 }
 
+$AppUI->setMsg( "System Lookup Keys", UI_MSG_ALERT );
 if ($del) {
 	if (($msg = $syskey->delete())) {
 		$AppUI->setMsg( $msg, UI_MSG_ERROR );
 	} else {
-		$AppUI->setMsg( "System lookup key deleted", UI_MSG_ALERT );
+		$AppUI->setMsg( "deleted", UI_MSG_ALERT, true );
 	}
 } else {
 	if (($msg = $syskey->store())) {
 		$AppUI->setMsg( $msg, UI_MSG_ERROR );
 	} else {
-		$isNotNew = @$_POST['syskey_id'];
-		$AppUI->setMsg( "System lookup key ".($isNotNew ? 'updated' : 'inserted'), UI_MSG_OK );
+		$AppUI->setMsg( @$_POST['syskey_id'] ? 'updated' : 'inserted', UI_MSG_OK, true );
 	}
 }
 $AppUI->redirect( "m=system&u=syskeys&a=keys" );
--- dotproject/modules/system/syskeys/do_sysval_aed.php
+++ dotproject/modules/system/syskeys/do_sysval_aed.php
@@ -8,18 +8,18 @@ if (($msg = $sysval->bind( $_POST ))) {
 	$AppUI->redirect();
 }
 
+$AppUI->setMsg( "System Lookup Values", UI_MSG_ALERT );
 if ($del) {
 	if (($msg = $sysval->delete())) {
 		$AppUI->setMsg( $msg, UI_MSG_ERROR );
 	} else {
-		$AppUI->setMsg( "System lookup value deleted", UI_MSG_ALERT );
+		$AppUI->setMsg( "deleted", UI_MSG_ALERT, true );
 	}
 } else {
 	if (($msg = $sysval->store())) {
 		$AppUI->setMsg( $msg, UI_MSG_ERROR );
 	} else {
-		$isNotNew = @$_POST['sysval_id'];
-		$AppUI->setMsg( "System lookup value ".($isNotNew ? 'updated' : 'inserted'), UI_MSG_OK );
+		$AppUI->setMsg( @$_POST['sysval_id'] ? 'updated' : 'inserted', UI_MSG_OK, true );
 	}
 }
 $AppUI->redirect( "m=system&u=syskeys" );
