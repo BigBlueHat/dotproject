96207000b32766a868dd8c953111af152be3ff2d
eddieajau <eddieajau>
2003-04-27 03:18:39 +0000

    Added check for mysql support

--- dotproject/style/default/login.php
+++ dotproject/style/default/login.php
@@ -40,6 +40,7 @@
 	$msg = '';
 	$msg .= ini_get( 'register_globals') ? '' : '<br /><span class="warning">WARNING: dotproject has not been fully tested with register_globals=off</span>';
 	$msg .=  phpversion() < '4.1' ? '<br /><span class="warning">WARNING: dotproject is NOT SUPPORT for this PHP Version ('.phpversion().')</span>' : '';
+	$msg .= function_exists( 'mysql_pconnect' ) ? '': '<br /><span class="warning">WARNING: PHP may not be compiled with MySQL support.  This will prevent proper operation of dotProject.  Please check you system setup.</span>';
 	echo $msg;
 ?>
 </div>
