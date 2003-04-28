29ddf31af9d00294ca9d41b8792609515030c504
eddieajau <eddieajau>
2003-04-28 04:36:57 +0000

    Reworked to use PEAR::Date class and supporting functions

--- dotproject/modules/calendar/calendar.class.php
+++ dotproject/modules/calendar/calendar.class.php
@@ -3,6 +3,12 @@
 ## Calendar classes
 ##
 
+require_once( $AppUI->getPearClass( 'Date' ) );
+require_once( $AppUI->getSystemClass ('dp' ) );
+
+/**
+ *
+ */
 class CMonthCalendar {
 	var $this_month;
 	var $prev_month;
@@ -27,10 +33,9 @@ class CMonthCalendar {
 	var $dayFunc;
 	var $weekFunc;
 
-	function CMonthCalendar( $dateObj=null ) {
-		if ($dateObj) {
-			$this->setDate( $dateObj );
-		}
+	function CMonthCalendar( $date=null ) {
+		$this->setDate( $date );
+
 		$this->classes = array();
 		$this->callback = '';
 		$this->showTitle = true;
@@ -48,20 +53,30 @@ class CMonthCalendar {
 		$this->events = array();
 	}
 // setting functions
-	function setDate( $dateObj ) {
-		$this->this_month = $dateObj;
+	function setDate( $date=null ) {
+		if (is_object($date) && (get_class($date) == 'date')) {
+			$this->this_month = new Date( $date );
+		} else {
+			$this->this_month = new Date( $date ? "{$date}000000" : $date );
+		}
 
-		$this->prev_month = $this->this_month;
-		$this->prev_month->addMonths( -1 );
+		$d = $this->this_month->getDay();
+		$m = $this->this_month->getMonth();
+		$y = $this->this_month->getYear();
 
-		$this->next_month = $this->this_month;
-		$this->next_month->addMonths( +1 );
+		//$date = Date_Calc::beginOfPrevMonth( $d, $m, $y-1, FORMAT_ISO );
+		$this->prev_year = new Date( $date );
+		$this->prev_year->setYear( $this->prev_year->getYear()-1 );
 
-		$this->prev_year = $this->this_month;
-		$this->prev_year->addYears( -1 );
+		$this->next_year = new Date( $date );
+		$this->next_year->setYear( $this->next_year->getYear()+1 );
 
-		$this->next_year = $this->this_month;
-		$this->next_year->addYears( +1 );
+		$date = Date_Calc::beginOfPrevMonth( $d, $m, $y, DATE_FORMAT_TIMESTAMP_DATE );
+		$this->prev_month = new Date( "{$date}000000" );
+
+		$date = Date_Calc::beginOfNextMonth( $d, $m, $y, DATE_FORMAT_TIMESTAMP_DATE );
+		$this->next_month =  new Date( "{$date}000000" );
+
 	}
 
 	function setStyles( $title, $main ) {
@@ -101,115 +116,113 @@ class CMonthCalendar {
 	}
 
 	function _drawTitle() {
-	        global $AppUI;
-		$qry = isset( $_GET['m'] ) ? 'm=' . $_GET['m'] : '';
-		$url = $_SERVER['PHP_SELF'] . ($qry ? "?$qry&" : '?');
+		global $AppUI, $m, $a;
+		$url = "index.php?m=$m";
+		$url .= $a ? "&a=$a" : '';
+		$url .= isset( $_GET['dialog']) ? "&dialog=1" : '';
 
-		//$s = '<table border="0" cellspacing="0" cellpadding="3" width="100%" class="'.$this->styleTitle.'">';
-		$s = "<table border=\"0\" cellspacing=\"0\" cellpadding=\"3\" width=\"100%\" class=\"" . $this->styleTitle . "\">\n";
-		$s .= "    <tr>\n";
+		$s = "\n<table border=\"0\" cellspacing=\"0\" cellpadding=\"3\" width=\"100%\" class=\"$this->styleTitle\">";
+		$s .= "\n\t<tr>";
 
 		if ($this->showArrows) {
-			$href = $url.'uts='.$this->prev_month->getTimestamp().($this->callback ? '&callback='.$this->callback : '');
-			$s .= "        <td align=\"left\">";
+			$href = $url.'&date='.$this->prev_month->format(DATE_FORMAT_TIMESTAMP_DATE).($this->callback ? '&callback='.$this->callback : '');
+			$s .= "\n\t\t<td align=\"left\">";
 			$s .= '<a href="'.$href.'"><img src="./images/prev.gif" width="16" height="16" alt="'.$AppUI->_('previous month').'" border="0" /></a>';
-			$s .= "</td>\n";
+			$s .= "</td>";
 
 		}
 
-		$s .= "        <th width=\"99%\" align=\"center\">";
-		$s .= $this->this_month->toString( "%B %Y" );
-		$s .= "</th>\n";
+		$s .= "\n\t<th width=\"99%\" align=\"center\">";
+		$s .= $this->this_month->format( "%B %Y" );
+		$s .= "</th>";
 
 		if ($this->showArrows) {
-			$href = $url.'uts='.$this->next_month->getTimestamp().($this->callback ? '&callback='.$this->callback : '');
-			$s .= "        <td align=\"right\">";
+			$href = $url.'&date='.$this->next_month->format(DATE_FORMAT_TIMESTAMP_DATE).($this->callback ? '&callback='.$this->callback : '');
+			$s .= "\n\t\t<td align=\"right\">";
 			$s .= '<a href="'.$href.'"><img src="./images/next.gif" width="16" height="16" alt="'.$AppUI->_('next month').'" border="0" /></a>';
-			$s .= "</td>\n";
+			$s .= "</td>";
 		}
 
-		$s .= "    </tr>\n";
-		$s .= "</table>\n";
+		$s .= "\n\t</tr>";
+		$s .= "\n</table>";
 
 		return $s;
 	}
-
+/**
+ *	@return string Returns table a row with the day names
+ */
 	function _drawDays() {
-		$s = $this->showWeek ? "        <th>&nbsp;</th>\n" : "";
-		$day = new CDate();
-		$day->setFormat( "%a" );
-		$day->setWeekday( LOCALE_FIRST_DAY );
-		for ($i=LOCALE_FIRST_DAY; $i < 7 + LOCALE_FIRST_DAY; $i++) {
-//      for ($i=0; $i < 7; $i++) {
-			$s .= "        <th width=\"14%\">".$day->toString()."</th>\n";
-			$day->addDays(1);
+		$bow = Date_Calc::beginOfWeek( null,null,null,null,LOCALE_FIRST_DAY );
+		$y = substr( $bow, 0, 4 );
+		$m = substr( $bow, 4, 2 );
+		$d = substr( $bow, 6, 2 );
+		$wk = Date_Calc::getCalendarWeek( $d, $m, $y, "%a", LOCALE_FIRST_DAY );
+
+		$s = $this->showWeek ? "\n\t\t<th>&nbsp;</th>" : "";
+		foreach( $wk as $day ) {
+			$s .= "\n\t\t<th width=\"14%\">$day</th>";
 		}
-		return "    <tr>\n$s    </tr>\n";
+
+		return "\n<tr>$s\n</tr>";
 	}
 
 	function _drawMain() {
 		GLOBAL $AppUI;
+		$today = new Date();
+		$today = $today->format( "%Y%m%d%w" );
 
-		$show_day = $this->this_month;
-		$show_day->setDay( 1 );
-		$show_day->setFormat( $AppUI->getPref( 'SHDATEFORMAT' ) );
+		$date = $this->this_month;
+		$this_day = $date->getDay();
+		$this_month = $date->getMonth();
+		$this_year = $date->getYear();
+		$cal = Date_Calc::getCalendarMonth( $this_month, $this_year, "%Y%m%d%w", LOCALE_FIRST_DAY );
 
-	// pre-pad the calendar
-		$pad = $this->this_month->getStartSpaces();
-		$p = '';
-		for ($i=0; $i < $pad; $i++) {
-			$p .= "        <td class=\"empty\">&nbsp;</td>\n";
+		$df = $AppUI->getPref( 'SHDATEFORMAT' );
+
+		$html = '';
+		foreach ($cal as $week) {
+			$html .= "\n<tr>";
+			if ($this->showWeek) {
+				$html .=  "\n\t<td class=\"week\">";
+				$html .= $this->dayFunc ? "<a href=\"javascript:$this->weekFunc('$week[0]')\">" : '';
+				$html .= '<img src="./images/view.week.gif" width="16" height="15" border="0" alt="Week View" /></a>';
+				$html .= $this->dayFunc ? "</a>" : '';
+				$html .= "\n\t</td>";
+			}
+
+			foreach ($week as $day) {
+				$this_day = new Date( "{$day}000000" );
+				$y = intval( substr( $day, 0, 4 ) );
+				$m = intval( substr( $day, 4, 2 ) );
+				$d = intval( substr( $day, 6, 2 ) );
+				$dow = intval( substr( $day, 8, 1 ) );
+				
+				if ($m != $this_month) {
+					$class = 'empty';
+				} else if ($day == $today) {
+					$class = 'today';
+				} else if ($dow == 0 || $dow == 6) {
+					$class = 'weekend';
+				} else {
+					$class = 'day';
+				}
+				$day = substr( $day, 0, 8 );
+				$html .= "\n\t<td class=\"$class\">";
+				if ($this->dayFunc) {
+					$html .= "<a href=\"javascript:$this->dayFunc('$day','".$this_day->format( $df )."')\" class=\"$class\">";
+					$html .= "$d";
+					$html .= "</a>";
+				} else {
+					$html .= "$d";
+				}
+				if ($m == $this_month && $this->showEvents) {
+					$html .= $this->_drawEvents( $d );
+				}
+				$html .= "\n\t</td>";
+			}
+			$html .= "\n</tr>";
 		}
-		$w = $this->showWeek ? $this->_drawWeek( $show_day ) : '';
-		$s = $p ? "    <tr>\n$w$p" : "";
-
-	// fill the calendar
-		$n = $this->this_month->daysInMonth();
-		for ($i=0; $i < $n; $i++) {
-			$day = $show_day->getWeekday();
-			$class = 'day';
-			//if ($show_day->D == $this->this_month->D) {
-			if ($show_day->isToday()) {
-				$class = 'today';
-			} else if ($day < 1 || $day > 5) {
-				$class = 'weekend';
-			}
-			// start new row
-			if ($day == LOCALE_FIRST_DAY) {
-				$w = $this->showWeek ? $this->_drawWeek( $show_day ) : '';
-				$s .= "    <tr>\n$w";
-			}
-			$s .= "        <td class=\"$class\">";
-			if ($this->dayFunc) {
-				$href = "javascript:$this->dayFunc(".$show_day->getTimestamp().",'".$show_day->toString()."')";
-				$s .= "<a href=\"$href\" class=\"$class\">".$show_day->D."</a>";
-			} else {
-				$s .= $show_day->D;
-			}
-			if ($this->showEvents) {
-				$s .= $this->_drawEvents( $show_day->D );
-			}
-			$s .= "</td>\n";
-
-			// finish a row
-			if ($day == 6 + LOCALE_FIRST_DAY) {
-				$s .= "    </tr>\n";
-			}
-			$show_day->addDays( 1 );
-		}
-
-		// post-pad the calendar
-		$pad = 7 - (($pad + $this->this_month->daysInMonth()) % 7);
-		if ($pad < 7) {
-			for ($i=0; $i < $pad; $i++) {
-				$s .= "        <td class=\"empty\">&nbsp;</td>\n";
-			}
-			$s .= "    </tr>\n";
-		}
-	/*
-	*/
-		// print it
-		return $s;
+		return $html;
 	}
 
 	function _drawWeek( $dateObj ) {
@@ -244,8 +257,10 @@ class CMonthCalendar {
 ##
 ## CEvent Class
 ##
-class CEvent {
+class CEvent extends CDpObject {
+/** @var int */
 	var $event_id = NULL;
+/** @var string The title of the event */
 	var $event_title = NULL;
 	var $event_start_date = NULL;
 	var $event_end_date = NULL;
@@ -260,48 +275,12 @@ class CEvent {
 	var $event_private = NULL;
 
 	function CEvent() {
-		// empty constructor
+		$this->CDpObject( 'events', 'event_id' );
 	}
 
 	function check() {
-		if (!$this->event_private) {
-			$this->event_private = '0';
-		}
-		// TODO
+		$this->event_private = intval( $this->event_private );
 		return NULL; // object is ok
-	}
-
-	function bind( $hash ) {
-		if (!is_array( $hash )) {
-			return get_class( $this )."::bind failed";
-		}
-		bindHashToObject( $hash, $this );
-	}
-
-	function store() {
-		$msg = $this->check();
-		if( $msg ) {
-			return get_class( $this )."::store-check failed";
-		}
-		if( $this->event_id ) {
-			$ret = db_updateObject( 'events', $this, 'event_id', false );
-		} else {
-			$ret = db_insertObject( 'events', $this, 'event_id' );
-		}
-		if( !$ret ) {
-			return get_class( $this )."::store failed <br />" . db_error();
-		} else {
-			return NULL;
-		}
-	}
-
-	function delete() {
-		$sql = "DELETE FROM events WHERE event_id = $this->event_id";
-		if (!db_exec( $sql )) {
-			return db_error();
-		} else {
-			return NULL;
-		}
 	}
 }
 ?>
