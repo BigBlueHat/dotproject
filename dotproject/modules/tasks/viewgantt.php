33e2f1ae52982b0038ed0e033a9ddd383a440028
eddieajau <eddieajau>
2003-05-01 04:23:11 +0000

    Updates for PEAR Date Class. Reworked Gantt presentation

--- dotproject/modules/tasks/viewgantt.php
+++ dotproject/modules/tasks/viewgantt.php
@@ -5,194 +5,159 @@ $min_view = defVal( @$min_view, false);
 $project_id = defVal( @$_GET['project_id'], 0);
 
 // sdate and edate passed as unix time stamps
-$sdate = defVal( @$_POST['sdate'], 0);
-$edate = defVal( @$_POST['edate'], 0);
+$sdate = dPgetParam( $_POST, 'sdate', 0 );
+$edate = dPgetParam( $_POST, 'edate', 0 );
 
 // months to scroll
 $scroll_date = 1;
 
-$display_option = defVal( @$_POST['display_option'], 'this_month');
+$display_option = dPgetParam( $_POST, 'display_option', 'this_month' );
 
 // format dates
 $df = $AppUI->getPref('SHDATEFORMAT');
 
 if ($display_option == 'custom') {
 	// custom dates
-	$start_date = new CDate( $sdate );
-	$end_date = new CDate( $edate );
+	$start_date = intval( $sdate ) ? new Date( "{$sdate}000000" ) : new Date();
+	$end_date = intval( $edate ) ? new Date( "{$edate}000000" ) : new Date();
 } else {
 	// month
-	$start_date = new CDate( null );
+	$start_date = new Date();
 	$end_date = $start_date;
 	$end_date->addMonths( $scroll_date );
 }
 
-$start_date->setFormat( $df );
-$end_date->setFormat( $df );
-
-$crumbs = array();
-$crumbs["?m=tasks"] = "tasks list";
-$crumbs["?m=projects&a=view&project_id=$project_id"] = "view this project";
-
+// setup the title block
+if (!@$min_view) {
+	$titleBlock = new CTitleBlock( 'Gantt Chart', 'applet-48.png', $m, "$m.$a" );
+	$titleBlock->addCrumb( "?m=tasks", "tasks list" );
+	$titleBlock->addCrumb( "?m=projects&a=view&project_id=$project_id", "view this project" );
+	$titleBlock->show();
+}
 ?>
 <script language="javascript">
 var calendarField = '';
 
 function popCalendar( field ){
 	calendarField = field;
-	uts = eval( 'document.ganttdate.' + field + '.value' );
-	window.open( './calendar.php?callback=setCalendar&uts=' + uts, 'calwin', 'top=250,left=250,width=250,height=220,scollbars=false' );
+	idate = eval( 'document.editFrm.' + field + '.value' );
+	window.open( 'index.php?m=public&a=calendar&dialog=1&callback=setCalendar&date=' + idate, 'calwin', 'top=250,left=250,width=250, height=220, scollbars=false' );
 }
 
-function setCalendar( uts, fdate ) {
-	fld_uts = eval( 'document.ganttdate.' + calendarField );
-	fld_fdate = eval( 'document.ganttdate.show_' + calendarField );
-	fld_uts.value = uts;
+/**
+ *	@param string Input date in the format YYYYMMDD
+ *	@param string Formatted date
+ */
+function setCalendar( idate, fdate ) {
+	fld_date = eval( 'document.editFrm.' + calendarField );
+	fld_fdate = eval( 'document.editFrm.show_' + calendarField );
+	fld_date.value = idate;
 	fld_fdate.value = fdate;
 }
 
 function scrollPrev() {
-	f = document.ganttdate;
-<?php 
+	f = document.editFrm;
+<?php
 	$new_start = $start_date;
 	$new_end = $end_date;
 	$new_start->addMonths( -$scroll_date );
 	$new_end->addMonths( -$scroll_date );
-	echo "f.sdate.value='".$new_start->getTimestamp()."';";
-	echo "f.edate.value='".$new_end->getTimestamp()."';";
+	echo "f.sdate.value='".$new_start->format( DATE_FORMAT_TIMESTAMP_DATE )."';";
+	echo "f.edate.value='".$new_end->format( DATE_FORMAT_TIMESTAMP_DATE )."';";
 ?>
-	document.ganttdate.display_option.value = 'custom';	
+	document.editFrm.display_option.value = 'custom';
 	f.submit()
 }
 
 function scrollNext() {
-	f = document.ganttdate;
-<?php 
+	f = document.editFrm;
+<?php
 	$new_start = $start_date;
 	$new_end = $end_date;
 	$new_start->addMonths( $scroll_date );
 	$new_end->addMonths( $scroll_date );
-	echo "f.sdate.value='" . $new_start->getTimestamp() . "';";
-	echo "f.edate.value='" . $new_end->getTimestamp() . "';";
+	echo "f.sdate.value='" . $new_start->format( DATE_FORMAT_TIMESTAMP_DATE ) . "';";
+	echo "f.edate.value='" . $new_end->format( DATE_FORMAT_TIMESTAMP_DATE ) . "';";
 ?>
-	document.ganttdate.display_option.value = 'custom';
+	document.editFrm.display_option.value = 'custom';
 	f.submit()
 }
-	
+
 function showThisMonth() {
-	document.ganttdate.display_option.value = "this_month";
-	document.ganttdate.submit();
+	document.editFrm.display_option.value = "this_month";
+	document.editFrm.submit();
 }
-	
+
 function showFullProject() {
-	document.ganttdate.display_option.value = "all";
-	document.ganttdate.submit();
-}	
-	
+	document.editFrm.display_option.value = "all";
+	document.editFrm.submit();
+}
+
 </script>
 
-<?php
-if (!$min_view) {
-	// Normal view (not inserted in tabbox)
-?>
-<table name="table" cellspacing="1" cellpadding="1" border="0" width="98%">
-<tr>
-	<td><?php echo dPshowImage( dPfindImage( 'applet-48.png', $m ), 16, 16, '' ); ?></td>
-	<td nowrap><h1><?php echo $AppUI->_( 'Gantt Chart' );?></h1></td>
-	<td nowrap><img src="./images/shim.gif" width="16" height="16" alt="" border="0"></td>
-	<td valign="top" align="right" width="100%"></td>
-</tr>
-</table>
+<table border="0" cellpadding="4" cellspacing="0">
 
-<table border="0" cellpadding="4" cellspacing="0" width="100%">
-<tr>
-	<td nowrap valign=top><?php echo breadCrumbs( $crumbs );?></td>
-<?php
-} else {
-	// Minimal view (inserted in tabbox)
-?>
-	<table width=100%>
-	<tr>
-<?php 	
-}
-?>
-<td align=right valign=bottom><?php echo "<a href='javascript:showThisMonth()'>".$AppUI->_('show this month')."</a> : <a href='javascript:showFullProject()'>".$AppUI->_('show full project')."</a><br>"; ?></td>	
-<td align=right width=0>	
-  <form name="ganttdate" method="post" action="?<?php echo "m=$m&a=$a&project_id=$project_id";?>">
-  <input type=hidden name=display_option value=<?php echo $display_option ?>>
-  <table border="0" cellpadding="1" cellspacing="1">
-  <tr>
-  <td align="right" nowrap="nowrap"><?php echo $AppUI->_( 'From' );?>:</td>
-  <td align=left nowrap>
-  <input type="hidden" name="sdate" value="<?php echo $start_date->getTimestamp();?>">
-  <input type="text" class="text" name="show_sdate" value="<?php echo $start_date->toString();?>" size="12" disabled="disabled">
-  <a href="javascript:popCalendar('sdate')">
-  <img src="./images/calendar.gif" width="24" height="12" alt="" border="0">
-  </a>
-  </td>
-  </tr>
-  
-  <tr>
-  <td align="right" nowrap="nowrap"><?php echo $AppUI->_( 'To' );?>:</td>
-  <td align=left nowrap>
-  <input type="hidden" name="edate" value="<?php echo $end_date->getTimestamp();?>">
-  <input type="text" class="text" name="show_edate" value="<?php echo $end_date->toString();?>" size="12" disabled="disabled">
-  <a href="javascript:popCalendar('edate')">
-  <img src="./images/calendar.gif" width="24" height="12" alt="" border="0">
-  </a>
-  </td>
-  </tr>
-  
-  <tr>
-  <td>
-  </td>
-  <td align=left>
-  <input type="button" class="button" value="<?php echo $AppUI->_( 'submit' );?>" onclick='display_option.value="custom";submit();'>
-  </td>
-</tr>
-	
-</form>
-</table>
-	
-</tr>
-</table>	
-<br>
+<form name="editFrm" method="post" action="?<?php echo "m=$m&a=$a&project_id=$project_id";?>">
+<input type="hidden" name="display_option" value="<?php echo $display_option;?>" />
 
-
-<table align=center width=100%>
 <tr>
-<?php if ($display_option != "all") {
-?>
-	<td align=left valign=top>
+	<td align="left" valign="top" width="20">
+<?php if ($display_option != "all") { ?>
 		<a href="javascript:scrollPrev()">
-			<img src="./images/prev.gif" width="16" height="16" alt="<?php echo $AppUI->_( 'previous' );?>" border="0">	  
+			<img src="./images/prev.gif" width="16" height="16" alt="<?php echo $AppUI->_( 'previous' );?>" border="0">
 		</a>
-	</td>   
-<?php
-}
-?>
-	<td align=center>
-<?php  
-$src = 
-  "?m=tasks&a=gantt&suppressHeaders=1&project_id=$project_id" .
-  ( $display_option == 'all' ? '' : 
-	'&start_date=' . $start_date->toString( "%Y-%m-%d" ) . '&end_date=' . $end_date->toString( "%Y-%m-%d" ) ) .
-  "&width=' + ((navigator.appName=='Netscape'?window.innerWidth:document.body.offsetWidth) - 100) + '";
-
-echo "<script>document.write('<img src=\"$src\">')</script>";
-?>	
+<?php } ?>
 	</td>
 
-<?php if ($display_option != "all") {
-?>
-	<td align=right valign=top>
+	<td align="right" nowrap="nowrap"><?php echo $AppUI->_( 'From' );?>:</td>
+	<td align="left" nowrap="nowrap">
+		<input type="hidden" name="sdate" value="<?php echo $start_date->format( DATE_FORMAT_TIMESTAMP_DATE );?>" />
+		<input type="text" class="text" name="show_sdate" value="<?php echo $start_date->format( $df );?>" size="12" disabled="disabled" />
+		<a href="javascript:popCalendar('sdate')"><img src="./images/calendar.gif" width="24" height="12" alt="" border="0"></a>
+	</td>
+
+	<td align="right" nowrap="nowrap"><?php echo $AppUI->_( 'To' );?>:</td>
+	<td align="left" nowrap="nowrap">
+		<input type="hidden" name="edate" value="<?php echo $end_date->format( DATE_FORMAT_TIMESTAMP_DATE );?>" />
+		<input type="text" class="text" name="show_edate" value="<?php echo $end_date->format( $df );?>" size="12" disabled="disabled" />
+		<a href="javascript:popCalendar('edate')"><img src="./images/calendar.gif" width="24" height="12" alt="" border="0"></a>
+
+	<td align="left">
+		<input type="button" class="button" value="<?php echo $AppUI->_( 'submit' );?>" onclick='display_option.value="custom";submit();'>
+	</td>
+
+	<td align="right" valign="top" width="20">
+<?php if ($display_option != "all") { ?>
 	  <a href="javascript:scrollNext()">
 	  	<img src="./images/next.gif" width="16" height="16" alt="<?php echo $AppUI->_( 'next' );?>" border="0">
 	  </a>
+<?php } ?>
 	</td>
 </tr>
-<?php
-}
-?>	  
+
+</form>
+
+<tr>
+	<td align="center" valign="bottom" colspan="7">
+		<?php echo "<a href='javascript:showThisMonth()'>".$AppUI->_('show this month')."</a> : <a href='javascript:showFullProject()'>".$AppUI->_('show full project')."</a><br>"; ?>
+	</td>
+</tr>
+
 </table>
 
+<table cellspacing="0" cellpadding="0" border="1" align="center">
+<tr>
+	<td>
+<?php
+$src =
+  "?m=tasks&a=gantt&suppressHeaders=1&project_id=$project_id" .
+  ( $display_option == 'all' ? '' :
+	'&start_date=' . $start_date->format( "%Y-%m-%d" ) . '&end_date=' . $end_date->format( "%Y-%m-%d" ) ) .
+  "&width=' + ((navigator.appName=='Netscape'?window.innerWidth:document.body.offsetWidth)*0.95) + '";
+
+echo "<script>document.write('<img src=\"$src\">')</script>";
+?>
+	</td>
+</tr>
+</table>
+<br />
