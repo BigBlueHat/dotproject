36915f0f45a10e63a4e42160feec16fa8153e21a
eddieajau <eddieajau>
2003-05-08 06:30:14 +0000

    Updated head block

--- dotproject/style/classic/header.php
+++ dotproject/style/classic/header.php
@@ -6,7 +6,12 @@ $dialog = dPgetParam( $_GET, 'dialog', 0 );
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
 	<meta name="Description" content="Classic dotProject Style" />
-	<meta http-equiv="Content-Type" content="text/html;charset=UTF-8" />
+	<meta name="Version" content="<?php echo @$AppUI->getConfig( 'version' );?>" />
+	<meta http-equiv="Content-Type" content="text/html;charset=<?php echo isset( $locale_char_set ) ? $locale_char_set : 'UTF-8';?>" />
+	<title><?php echo @$AppUI->getConfig( 'page_title' );?></title>
+	<link rel="stylesheet" type="text/css" href="./style/<?php echo $uistyle;?>/main.css" media="all" />
+	<style type="text/css" media="all">@import "./style/<?php echo $uistyle;?>/main.css";</style>
+
 	<script language="JavaScript">
 	function doBtn() {
 		var oEl = event.srcElement;
@@ -32,8 +37,6 @@ $dialog = dPgetParam( $_GET, 'dialog', 0 );
 		oEl.className = "topBtnOff";
 	}
 	</script>
-	<title><?php echo $AppUI->cfg['page_title'];?></title>
-	<link rel="stylesheet" type="text/css" href="./style/<?php echo $uistyle;?>/main.css" />
 </head>
 <body class="mainpage" background="style/classic/images/bground.gif">
 <table class="nav" width="100%" cellpadding="0" cellspacing="2">
@@ -65,9 +68,6 @@ $dialog = dPgetParam( $_GET, 'dialog', 0 );
 	echo '</td><input type="hidden" name="a" value="addedit" />';
 
 //build URI string
-	if ($AppUI->getDaySelected()) {
-		echo '<input type="hidden" name="uts" value="'.$AppUI->getDaySelected().'" />';
-	}
 	if (isset( $company_id )) {
 		echo '<input type="hidden" name="company_id" value="'.$company_id.'" />';
 	}
--- dotproject/style/default/header.php
+++ dotproject/style/default/header.php
@@ -5,9 +5,9 @@ $dialog = dPgetParam( $_GET, 'dialog', 0 );
 	   "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
-	<meta name="Description" content="Ultra Clean dotProject Style" />
+	<meta name="Description" content="dotProject Default Style" />
 	<meta name="Version" content="<?php echo @$AppUI->getConfig( 'version' );?>" />
-	<meta http-equiv="Content-Type" content="text/html;charset=<?php echo isset( $locale_char_set ) ? $locale_char_set:'UTF-8';?>" />
+	<meta http-equiv="Content-Type" content="text/html;charset=<?php echo isset( $locale_char_set ) ? $locale_char_set : 'UTF-8';?>" />
 	<title><?php echo @$AppUI->getConfig( 'page_title' );?></title>
 	<link rel="stylesheet" type="text/css" href="./style/<?php echo $uistyle;?>/main.css" media="all" />
 	<style type="text/css" media="all">@import "./style/<?php echo $uistyle;?>/main.css";</style>
