c96c82ee2a39a04fc42e9286a86fa840df4b7d67
ajdonnison <ajdonnison>
2003-11-04 23:13:09 +0000

    Added Tom Reinertson's patch to fix user edit errors.

--- dotproject/modules/admin/do_user_aed.php
+++ dotproject/modules/admin/do_user_aed.php
@@ -1,5 +1,6 @@
 <?php /* ADMIN $Id$ */
-$del = isset($_REQUEST['del']) ? $_REQUEST['del'] : 0;
+
+$del = isset($_REQUEST['del']) ? $_REQUEST['del'] : FALSE;
 
 $obj = new CUser();
 
@@ -11,19 +12,6 @@ if (!$obj->bind( $_POST )) {
 // prepare (and translate) the module name ready for the suffix
 $AppUI->setMsg( 'User' );
 
-//pull a list of existing usernames
-$sql = "SELECT user_username FROM users";
-$users = db_loadList( $sql );
-
-// check if a user with the param Username already exists
-$userEx = FALSE;
-function userExistence( $userName ) {
-	global $obj, $userEx;
-	if ( $userName == $obj->user_username ) {
-		$userEx = TRUE;
-	}
-}
-
 if ($del) {
 	if (($msg = $obj->delete())) {
 		$AppUI->setMsg( $msg, UI_MSG_ERROR );
@@ -32,27 +20,42 @@ if ($del) {
 		$AppUI->setMsg( "deleted", UI_MSG_ALERT, true );
 		$AppUI->redirect( '', -1 );
 	}
-} else {
-	// Iterate the above userNameExistenceCheck for each user
-	foreach ( $users as $usrs ) {
-		$usrLst = array_map( "userExistence", $usrs );
-	}
+	return;
+}
+	$isNewUser = !($_REQUEST['user_id']);
+	if ( $isNewUser ) {
+		// check if a user with the param Username already exists
+		$userEx = FALSE;
 
-	// If userName already exists quit with error and do nothing
-	if ( $userEx == TRUE ) {
-		$AppUI->setMsg( "already exists. Try another username.", UI_MSG_ERROR, true );
-		$AppUI->redirect( );
-	}
+		function userExistence( $userName ) {
+			global $obj, $userEx;
+			if ( $userName == $obj->user_username ) {
+				$userEx = TRUE;
+			}
+		}
 
-	$isNotNew = @$_REQUEST['user_id'];
-	if (!$isNotNew) {
+		//pull a list of existing usernames
+		$sql = "SELECT user_username FROM users";
+		$users = db_loadList( $sql );
+
+		// Iterate the above userNameExistenceCheck for each user
+		foreach ( $users as $usrs ) {
+			$usrLst = array_map( "userExistence", $usrs );
+		}
+		// If userName already exists quit with error and do nothing
+		if ( $userEx == TRUE ) {
+			$AppUI->setMsg( "already exists. Try another username.", UI_MSG_ERROR, true );
+			$AppUI->redirect( );
+		}
+
 		$obj->user_owner = $AppUI->user_id;
 	}
+
 	if (($msg = $obj->store())) {
 		$AppUI->setMsg( $msg, UI_MSG_ERROR );
 	} else {
-		$AppUI->setMsg( $isNotNew ? 'updated' : 'added', UI_MSG_OK, true );
+		$AppUI->setMsg( $isNewUser ? 'added' : 'updated', UI_MSG_OK, true );
 	}
 	$AppUI->redirect();
-}
+
 ?>
