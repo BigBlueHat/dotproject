66d00da5e05ebcaea51b82784e3be1d9aea9f0e4
gregorerhardt <gregorerhardt>
2003-12-09 00:17:16 +0000

    Added Text String 'Filter' on the left side of the projectsDropDown

--- dotproject/modules/files/index.php
+++ dotproject/modules/files/index.php
@@ -21,6 +21,7 @@ $projects = arrayMerge( array( '0'=>$AppUI->_('All') ), $projects );
 
 // setup the title block
 $titleBlock = new CTitleBlock( 'Files', 'folder5.png', $m, "$m.$a" );
+$titleBlock->addCell( $AppUI->_('Filter') . ':' );
 $titleBlock->addCell(
 	arraySelect( $projects, 'project_id', 'onChange="document.pickProject.submit()" size="1" class="text"', $project_id ), '',
 	'<form name="pickProject" action="?m=files" method="post">', '</form>'
