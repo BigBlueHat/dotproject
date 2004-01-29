7a77191ea172d84395fa99677005d502a14bce45
ajdonnison <ajdonnison>
2004-01-29 04:12:33 +0000

    Added patch 777016 from iexposure to fix word wrapping.

--- dotproject/modules/ticketsmith/common.inc.php
+++ dotproject/modules/ticketsmith/common.inc.php
@@ -203,6 +203,113 @@ function smart_wrap ($text, $width) {
 
 }
 
+/* word_wrap($string, $cols, $quote_old, $prefix, $nice_prefix)
+ *
+ * Takes $string, and wraps it on a per-word boundary (does not clip
+ * words UNLESS the word is more than $cols long), no more than $cols per
+ * line. Allows for optional prefix string for each line. (Was written to
+ * easily format replies to e-mails, prefixing each line with "> ".
+ *
+ * Puts words that do not fit at the end of the line to the 
+ * beginning of the next line (keeping in mind prefixes) and not on a line
+ * by themselves.
+ *
+ * parameter $string 		-- text to be wrapped
+ * parameter $cols  		-- maximum width of text
+ * parameter $quote_old 	-- adds the prefix for each line if true
+ * parameter $prefix 		-- prefix used in message (nice_prefix but w/o spaces)
+ * parameter $nice_prefix	-- prefix that is easier to read (i.e. "> " instead of ">").
+ *
+ * $nice_prefix is actually written to the message (if $quote_old), but $prefix is used for 
+ * backward compatibility
+ * 
+ * Please note that though quote_old may be false, prefix and nice_prefix are still used to maintain
+ * the existing structure of a message (in case the message is a reply or etc...)
+ *
+ * Original by Dominic J. Eidson.
+ * Copyright 1999 Dominic J. Eidson, use as you wish, but give credit
+ * where credit due.
+ * Modified by Daniel Kigelman in 2003.
+ *
+ */
+
+function word_wrap ($string, $cols = 78, $quote_old = false, $prefix = ">", $nice_prefix = "> ") {
+
+if (preg_match("/^.*\r\n/", $string)) {
+	$t_lines = split( "\r\n", $string);
+} else if (preg_match("/^.*\n/", $string)) {
+	$t_lines = split( "\n", $string);
+} else {
+	$t_lines = split( "\r", $string);
+}
+
+$outlines = "";
+$leftover = "";
+
+// Loop through each line of message
+while(list(, $thisline) = each($t_lines)) {
+	// Process Leftover
+	if (strlen($leftover) > 0) {
+		$counter = 0;
+
+		// Subtract all prefixes from the beginning of this line.
+		while (substr($thisline, 0, strlen($prefix)) == $prefix) {
+			$counter++;
+			
+			if (substr($thisline, 0, strlen($nice_prefix)) == $nice_prefix) {
+				$thisline = substr($thisline, strlen($nice_prefix));
+			} else {
+				$thisline = substr($thisline, strlen($prefix));
+			}
+		}
+		
+		// Add the leftover to the beginning of this line.
+		$thisline = $leftover . $thisline;
+		
+		// Add all the prefixes back on to the beginning of the line.
+		for ($i = 0; $i < $counter; $i++) {
+			$thisline = $nice_prefix . $thisline;
+		}
+	}
+
+	if(strlen($thisline) + strlen($nice_prefix) > $cols) {
+		$newline = "";
+		$t_l_lines = split(" ", $thisline);
+		// This line is too big.  Break it up into words and add them one by one.
+		while(list(, $thisword) = each($t_l_lines)) {
+			// Process words that are longer than $cols
+			while((strlen($thisword) + strlen($nice_prefix)) > $cols) {
+				$cur_pos = 0;
+				$outlines .= $nice_prefix;
+				for($num=0; $num < $cols-1; $num++) {
+					$outlines .= $thisword[$num];
+					$cur_pos++;
+				}
+				$outlines .= "\n";
+				$thisword = substr($thisword, $cur_pos, (strlen($thisword)-$cur_pos));
+			}
+
+			// Check that the line is within $cols.  If not, don't add the word; start a new line.
+			if((strlen($newline) + strlen($thisword) + strlen($nice_prefix) + 1) > $cols) {
+				if ($quote_old) $outlines .= $nice_prefix.$newline."\n";
+				else $outlines .= $newline."\n";
+				$newline = $thisword." ";
+			} else {
+				$newline .= $thisword." ";
+			}
+		}
+		// Whatever is leftover from processing the line, assign to $leftover
+		$leftover = $newline;
+	} else {
+		if ($quote_old) $outlines .= $nice_prefix . $thisline."\n";
+		else $outlines .= $thisline."\n";
+		$leftover = "";
+	}
+}
+return $outlines;
+}
+
+
 /* format display field */
 function format_field ($value, $type, $ticket = NULL) {
 
@@ -305,6 +412,9 @@ function format_field ($value, $type, $ticket = NULL) {
             $output .= " <em>($time_ago)</em>";
             break;
         case "body":
+	    if ($CONFIG["wordwrap"]) {
+	    	$value = word_wrap($value, 78);
+	    }
             $value = htmlspecialchars($value);
             $output = "<table width=\"100%\" border=\"1\" cellspacing=\"0\" cellpadding=\"10\">\n";
             $output .= "<tr><td bgcolor=\"" . $CONFIG["ticket_color"] . "\">\n<tt><pre>\n";
@@ -312,17 +422,12 @@ function format_field ($value, $type, $ticket = NULL) {
             $url_replace = "<a href=\"\\1\\2\" target=\"new\">";
             $url_replace .= "<span style=\"font-size: 10pt;\">\\1\\2</span></a>";
             $value = preg_replace($url_find, $url_replace, $value);
-            if ($CONFIG["wordwrap"]) {
-                $output .= smart_wrap($value, 72);
-            }
-            else {
-                $output .= $value;
-            }
+	    $output .= $value;
             $output .= "\n</pre></tt>\n</td></tr>\n</table>\n";
             break;
         case "followup":
             $output = "\n<tt>\n";
-            $output .= "<textarea name=\"followup\" wrap=\"hard\" cols=\"72\" rows=\"20\">\n";
+            $output .= "<textarea style='font-family: monospace;' name=\"followup\" wrap=\"hard\" cols=\"72\" rows=\"20\">\n";
             $signature = query2result("SELECT user_signature FROM users WHERE user_id = '$AppUI->user_id'");
             if ($signature) {
                 $output .= "\n";
@@ -332,14 +437,10 @@ function format_field ($value, $type, $ticket = NULL) {
             $output .= "\n\n";
             $output .= "---- ".$AppUI->_('Original message')." ----\n\n";
             if ($CONFIG["wordwrap"]) {
-                $value = smart_wrap($value, 70);
+                $value = word_wrap($value, 70, true);
             }
             $value = htmlspecialchars($value);
-            $lines = explode("\n", $value);
-            for ($loop = 0; $loop < count($lines); $loop++) {
-                $lines[$loop] = "&gt; " . $lines[$loop];
-            }
-            $output .= join("\n", $lines);
+            $output .= $value;
             $output .= "\n</textarea>\n";
             $output .= "</tt>\n";
             break;
