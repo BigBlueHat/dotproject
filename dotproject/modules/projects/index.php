702853ce4a8ca338eebb3a31a2e908070c95050d
bloaterpaste <bloaterpaste>
2004-05-19 21:36:01 +0000

    Changed company filter to be Company/Department filter.  In conjunction to my contact/deptartment tables that I just checked in, this will allow a user to winnow projects down by the department(s) they are assigned to.  The company drop down now lists all companies and departments.

--- dotproject/modules/projects/index.php
+++ dotproject/modules/projects/index.php
@@ -28,10 +28,22 @@ $active = intval( !$AppUI->getState( 'ProjIdxTab' ) );
 if (isset( $_POST['company_id'] )) {
 	$AppUI->setState( 'ProjIdxCompany', intval( $_POST['company_id'] ) );
 }
-
-// BUG FIX: Selecting all companies didn't work
 $company_id = $AppUI->getState( 'ProjIdxCompany' ) !== NULL ? $AppUI->getState( 'ProjIdxCompany' ) : $AppUI->user_company;
-//$company_id = $AppUI->getState( 'ProjIdxCompany' );
+
+$company_prefix = 'company_';
+
+if (isset( $_POST['department'] )) {
+	$AppUI->setState( 'ProjIdxDepartment', $_POST['department'] );
+}
+$department = $AppUI->getState( 'ProjIdxDepartment' ) !== NULL ? $AppUI->getState( 'ProjIdxDepartment' ) : $company_prefix.$AppUI->user_company;
+
+//if $department contains the $company_prefix string that it's requesting a company and not a department.  So, clear the 
+// $department variable, and populate the $company_id variable.
+if(!(strpos($department, $company_prefix)===false)){
+	$company_id = substr($department,strlen($company_prefix));
+	$AppUI->setState( 'ProjIdxCompany', $company_id );
+	unset($department);
+}
 
 if (isset( $_GET['orderby'] )) {
 	$AppUI->setState( 'ProjIdxOrderBy', $_GET['orderby'] );
@@ -68,13 +80,22 @@ CREATE TEMPORARY TABLE tasks_summy
 
 $tasks_summy = db_exec($sql);
 
+if(isset($department)){
+	//If a department is specified, we want to display projects from the department, and all departments under that, so we need to build that list of departments
+	$dept_ids = array();
+	$sql = 'SELECT dept_id, dept_parent FROM departments ORDER BY dept_parent, dept_name';
+	$rows = db_loadList( $sql );
+	addDeptId($rows, $department);
+	$dept_ids[] = $department;
+}
+
 // retrieve list of records
 // modified for speed
 // by Pablo Roca (pabloroca@mvps.org)
 // 16 August 2003
 $sql = "
 SELECT
-	project_id, project_active, project_status, project_color_identifier, project_name, project_description,
+	projects.project_id, project_active, project_status, project_color_identifier, project_name, project_description,
 	project_start_date, project_end_date, project_actual_end_date,
 	project_color_identifier,
 	project_company, company_name, project_status,
@@ -86,21 +107,22 @@ FROM permissions,projects
 LEFT JOIN companies ON projects.project_company = company_id
 LEFT JOIN users ON projects.project_owner = users.user_id
 LEFT JOIN tasks_sum ON projects.project_id = tasks_sum.task_project
-LEFT JOIN tasks_summy ON projects.project_id = tasks_summy.task_project
+LEFT JOIN tasks_summy ON projects.project_id = tasks_summy.task_project"
+.($department ? "\nLEFT JOIN project_departments ON project_departments.project_id = projects.project_id" : '')."
 WHERE permission_user = $AppUI->user_id
 	AND permission_value <> 0
 	AND (
 		(permission_grant_on = 'all')
 		OR (permission_grant_on = 'projects' AND permission_item = -1)
-		OR (permission_grant_on = 'projects' AND permission_item = project_id)
+		OR (permission_grant_on = 'projects' AND permission_item = projects.project_id)
 		)"
-.(count($deny) > 0 ? "\nAND project_id NOT IN (" . implode( ',', $deny ) . ')' : '')
+.(count($deny) > 0 ? "\nAND projects.project_id NOT IN (" . implode( ',', $deny ) . ')' : '')
 .($company_id ? "\nAND projects.project_company = '$company_id'" : '')
+.($department ? "\nAND project_departments.department_id in ( ".implode(',',$dept_ids)." )" : '')
 ."
-GROUP BY project_id
+GROUP BY projects.project_id
 ORDER BY $orderby
 ";
-//echo "<pre>$sql</pre>";
 
 $projects = db_loadList( $sql );
 
@@ -110,12 +132,42 @@ $obj = new CCompany();
 $companies = $obj->getAllowedRecords( $AppUI->user_id, 'company_id,company_name', 'company_name' );
 $companies = arrayMerge( array( '0'=>$AppUI->_('All') ), $companies );
 
+//get list of all departments, filtered by the list of permitted companies.
+$sql = "
+SELECT company_id, company_name, departments.* 
+FROM companies
+	LEFT JOIN departments ON companies.company_id = departments.dept_company
+WHERE company_id in( ".implode(',',array_keys($companies))." )
+ORDER BY company_name,dept_parent,dept_name
+";
+//echo "<pre>$sql</pre>";
+$rows = db_loadList( $sql, NULL );
+
+//display the select list
+$buffer = '<form action="?m=projects" method="post" name="pickCompany">';
+$buffer .= '<select name="department" onChange="document.pickCompany.submit()" class="text">';
+$buffer .= '<option value="company_0" style="font-weight:bold;">'.$AppUI->_('All').'</option>'."\n";
+$company = '';
+foreach ($rows as $row) {
+	if ($row["dept_parent"] == 0) {
+		if($company!=$row['company_id']){
+			$buffer .= '<option value="'.$company_prefix.$row['company_id'].'" style="font-weight:bold;"'.($company_id==$row['company_id']?'selected="selected"':'').'>'.$row['company_name'].'</option>'."\n";
+			$company=$row['company_id'];
+		}
+		if($row["dept_parent"]!=null){
+			showchild( $row );
+			findchild( $rows, $row["dept_id"] );
+		}
+	}
+}
+$buffer .= "</select></form>";
+
 // setup the title block
 $titleBlock = new CTitleBlock( 'Projects', 'applet3-48.png', $m, "$m.$a" );
-$titleBlock->addCell( $AppUI->_('Company') . ':' );
-$titleBlock->addCell(
-	arraySelect( $companies, 'company_id', 'onChange="document.pickCompany.submit()" class="text"', $company_id ), '',
-	'<form action="?m=projects" method="post" name="pickCompany">', '</form>'
+$titleBlock->addCell( $AppUI->_('Company') . '/' . $AppUI->_('Division') . ':' );
+$titleBlock->addCell( $buffer
+//	arraySelect( $companies, 'company_id', 'onChange="document.pickCompany.submit()" class="text"', $company_id ), '',
+//	'<form action="?m=projects" method="post" name="pickCompany">', '</form>'
 );
 $titleBlock->addCell();
 if ($canEdit) {
@@ -166,4 +218,46 @@ foreach($project_types as $project_type)
 	$tabBox->add($project_file_type[$project_type], $project_type);
 
 $tabBox->show();
+
+//writes out a single <option> element for display of departments
+function showchild( &$a, $level=1 ) {
+	Global $buffer, $department;
+	$s = '<option value="'.$a["dept_id"].'"'.(isset($department)&&$department==$a["dept_id"]?'selected="selected"':'').'>';
+
+	for ($y=0; $y < $level; $y++) {
+		if ($y+1 == $level) {
+			$s .= '';
+		} else {
+			$s .= '&nbsp;&nbsp;';
+		}
+	}
+
+	$s .= '&nbsp;&nbsp;'.$a["dept_name"]."</option>\n";
+	$buffer .= $s;
+
+//	echo $s;
+}
+
+//recursive function to display children departments.
+function findchild( &$tarr, $parent, $level=1 ){
+	$level = $level+1;
+	$n = count( $tarr );
+	for ($x=0; $x < $n; $x++) {
+		if($tarr[$x]["dept_parent"] == $parent && $tarr[$x]["dept_parent"] != $tarr[$x]["dept_id"]){
+			showchild( $tarr[$x], $level );
+			findchild( $tarr, $tarr[$x]["dept_id"], $level);
+		}
+	}
+}
+
+function addDeptId($dataset, $parent){
+	Global $dept_ids;
+	foreach ($dataset as $data){
+		if($data['dept_parent']==$parent){
+			$dept_ids[] = $data['dept_id'];
+			addDeptId($dataset, $data['dept_id']);
+		}
+	}
+}
+
 ?>
