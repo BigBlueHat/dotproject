a892a95e5cb53267a750283e315357a47c702fa1
gregorerhardt <gregorerhardt>
2004-06-12 15:26:25 +0000

    #914075: yet another ie bug

--- dotproject/fileviewer.php
+++ dotproject/fileviewer.php
@@ -100,6 +100,9 @@ if ($file_id) {
 		$AppUI->redirect( "m=public&a=access_denied" );
 	};
 
+	/*
+	 * DISABLED LINES TO FIX A NEWER BUG 914075 WITH IE 6 (GREGORERHARDT 20040612)
+
 	// BEGIN extra headers to resolve IE caching bug (JRP 9 Feb 2003)
 	// [http://bugs.php.net/bug.php?id=16173]
 	header("Pragma: ");
@@ -109,6 +112,7 @@ if ($file_id) {
 	header("Cache-Control: no-store, no-cache, must-revalidate");  //HTTP/1.1
 	header("Cache-Control: post-check=0, pre-check=0", false);
 	// END extra headers to resolve IE caching bug
+	*/
 
 	header("MIME-Version: 1.0");
 	header( "Content-length: {$file['file_size']}" );
