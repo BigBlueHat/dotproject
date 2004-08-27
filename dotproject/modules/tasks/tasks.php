8445724dee3454fced167ca24189265544ad6566
jcgonz <jcgonz>
2004-08-27 16:42:31 +0000

    Updated the use of dPconfig array to dPgetConfig() function

--- dotproject/modules/tasks/tasks.php
+++ dotproject/modules/tasks/tasks.php
@@ -424,7 +424,7 @@ function showtask( &$a, $level=0, $is_opened = true ) {
 	$s .= '<td nowrap="nowrap" align="center" style="'.$style.'">'.($last_update ? $last_update->format( $df ) : '-').'</td>';
 
 // Assignment checkbox
-        if ($canEdit && $dPconfig['direct_edit_assignment']) {
+        if ($canEdit && dPgetConfig('direct_edit_assignment')) {
                 $s .= "\n\t<td align='center'><input type=\"checkbox\" name=\"task_id{$a['task_id']}\" value=\"{$a['task_id']}\"/></td>";
         }
 	$s .= '</tr>';
@@ -633,7 +633,7 @@ function chAssignment(project_id, rmUser, del) {
 	<th nowrap="nowrap"><?php sort_by_item_title( 'Duration', 'task_duration', SORT_NUMERIC );?>&nbsp;&nbsp;</th>
 	<th nowrap="nowrap"><?php sort_by_item_title( 'Finish Date', 'task_end_date', SORT_NUMERIC );?></th>
 	<th nowrap="nowrap"><?php sort_by_item_title( 'Last Update', 'last_update', SORT_NUMERIC );?></th>
-        <?php if ($dPconfig['direct_edit_assignment']) { echo '<th width="1">&nbsp;</th>'; }?>
+        <?php if (dPgetConfig('direct_edit_assignment')) { echo '<th width="1">&nbsp;</th>'; }?>
 </tr>
 <?php
 //echo '<pre>'; print_r($projects); echo '</pre>';
