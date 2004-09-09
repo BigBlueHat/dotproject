16184717edee8374a317e9115201288f4b9ca81b
cyberhorse <cyberhorse>
2004-09-09 16:56:19 +0000

    fixed a javascript bug, stopping users from editing tasks.

--- dotproject/modules/tasks/addedit.php
+++ dotproject/modules/tasks/addedit.php
@@ -436,7 +436,7 @@ function submitIt( nt ){
                 <?php echo $project_start_date; ?> && form.task_start_date.value < <?php echo $project_start_date; ?>) {
                 alert( "<?php echo $AppUI->_('task dates outside the project dates'); ?>" );
         }
-        else if (form.reoccur.value > 0 && 
+        else if (form.reoccur && form.reoccur.value > 0 && 
                 (!form.task_start_date.value ||
                 !form.task_end_date.value ||
                 !<? echo $project_end_date; ?>)) {
