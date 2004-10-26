77a21e1ed21cf772adeaaff8c039648c9ec8aba3
jcgonz <jcgonz>
2004-10-26 23:57:33 +0000

    Fixed the custom fields parser module. It was left in the resources allocation tab, so it didn't work correctly.

--- dotproject/modules/tasks/ae_resource.php
+++ dotproject/modules/tasks/ae_resource.php
@@ -98,58 +98,7 @@ for ($i = 1; $i < sizeof($keys); $i++) {
 		<textarea name="email_comment" class="textarea" cols="60" rows="10" wrap="virtual"></textarea><br />
 		<input type="checkbox" name="task_notify" value="1" <?php if($obj->task_notify!="0") echo "checked"?> /> <?php echo $AppUI->_( 'notifyChange' );?>
 		</td></tr></table><br />
-		<?php
-			error_reporting(E_ALL);
-			require_once("./classes/customfieldsparser.class.php");
-			// let's create the parser
-			$cfp = new CustomFieldsParser("TaskCustomFields", $obj->task_id);
-			
-			// we will need the amount of record types
-			$amount_task_record_types = count($cfp->custom_record_types);
-		?>
 		
-		<?php
-			// let's parse the custom fields form table
-			echo $cfp->parseTableForm(true);
-		?>
-		
-		<script language="javascript">
-		    var task_types;
-		    
-		    // We need to create an array of all the names
-		    // of the record types in JS so we can map the Key to the type name (used in the field filter)
-		    task_types = new Array(<?php echo $amount_task_record_types; ?>);
-		    
-		    <?php
-		    	foreach($cfp->custom_record_types as $key => $record_type){
-		    		echo "task_types[$key] = new String('".$record_type."');\n";
-		    	}
-		    	reset($cfp->custom_record_types);
-		    	if(count($cfp->custom_record_types) == 0){
-		    		$record_type = "";
-		    	} else {
-		    		$record_type = isset($cfp->custom_record_types[$obj->task_type]) ? $cfp->custom_record_types[$obj->task_type] : null;
-		    		if(is_null($record_type)){
-		    			$record_type = current($cfp->custom_record_types);
-		    		}
-		    	}
-		    	
-		    	$actual_record_type = str_replace(" ", "_", $record_type);
-		    	
-		    	// Let's parse all the show functions
-		    	echo $cfp->parseShowFunctions();
-		    ?>
-		    
-		    
-			<?php echo $cfp->showHideAllRowsFunction(); ?>
-			
-			// by default hide everything and show the actual type record
-			<?php echo "\n\nhideAllRows();";
-			      if($actual_record_type != ""){
-				      echo "show$actual_record_type();";
-			      } 
-			?>
-		</script>
 	</td>
 </tr>
 </table>
