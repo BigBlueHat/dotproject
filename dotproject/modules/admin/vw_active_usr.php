3d99195f32773af3f5500c0e63537a7015d9a132
cyberhorse <cyberhorse>
2004-11-11 14:04:36 +0000

    Fix users not showing in user admin because of permissions Fix search and alphabetical listing in user admin (both errors from relying on old permissions system)

--- dotproject/modules/admin/vw_active_usr.php
+++ dotproject/modules/admin/vw_active_usr.php
@@ -7,22 +7,20 @@ SELECT DISTINCT(user_id), user_username, contact_last_name, contact_first_name,
 FROM users
 LEFT JOIN contacts ON user_contact = contact_id
 LEFT JOIN permissions ON user_id = permission_user
-LEFT JOIN companies ON company_id = contact_company
-WHERE permission_value IS NOT NULL
-GROUP BY user_id
-";
+LEFT JOIN companies ON company_id = contact_company";
+//WHERE permission_value IS NOT NULL
 
 if ($stub) {
-	$sql .= "\n	AND (UPPER(user_username) LIKE '$stub%' or UPPER(contact_first_name) LIKE '$stub%' OR UPPER(contact_last_name) LIKE '$stub%')";
+	$sql .= "\n	WHERE (UPPER(user_username) LIKE '$stub%' or UPPER(contact_first_name) LIKE '$stub%' OR UPPER(contact_last_name) LIKE '$stub%')";
 } else if ($where) {
-	$sql .= "\n	AND (UPPER(user_username) LIKE '%$where%' or UPPER(contact_first_name) LIKE '%$where%' OR UPPER(contact_last_name) LIKE '%$where%')";
+	$sql .= "\n	WHERE (UPPER(user_username) LIKE '%$where%' or UPPER(contact_first_name) LIKE '%$where%' OR UPPER(contact_last_name) LIKE '%$where%')";
 }
 
-$sql .= "\nORDER by $orderby";
+$sql .= 	"\nGROUP BY user_id
+	ORDER by $orderby";
 
 $users = db_loadList( $sql );
 $canLogin = true;
 
 require "{$dPconfig['root_dir']}/modules/admin/vw_usr.php";
 ?>
-
--- dotproject/modules/admin/vw_inactive_usr.php
+++ dotproject/modules/admin/vw_inactive_usr.php
@@ -5,15 +5,14 @@ $sql = "
 SELECT DISTINCT(user_id), user_username, contact_last_name, contact_first_name, permission_user, contact_email, company_name, contact_company
 FROM users
 LEFT JOIN contacts ON contact_id = user_contact
-LEFT JOIN permissions ON user_id = permission_user 
+LEFT JOIN permissions ON user_id = permission_user
 LEFT JOIN companies ON company_id = contact_company
-WHERE permission_value IS NULL
 ";
 
 if ($stub) {
-	$sql .= "\n	AND (UPPER(user_username) LIKE '$stub%' or UPPER(contact_first_name) LIKE '$stub%' OR UPPER(contact_last_name) LIKE '$stub%')";
+	$sql .= "\n	WHERE (UPPER(user_username) LIKE '$stub%' or UPPER(contact_first_name) LIKE '$stub%' OR UPPER(contact_last_name) LIKE '$stub%')";
 } else if ($where) {
-	$sql .= "\n	AND (UPPER(user_username) LIKE '%$where%' or UPPER(contact_first_name) LIKE '%$where%' OR UPPER(contact_last_name) LIKE '%$where%')";
+	$sql .= "\n	WHERE (UPPER(user_username) LIKE '%$where%' or UPPER(contact_first_name) LIKE '%$where%' OR UPPER(contact_last_name) LIKE '%$where%')";
 }
 
 $sql .= "\nORDER by $orderby";
