284d1dd9c7d4fa72229d8f283b262f5dd61ba604
cyberhorse <cyberhorse>
2004-12-11 19:47:14 +0000

    After creating a new user, go directly in the roles section, to assign a role. (number of tab was wrong

--- dotproject/modules/admin/do_user_aed.php
+++ dotproject/modules/admin/do_user_aed.php
@@ -70,6 +70,6 @@ if ($del) {
 		$AppUI->setMsg( $isNewUser ? 'added - <b>please setup roles and permissions now.  User must have at least one role to log in.</b>' : 'updated', UI_MSG_OK, true );
 	}
         }
-	($isNewUser)?$AppUI->redirect("m=admin&a=viewuser&user_id=". $obj->user_id . "&tab=2"):$AppUI->redirect();
+	($isNewUser)?$AppUI->redirect("m=admin&a=viewuser&user_id=". $obj->user_id . "&tab=3"):$AppUI->redirect();
 
 ?>
