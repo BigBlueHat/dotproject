b6b30eae8e4d7af1a419896bd6aa7116666dd3f4
ajdonnison <ajdonnison>
2005-02-07 23:07:26 +0000

    Shortened the alt text (title) of task link so that large descriptions don't cause slow loading of the page.

--- dotproject/modules/tasks/tasks.class.php
+++ dotproject/modules/tasks/tasks.class.php
@@ -1532,7 +1532,7 @@ function showtask( &$a, $level=0, $is_opened = true, $today_view = false) {
 		}
 	}
 // name link
-	$alt = htmlspecialchars( $a["task_description"] );
+	$alt = htmlspecialchars( strlen($a['task_description']) > 80 ? substr($a["task_description"],0,80) . '...' : $a['task_description'] );
 
 	$open_link = $is_opened ? "<a href='index.php$query_string&close_task_id=".$a["task_id"]."'><img src='images/icons/collapse.gif' border='0' align='center' /></a>" : "<a href='index.php$query_string&open_task_id=".$a["task_id"]."'><img src='images/icons/expand.gif' border='0' /></a>";
 	if ($a["task_milestone"] > 0 ) {
