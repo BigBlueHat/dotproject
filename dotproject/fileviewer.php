c7a54c9973426ab0ca720118ef2f32af5bc211f6
ajdonnison <ajdonnison>
2005-02-08 01:20:23 +0000

    Fixed order of includes.

--- dotproject/fileviewer.php
+++ dotproject/fileviewer.php
@@ -1,8 +1,8 @@
 <?php /* $Id$ */
 //file viewer
 require_once "./includes/config.php";
-require_once "./classes/ui.class.php";
 require_once "./includes/main_functions.php";
+require_once "./classes/ui.class.php";
 require_once "./includes/db_adodb.php";
 require_once "./includes/db_connect.php";
 require_once "./includes/session.php";
