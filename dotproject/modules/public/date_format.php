11f3138efb0ee70c160146f4bdcf7fe5b4768a49
cyberhorse <cyberhorse>
2005-02-10 08:00:51 +0000

    to be used in a thread to fill in a field with the user formatted date.

---
+++ dotproject/modules/public/date_format.php
@@ -0,0 +1,17 @@
+<?php
+require_once( "./classes/ui.class.php" );
+require_once $AppUI->getSystemClass('date');
+$df = $AppUI->getPref( 'SHDATEFORMAT' );;
+$date = $_GET['date'];
+$field = $_GET['field'];
+$this_day = new CDate($date);
+$formatted_date = $this_day->format( $df );
+?>
+
+<script language="JavaScript" type="text/javascript">
+<!--
+	window.parent.document.<?php echo $field; ?>.value = '<?php echo $formatted_date; ?>';
+//	window.close();
+//-->
+</script>
+
