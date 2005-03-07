505670315f93d0aeedb88ebfddacaf7ed48306c5
cyberhorse <cyberhorse>
2005-03-07 01:57:33 +0000

    no need to call the savetab function when we have javascript tabs.

--- dotproject/modules/tasks/addedit.php
+++ dotproject/modules/tasks/addedit.php
@@ -335,7 +335,7 @@ var daily_working_hours = <?php echo dPgetConfig('daily_working_hours'); ?>;
 	if (isset($_GET['tab']))
 	  $AppUI->setState('TaskAeTabIdx', dPgetParam($_GET, 'tab', 0));
 	$tab = $AppUI->getState('TaskAeTabIdx', 0);
-	$tabBox =& new CTabBox("?m=tasks&a=addedit&task_id=$task_id", "", $tab, "saveTab");
+	$tabBox =& new CTabBox("?m=tasks&a=addedit&task_id=$task_id", "", $tab, "");
 	$tabBox->add("{$dPconfig['root_dir']}/modules/tasks/ae_desc", "Details");
         $tabBox->add("{$dPconfig['root_dir']}/modules/tasks/ae_dates", "Dates");
 	$tabBox->add("{$dPconfig['root_dir']}/modules/tasks/ae_depend", "Dependencies");
