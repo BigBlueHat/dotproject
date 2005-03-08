350190d441675d2a0f791ceabbe10d50ea7a6317
ajdonnison <ajdonnison>
2005-03-08 01:36:16 +0000

    Changed set timer logic so that a separate button isn't required, simply edit the hours field and hit Start.  It also allows saved task logs to be restarted from where they leave off.

--- dotproject/modules/tasks/vw_log_update.php
+++ dotproject/modules/tasks/vw_log_update.php
@@ -90,6 +90,7 @@ if ($canEdit) {
 	
 	function timerStart() {
 		if(!timerID){ // this means that it needs to be started
+			timerSet();
 			document.editFrm.timerStartStopButton.value = "<?php echo $AppUI->_('Stop');?>";
             UpdateTimer();
 		} else { // timer must be stoped
@@ -113,7 +114,7 @@ if ($canEdit) {
 	}
 
 	function timerSet() {
-		total_minutes = Math.round(document.editFrm.task_log_hours.value * 60);
+		total_minutes = Math.floor(document.editFrm.task_log_hours.value * 60);
 	}
 	
 </script>
@@ -176,7 +177,6 @@ if ($canEdit) {
 		<input type="text" class="text" name="task_log_hours" value="<?php echo $log->task_log_hours;?>" maxlength="8" size="6" /> 
 		<input type='button' class="button" value='<?php echo $AppUI->_('Start');?>' onclick='javascript:timerStart()' name='timerStartStopButton' />
 		<input type='button' class="button" value='<?php echo $AppUI->_('Reset'); ?>' onclick="javascript:timerReset()" name='timerResetButton' /> 
-		<input type='button' class="button" value='<?php echo $AppUI->_('Set'); ?>' onclick="javascript:timerSet()" name='timerSetButton' /> 
 		<span id='timerStatus'></span>
 	</td>
 </tr>
