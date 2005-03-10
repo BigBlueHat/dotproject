e9479f4711110824d6886720bec8e7673f574a1f
cyberhorse <cyberhorse>
2005-03-10 13:26:08 +0000

    fixed project selection combo box to keep the report you are in and to disable reports for all projects (where supported).

--- dotproject/modules/projects/reports.php
+++ dotproject/modules/projects/reports.php
@@ -32,7 +32,7 @@ GROUP BY project_id
 ORDER BY project_short_name
 ";
                                                                                 
-$project_list=array("0"=> $AppUI->_("Select Project") );
+$project_list=array("0"=> $AppUI->_("All", UI_OUTPUT_RAW) );
 $ptrc = db_exec($sql);
 $nums=db_num_rows($ptrc);
 echo db_error();
@@ -69,10 +69,14 @@ if (! $suppressHeaders) {
 }
 
 if (!isset($display_project_name)) $display_project_name = "None"; ?>
-<?php echo $AppUI->_('Selected Project') . ": <b>".$display_project_name."</b>"; ?>
-<form name="changeMe" action="./index.php?m=projects&a=reports" method="post">
+<?php echo $AppUI->_('Selected Project') . ": <b>".$display_project_name."</b>"; 
+$report_type_var = dPgetParam($_GET, 'report_type', '');
+if (!empty($report_type_var))
+	$report_type_var = '&report_type=' . $report_type;
+?>
+<form name="changeMe" action="./index.php?m=projects&a=reports<?php echo $report_type_var; ?>" method="post">
 <?php echo $AppUI->_('Projects') . ':';?>
-<?php echo arraySelect( $project_list, 'project_id', 'size="1" class="text" onchange="changeIt();"', $obj->project_status, false );?>
+<?php echo arraySelect( $project_list, 'project_id', 'size="1" class="text" onchange="changeIt();"', $project_id, false );?>
 </form>
 
 <?php
