c3947cc6a36d354f61f7e415c126c2082e03ecbc
ajdonnison <ajdonnison>
2005-03-11 01:34:46 +0000

    Changed license from BSD to GPL.

--- dotproject/includes/config-dist.php
+++ dotproject/includes/config-dist.php
@@ -1,43 +1,34 @@
 <?php /* $Id$ */
 
-/**  BSD LICENSE  **
+/*
+Copyright (c) 2003-2005 The dotProject Development Team <core-developers@dotproject.net>
 
-Copyright (c) 2003, The dotProject Development Team sf.net/projects/dotproject
-All rights reserved.
+    This file is part of dotProject.
 
-Redistribution and use in source and binary forms, with or without modification,
-are permitted provided that the following conditions are met:
+    dotProject is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
 
-* Redistributions of source code must retain the above copyright notice,
-  this list of conditions and the following disclaimer.
-* Redistributions in binary form must reproduce the above copyright notice,
-  this list of conditions and the following disclaimer in the documentation
-  and/or other materials provided with the distribution.
-* Neither the name of the dotproject development team (past or present) nor the
-  names of its contributors may be used to endorse or promote products derived
-  from this software without specific prior written permission.
+    dotProject is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
 
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
-FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+    You should have received a copy of the GNU General Public License
+    along with dotProject; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
-**/
+The full text of the GPL is in the COPYING file.
+*/
 
 /*
 	* * * INSTALLATION INSTRUCTIONS * * *
 
-	YOU MUST customise "config-dist.php" to your local system:
+  Point your browser to install/index.php and follow the prompts.
+	It is no longer necessary to manually create this file unless
+	the web server cannot write to the includes directory.
 
-	1) COPY config-dist.php to "config.php" [if it doesn't exist]
-
-	2) EDIT "config.php" to include your database connection and other local settings.
 */
 
 // DATABASE ACCESS INFORMATION [DEFAULT example]
