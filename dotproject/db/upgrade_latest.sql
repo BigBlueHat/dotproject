4b8af020e2595cfe21f48b36f4751d27ce5be2a6
ajdonnison <ajdonnison>
2005-03-16 06:11:58 +0000

    Removed config items that were no longer used in code.

--- dotproject/db/upgrade_latest.sql
+++ dotproject/db/upgrade_latest.sql
@@ -5,4 +5,7 @@
 #
 # All entries must be date stamped in the correct format.
 #
-#
+# 20050316
+# Remove config elements that are no longer used.
+DELETE FROM `config` where `config_name` = 'cal_day_view_show_minical';
+DELETE FROM `config` where `config_name` = 'show_all_tasks';
