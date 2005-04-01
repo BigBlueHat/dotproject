c0635c1b196b9fa495b2a67d2d67a913899f6dc0
gregorerhardt <gregorerhardt>
2005-04-01 09:13:12 +0000

    703: page titles different from dotProject string

--- dotproject/style/dp-grey-theme/header.php
+++ dotproject/style/dp-grey-theme/header.php
@@ -3,7 +3,8 @@ $dialog = dPgetParam( $_GET, 'dialog', 0 );
 if ($dialog)
 	$page_title = '';
 else
-	$page_title = $dPconfig['page_title'] . '&nbsp;' . $AppUI->getVersion();
+	$page_title = ($dPconfig['page_title'] == 'dotProject') ? $dPconfig['page_title'] . '&nbsp;' . $AppUI->getVersion() : $dPconfig['page_title'];
+
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
 	   "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
