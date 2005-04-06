3ffdca0036128968915e3d12891bda2233589830
gregorerhardt <gregorerhardt>
2005-04-06 23:10:51 +0000

    raw output for translation support in sendpass

--- dotproject/includes/sendpass.php
+++ dotproject/includes/sendpass.php
@@ -10,59 +10,59 @@
 // www.mamboserver.com | mosforge.net
 //
 function sendNewPass() {
-	global $AppUI, $dPconfig;
+ global $AppUI, $dPconfig;
 
-	$_live_site = $dPconfig['base_url'];
-	$_sitename = $dPconfig['company_name'];
+ $_live_site = $dPconfig['base_url'];
+ $_sitename = $dPconfig['company_name'];
 
-	// ensure no malicous sql gets past
-	$checkusername = trim( dPgetParam( $_POST, 'checkusername', '') );
-	$checkusername = db_escape( $checkusername );
-	$confirmEmail = trim( dPgetParam( $_POST, 'checkemail', '') );
-	$confirmEmail = strtolower( db_escape( $confirmEmail ) );
+ // ensure no malicous sql gets past
+ $checkusername = trim( dPgetParam( $_POST, 'checkusername', '') );
+ $checkusername = db_escape( $checkusername );
+ $confirmEmail = trim( dPgetParam( $_POST, 'checkemail', '') );
+ $confirmEmail = strtolower( db_escape( $confirmEmail ) );
 
-	$query = "SELECT user_id FROM users"
-	." LEFT JOIN contacts ON user_contact = contact_id"
-	. "\nWHERE user_username='$checkusername' AND LOWER(contact_email)='$confirmEmail'"
-	;
-	if (!($user_id = db_loadResult($query)) || !$checkusername || !$confirmEmail) {
-		$AppUI->setMsg( 'Invalid username or email.', UI_MSG_ERROR );
-		$AppUI->redirect();
-	}
-	
-	$newpass = makePass();
-	$message = $AppUI->_('sendpass0')." $checkusername ". $AppUI->_('sendpass1') . " $_live_site  ". $AppUI->_('sendpass2') ." $newpass ". $AppUI->_('sendpass3');
-	$subject = "$_sitename :: ".$AppUI->_('sendpass4')." - $checkusername";
-	$headers = "";
-	$headers .= "From: dotproject\r\n";
-	//$headers .= "Reply-To: <".$adminEmail.">\r\n";
-	$headers .= "X-Priority: 3\r\n";
-	$headers .= "X-MSMail-Priority: Low\r\n";
-	$headers .= "X-Mailer: dotproject\r\n";
-	mail( $confirmEmail, $subject, $message, $headers );
+ $query = "SELECT user_id FROM users"
+ ." LEFT JOIN contacts ON user_contact = contact_id"
+ . "\nWHERE user_username='$checkusername' AND LOWER(contact_email)='$confirmEmail'"
+ ;
+ if (!($user_id = db_loadResult($query)) || !$checkusername || !$confirmEmail) {
+  $AppUI->setMsg( 'Invalid username or email.', UI_MSG_ERROR );
+  $AppUI->redirect();
+ }
+ 
+ $newpass = makePass();
+ $message = $AppUI->_('sendpass0', UI_OUTPUT_RAW)." $checkusername ". $AppUI->_('sendpass1', UI_OUTPUT_RAW) . " $_live_site  ". $AppUI->_('sendpass2', UI_OUTPUT_RAW) ." $newpass ". $AppUI->_('sendpass3', UI_OUTPUT_RAW);
+ $subject = "$_sitename :: ".$AppUI->_('sendpass4', UI_OUTPUT_RAW)." - $checkusername";
+ $headers = "";
+ $headers .= "From: dotproject\r\n";
+ //$headers .= "Reply-To: <".$adminEmail.">\r\n";
+ $headers .= "X-Priority: 3\r\n";
+ $headers .= "X-MSMail-Priority: Low\r\n";
+ $headers .= "X-Mailer: dotproject\r\n";
+ mail( $confirmEmail, $subject, $message, $headers );
 
-	$newpass = md5( $newpass );
-	$sql = "UPDATE users SET user_password='$newpass' WHERE user_id='$user_id'";
-	$cur = db_exec( $sql );
-	if (!$cur) {
-		die("SQL error" . $database->stderr(true));
-	} else {
-		$AppUI->setMsg( 'New User Password created and emailed to you' );
-		$AppUI->redirect();
-	}
+ $newpass = md5( $newpass );
+ $sql = "UPDATE users SET user_password='$newpass' WHERE user_id='$user_id'";
+ $cur = db_exec( $sql );
+ if (!$cur) {
+  die("SQL error" . $database->stderr(true));
+ } else {
+  $AppUI->setMsg( 'New User Password created and emailed to you' );
+  $AppUI->redirect();
+ }
 }
 
 function makePass(){
-	$makepass="";
-	$salt = "abchefghjkmnpqrstuvwxyz0123456789";
-	srand((double)microtime()*1000000);
-	$i = 0;
-	while ($i <= 7) {
-		$num = rand() % 33;
-		$tmp = substr($salt, $num, 1);
-		$makepass = $makepass . $tmp;
-		$i++;
-	}
-	return ($makepass);
+ $makepass="";
+ $salt = "abchefghjkmnpqrstuvwxyz0123456789";
+ srand((double)microtime()*1000000);
+ $i = 0;
+ while ($i <= 7) {
+  $num = rand() % 33;
+  $tmp = substr($salt, $num, 1);
+  $makepass = $makepass . $tmp;
+  $i++;
+ }
+ return ($makepass);
 }
 ?>
