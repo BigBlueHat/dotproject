1bf1d165effed9d67a403c0f2d665a01f86ac7c7
gregorerhardt <gregorerhardt>
2005-04-08 13:06:00 +0000

    restructured todo with tabs to opt-in other things like helpdesk items, task logs etc

--- dotproject/modules/tasks/todo.php
+++ dotproject/modules/tasks/todo.php
@@ -6,6 +6,11 @@ $showEditCheckbox = true;
 $project_on_hold_status = 4;
 $perms =& $AppUI->acl();
 
+if (isset( $_GET['tab'] )) {
+	$AppUI->setState( 'ToDoTab', $_GET['tab'] );
+}
+$tab = $AppUI->getState( 'ToDoTab' ) !== NULL ? $AppUI->getState( 'ToDoTab' ) : 0;
+
 $project_id = intval( dPgetParam( $_GET, 'project_id', 0 ) );
 $date       = intval( dPgetParam( $_GET, 'date', '' ) );
 $user_id    = $AppUI->user_id;
@@ -136,153 +141,19 @@ if (!@$min_view) {
 
 ?>
 
-<table width="100%" border="0" cellpadding="1" cellspacing="0">
-<form name="form_buttons" method="post" action="index.php?<?php echo "m=$m&a=$a&date=$date";?>">
-<input type="hidden" name="show_form" value="1" />
 
+<table cellspacing="0" cellpadding="2" border="0" width="100%">
 <tr>
-	<td width="50%">
-		<?php
-	if ($other_users) {
-		echo $AppUI->_("Show Todo for:");
-		?>
-		<select name="show_user_todo" onchange="document.form_buttons.submit()">
-<?php
-                $usersql = "
-                SELECT user_id, user_username, contact_first_name, contact_last_name
-                FROM users, contacts
-                WHERE user_contact = contact_id
-                ";
-
-
-                if (($rows = db_loadList( $usersql, NULL )))
-                {
-                        foreach ($rows as $row)
-                        {
-                                if ( $user_id == $row["user_id"])
-                                        echo "<OPTION VALUE='".$row["user_id"]."' SELECTED>".$row["user_username"];
-                                else
-                                        echo "<OPTION VALUE='".$row["user_id"]."'>".$row["user_username"];
-			                  }
-							  }
-			}
-		?>
-		</select>
+	<td width="80%" valign="top">
+  <?php
+  // Tabbed information boxes
+  $tabBox = new CTabBox( "?m=tasks&a=todo", "{$dPconfig['root_dir']}/modules/", $tab );
+  $tabBox->add( 'tasks/todo_tasks_sub', $AppUI->_('My Tasks') );
+  if ($AppUI->isActiveModule('helpdesk')){ 
+  $tabBox->add( 'helpdesk/vw_idx_my', 'My Open Requests' );
+  }
+  $tabBox->show();
+  ?>
 	</td>
-	<td align="right" width="50%">
-		<?php echo $AppUI->_('Show'); ?>:
-	</td>
-	<td>
-		<input type=checkbox name="show_pinned" onclick="document.form_buttons.submit()" <?php echo $showPinned ? 'checked="checked"' : ""; ?> />
-	</td>
-	<td nowrap="nowrap">
-		<?php echo $AppUI->_('Pinned Only'); ?>
-	</td>
-	<td>
-		<input type=checkbox name="show_arc_proj" onclick="document.form_buttons.submit()" <?php echo $showArcProjs ? 'checked="checked"' : ""; ?> />
-	</td>
-	<td nowrap="nowrap">
-		<?php echo $AppUI->_('Archived Projects'); ?>
-	</td>
-	<td>
-		<input type=checkbox name="show_hold_proj" onclick="document.form_buttons.submit()" <?php echo $showHoldProjs ? 'checked="checked"' : ""; ?> />
-	</td>
-    <td nowrap="nowrap">
-		<?php echo $AppUI->_('Projects on Hold'); ?>
-	</td>
-	<td>
-		<input type=checkbox name="show_dyn_task" onclick="document.form_buttons.submit()" <?php echo $showDynTasks ? 'checked="checked"' : ""; ?> />
-	</td>
-	<td nowrap="nowrap">
-		<?php echo $AppUI->_('Dynamic Tasks'); ?>
-	</td>
-	<td>
-		<input type=checkbox name="show_low_task" onclick="document.form_buttons.submit()" <?php echo $showLowTasks ? 'checked="checked"' : ""; ?> />
-	</td>
-	<td nowrap="nowrap">
-		<?php echo $AppUI->_('Low Priority Tasks'); ?>
-	</td>
-	<td>
-		<input type=checkbox name="show_empty_date" onclick="document.form_buttons.submit()" <?php echo $showEmptyDate ? 'checked="checked"' : ""; ?> />
-	</td>
-	<td nowrap="nowrap">
-		<?php echo $AppUI->_('Empty Dates'); ?>
-	</td>
-</tr>
-</form>
-</table>
-
-<table width="100%" border="0" cellpadding="2" cellspacing="1" class="tbl">
-<form name="form" method="post" action="index.php?<?php echo "m=$m&a=$a&date=$date";?>">
-<tr>
-	<th width='10'>&nbsp;</th>
-	<th width='10'><?php echo $AppUI->_('Pin'); ?></th>
-	<th width="20" colspan="2"><?php echo $AppUI->_('Progress');?></th>
-	<th width="15" align="center"><?php echo $AppUI->_('P');?></th>
-	<th colspan="2"><?php echo $AppUI->_('Task / Project');?></th>
-	<th nowrap><?php echo $AppUI->_('Start Date');?></th>
-	<th nowrap><?php echo $AppUI->_('Duration');?></th>
-	<th nowrap><?php echo $AppUI->_('Finish Date');?></th>
-	<th nowrap><?php echo $AppUI->_('Due In');?></th>
-	<th width="0">&nbsp;</th>
-</tr>
-
-<?php
-
-/*** Tasks listing ***/
-$now = new CDate();
-$df = $AppUI->getPref('SHDATEFORMAT');
-
-foreach ($tasks as $task) {
-	$sign = 1;
-	$start = intval( @$task["task_start_date"] ) ? new CDate( $task["task_start_date"] ) : null;
-	$end = intval( @$task["task_end_date"] ) ? new CDate( $task["task_end_date"] ) : null;
-	
-	if (!$end && $start) {
-		$end = $start;
-		$end->addSeconds( @$task["task_duration"]*$task["task_duration_type"]*SEC_HOUR );
-	}
-
-	if ($end && $now->after( $end )) {
-		$sign = -1;
-	} 
-
-	$days = $end ? $now->dateDiff( $end ) * $sign : null;
-	$task['task_due_in'] = $days;
-
-	showtask($task, 0, false, true);
-
-} ?>
-<tr>
-	<td colspan="9" align="right" height="30">
-		<input type="submit" class="button" value="<?php echo $AppUI->_('update task');?>">
-	</td>
-	<td colspan="3" align="center">
-<?php
-foreach($priorities as $k => $v) {
-	$options[$k] = $AppUI->_('set priority to ' . $v, UI_OUTPUT_RAW);
-}
-$options['c'] = $AppUI->_('mark as finished', UI_OUTPUT_RAW);
-$options['d'] = $AppUI->_('delete', UI_OUTPUT_RAW);
-echo arraySelect( $options, 'task_priority', 'size="1" class="text"', '0' );
-?>
-	</td>
-</form>
-</table>
-
-<table>
-<tr>
-	<td><?php echo $AppUI->_('Key');?>:</td>
-	<td>&nbsp; &nbsp;</td>
-	<td bgcolor="#ffffff">&nbsp; &nbsp;</td>
-	<td>=<?php echo $AppUI->_('Future Task');?></td>
-	<td bgcolor="#e6eedd">&nbsp; &nbsp;</td>
-	<td>=<?php echo $AppUI->_('Started and on time');?></td>
-	<td>&nbsp; &nbsp;</td>
-	<td bgcolor="#ffeebb">&nbsp; &nbsp;</td>
-	<td>=<?php echo $AppUI->_('Should have started');?></td>
-	<td>&nbsp; &nbsp;</td>
-	<td bgcolor="#CC6666">&nbsp; &nbsp;</td>
-	<td>=<?php echo $AppUI->_('Overdue');?></td>
 </tr>
 </table>
