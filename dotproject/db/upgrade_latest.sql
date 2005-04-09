fcf640d46698d8679b78a9c66f64283edd23e6c2
ajdonnison <ajdonnison>
2005-04-09 03:13:55 +0000

    Changes for 2.0.1 release

--- dotproject/db/upgrade_latest.sql
+++ dotproject/db/upgrade_latest.sql
@@ -6,9 +6,3 @@
 # All entries must be date stamped in the correct format.
 #
 
-# 2005 04 04 - minical config var restaured 
-INSERT INTO `config` VALUES ('', 'cal_day_view_show_minical', 'true', '', 'checkbox');
-
-# 2005 04 05 - temporarily reset the memory limit for gantt charts
-INSERT INTO `config` ( `config_id` , `config_name` , `config_value` , `config_group` , `config_type` )
-VALUES ('', 'reset_memory_limit', '8M', '', 'text');
