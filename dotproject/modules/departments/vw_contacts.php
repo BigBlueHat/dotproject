9248e4a41b2ea87c7ce80b5ed3946ff5d6b4521b
ajdonnison <ajdonnison>
2005-04-26 06:02:46 +0000

    Contacts no longer uses a string department name, but an integer, so this query had to be converted.

--- dotproject/modules/departments/vw_contacts.php
+++ dotproject/modules/departments/vw_contacts.php
@@ -7,18 +7,10 @@
 	echo "<tr><th>".$AppUI->_("Name")."</th><th>".$AppUI->_("Email")."</th><th>".$AppUI->_("Telephone")."</th></tr>";
 	
 	$q  = new DBQuery;
-	$q->addTable('departments', 'dep');
-	$q->addQuery('dep.dept_name');
-	$q->addWhere('dep.dept_id = '.$dept_id);
-	$sql = $q->prepare();
-	$q->clear();
-	$contact_department = db_loadResult($sql);
-	
-	$q  = new DBQuery;
 	$q->addTable('contacts', 'con');
 	$q->addQuery('contact_id, con.contact_first_name');
 	$q->addQuery('con.contact_last_name, contact_email, contact_phone');
-	$q->addWhere("contact_department='$contact_department'");
+	$q->addWhere("contact_department='$dept_id'");
 	$q->addWhere("(contact_owner = '$AppUI->user_id' or contact_private = '0')");
 	$q->addOrder('contact_first_name');
 	$contacts = $q->loadHashList("contact_id");
