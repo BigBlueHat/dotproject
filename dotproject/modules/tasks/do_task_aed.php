cb902609798b3c98bd514faaceb7da8549406e67
jcgonz <jcgonz>
2005-04-27 19:43:24 +0000

    Checked to se wether task_dynamic checkbox exists within POST array. If not; then that flag is disabled.

--- dotproject/modules/tasks/do_task_aed.php
+++ dotproject/modules/tasks/do_task_aed.php
@@ -67,6 +67,12 @@ if ($sub_form) {
 	if (isset($_POST['new_task_project']) && $_POST['new_task_project'])
 		$obj->task_project = $_POST['new_task_project'];
 
+		
+    // Let's check if task_dynamic is unchecked
+    if( !array_key_exists("task_dynamic", $_POST) ){
+        $obj->task_dynamic = false;
+    }
+		
 	// Map task_dynamic checkboxes to task_dynamic values for task dependencies.
 	if ( $obj->task_dynamic != 1 ) {
 		$task_dynamic_delay = setItem("task_dynamic_nodelay", '0');
