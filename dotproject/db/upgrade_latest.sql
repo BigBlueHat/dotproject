00af220932aa37903773e33976f953db8424a04f
ajdonnison <ajdonnison>
2005-05-19 05:30:07 +0000

    Added task reminders

--- dotproject/db/upgrade_latest.sql
+++ dotproject/db/upgrade_latest.sql
@@ -9,3 +9,7 @@
 # 20050516
 UPDATE `config` SET `config_name` = 'admin_user' WHERE `config_name` = 'admin_username';
 
+# 20050519
+INSERT INTO `config` VALUES ('', 'task_reminder_control', 'false', 'task_reminder', 'checkbox');
+INSERT INTO `config` VALUES ('', 'task_reminder_days_before', '1', 'task_reminder', 'text');
+INSERT INTO `config` VALUES ('', 'task_reminder_repeat', '100', 'task_reminder', 'text');
