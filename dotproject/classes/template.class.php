85a90af6976bfa54cbc282639081b738d553deca
cyberhorse <cyberhorse>
2005-07-31 03:50:11 +0000

    added a new place for dp introduced smarty plugins functions

--- dotproject/classes/template.class.php
+++ dotproject/classes/template.class.php
@@ -3,14 +3,18 @@ require_once($baseDir . '/lib/smarty/Smarty.class.php');
 
 class CTemplate extends Smarty
 {
+//	var $plugins_dir = array('/var/www/html/dotproject/includes/smarty');
+
 	function CTemplate()
 	{
 		global $AppUI, $baseDir;
-		
+	
+		parent::Smarty();
 		// $this->template_dir = $baseDir . '/style/' . $AppUI->getPref('template');
 		$this->template_dir = $baseDir . '/style/smarty1';
 		$this->compile_dir	= $baseDir . '/files/cache/smarty_templates';
 		$this->cache_dir		= $baseDir . '/files/cache/smarty';
+		$this->plugins_dir[]= $baseDir . '/includes/smarty';
 	}
 	
 	function displayList($module, $rows, $show = null)
