6e2dff56da96fc454bc0f1fb7e92397f4c376d44
cyberhorse <cyberhorse>
2005-09-07 09:43:06 +0000

    smartified the viewuser page in admin module.

--- dotproject/modules/admin/viewuser.php
+++ dotproject/modules/admin/viewuser.php
@@ -60,93 +60,11 @@ function popChgPwd() {
 <?php } ?>
 </script>
 
-<table border="0" cellpadding="4" cellspacing="0" width="100%" class="std">
-<tr valign="top">
-	<td width="50%">
-		<table cellspacing="1" cellpadding="2" border="0" width="100%">
-		<tr>
-			<td align="right" nowrap><?php echo $AppUI->_('Login Name');?>:</td>
-			<td class="hilite" width="100%"><?php echo $user["user_username"];?></td>
-		</tr>
-		<tr>
-			<td align="right" nowrap><?php echo $AppUI->_('User Type');?>:</td>
-			<td class="hilite" width="100%"><?php echo $AppUI->_($utypes[$user["user_type"]]);?></td>
-		</tr>
-		<tr>
-			<td align="right" nowrap><?php echo $AppUI->_('Real Name');?>:</td>
-			<td class="hilite" width="100%"><?php echo $user["contact_first_name"].' '.$user["contact_last_name"];?></td>
-		</tr>
-		<tr>
-			<td align="right" nowrap><?php echo $AppUI->_('Company');?>:</td>
-			<td class="hilite" width="100%">
-				<a href="?m=companies&a=view&company_id=<?php echo @$user["contact_company"];?>"><?php echo @$user["company_name"];?></a>
-			</td>
-		</tr>
-		<tr>
-			<td align="right" nowrap><?php echo $AppUI->_('Department');?>:</td>
-			<td class="hilite" width="100%">
-				<a href="?m=departments&a=view&dept_id=<?php echo @$user["contact_department"];?>"><?php echo $user["dept_name"];?></a>
-			</td>
-		</tr>
-		<tr>
-			<td align="right" nowrap><?php echo $AppUI->_('Phone');?>:</td>
-			<td class="hilite" width="100%"><?php echo @$user["contact_phone"];?></td>
-		</tr>
-		<tr>
-			<td align="right" nowrap><?php echo $AppUI->_('Home Phone');?>:</td>
-			<td class="hilite" width="100%"><?php echo @$user["contact_phone2"];?></td>
-		</tr>
-		<tr>
-			<td align="right" nowrap><?php echo $AppUI->_('Mobile');?>:</td>
-			<td class="hilite" width="100%"><?php echo @$user["contact_mobile"];?></td>
-		</tr>
-		<tr valign=top>
-			<td align="right" nowrap><?php echo $AppUI->_('Address');?>:</td>
-			<td class="hilite" width="100%"><?php
-				echo @$user["contact_address1"]
-					.( ($user["contact_address2"]) ? '<br />'.$user["contact_address2"] : '' )
-					.'<br />'.$user["contact_city"]
-					.'&nbsp;&nbsp;'.$user["contact_state"]
-					.'&nbsp;&nbsp;'.$user["contact_zip"]
-					.'<br />'.$user["contact_country"]
-					;
-			?></td>
-		</tr>
-		</table>
 
-	</td>
-	<td width="50%">
-		<table width="100%">
-		<tr>
-			<td align="right" nowrap><?php echo $AppUI->_('Birthday');?>:</td>
-			<td class="hilite" width="100%"><?php echo @$user["contact_birthday"];?></td>
-		</tr>
-		<tr>
-			<td align="right" nowrap>ICQ#:</td>
-			<td class="hilite" width="100%"><?php echo @$user["contact_icq"];?></td>
-		</tr>
-		<tr>
-			<td align="right" nowrap>AOL Nick:</td>
-			<td class="hilite" width="100%"><a href="aim:<?php echo @$user["contact_aol"];?>"><?php echo @$user["contact_aol"];?></a></td>
-		</tr>
-		<tr>
-			<td align="right" nowrap><?php echo $AppUI->_('Email');?>:</td>
-			<td class="hilite" width="100%"><?php echo '<a href="mailto:'.@$user["contact_email"].'">'.@$user["contact_email"].'</a>';?></td>
-		</tr>
-		<tr>
-			<td colspan="2"><strong><?php echo $AppUI->_('Signature');?>:</strong></td>
-		</tr>
-		<tr>
-			<td class="hilite" width="100%" colspan="2">
-				<?php echo str_replace( chr(10), "<br />", $user["user_signature"]);?>&nbsp;
-			</td>
-		</tr>
-		</table>
-	</td>
-</tr>
-</table>
 
 <?php
+	$user['user_type_name'] = $utypes[$user['user_type']];
+	$tpl->displayView($user);
 	// tabbed information boxes
 	$tabBox = new CTabBox( "?m=admin&a=viewuser&user_id=$user_id", "{$dPconfig['root_dir']}/modules/admin/", $tab );
 	$tabBox->add( 'vw_usr_proj', 'Owned Projects' );
