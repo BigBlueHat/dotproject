3138139124ee59473f724d14998af27476c5921c
mosen <mosen>
2005-10-20 23:24:51 +0000

    Added smarty template for custom field editor - custom field listing page

--- dotproject/modules/system/custom_field_editor.php
+++ dotproject/modules/system/custom_field_editor.php
@@ -1,8 +1,10 @@
 <?php
-	/*
-	 *	Custom Field Editor (NEW)
-	 *
-	 */
+/*	$Id$ 	
+ *
+ *	Custom field editor - lists custom fields by module 	
+ *
+ */
+
 if (!$canEdit) {
     $AppUI->redirect( "m=public&a=access_denied" );
 }
@@ -17,6 +19,7 @@ if (!$canEdit) {
 
 	$titleBlock->show();
 
+	// Load module list
 	$q  = new DBQuery;
 	$q->addTable('modules');
 	$q->addOrder('mod_ui_order');
@@ -24,33 +27,20 @@ if (!$canEdit) {
 	$modules = $q->loadList();
 	$q->clear();
 	
-	echo "<table cellpadding=\"2\">";
+	$module_fields = Array();
 
-	foreach ($modules as $module)
+	foreach ($modules as $i=>$module)
 	{
-		echo "<tr><td colspan=\"4\">";
-		echo "<h3>".$AppUI->_($module["mod_name"])."</h3>";
-		echo "</td></tr>";
+		// Load field list for each module
+		$q = new DBQuery;
+                $q->addTable('custom_fields_struct');
+                $q->addWhere("field_module = '".strtolower($module["mod_name"])."'");
+                $custom_fields = $q->loadList();
 
-		echo "<tr><td colspan=\"4\">";
-		echo "<a href=\"?m=system&a=custom_field_addedit&module=".$module["mod_name"]."\"><img src='./images/icons/stock_new.png' align='center' width='16' height='16' border='0'>".$AppUI->_('Add a new Custom Field to this Module')."</a><br /><br />";
-		echo "</td></tr>";
-		
-		$q  = new DBQuery;
-		$q->addTable('custom_fields_struct');
-		$q->addWhere("field_module = '".strtolower($module["mod_name"])."'");
-		$custom_fields = $q->loadList();
-		$q->clear();
+		$modules[$i]['custom_fields'] = $custom_fields;
+                $q->clear();
+	}	
 
-		foreach ($custom_fields as $f)
-		{
-			echo "<tr><td class=\"hilite\">";
-			echo "<a href=\"?m=system&a=custom_field_addedit&module=".$module["mod_name"]."&field_id=".$f["field_id"]."\"><img src='./images/icons/stock_edit-16.png' align='center' width='16' height='16' border='0'>Edit</a>";
-			echo "</td><td class=\"hilite\">";
-			echo "<a href=\"?m=system&a=custom_field_addedit&field_id=".$f["field_id"]."&delete=1\"><img src='./images/icons/stock_delete-16.png' align='center' width='16' height='16' border='0'>Delete</a> ";
-			echo "</td><td class=\"hilite\">";
-			echo stripslashes($f["field_description"])."\n";
-			echo "</td></tr>";
-		}
-	}
+	$tpl->assign('modules', $modules);
+	$tpl->displayFile('custom_field_editor');
 ?>
