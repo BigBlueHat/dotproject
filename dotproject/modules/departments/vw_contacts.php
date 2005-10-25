8645e5a46e13ea192a5eaabb8638b5bc6c40b1a2
mosen <mosen>
2005-10-25 04:04:07 +0000

    added template for vw_contacts tab in departments

--- dotproject/modules/departments/vw_contacts.php
+++ dotproject/modules/departments/vw_contacts.php
@@ -1,10 +1,5 @@
 <?php
-	global $dept_id;
-?>
-
-<table border="0" cellpadding="2" cellspacing="1" width="100%" class="tbl">
-<?php
-	echo "<tr><th>".$AppUI->_("Name")."</th><th>".$AppUI->_("Email")."</th><th>".$AppUI->_("Telephone")."</th></tr>";
+	global $dept_id, $tpl;
 	
 	$q  = new DBQuery;
 	$q->addTable('contacts', 'con');
@@ -14,11 +9,7 @@
 	$q->addWhere("(contact_owner = '$AppUI->user_id' or contact_private = '0')");
 	$q->addOrder('contact_first_name');
 	$contacts = $q->loadHashList("contact_id");
-	
-	foreach($contacts as $contact_id => $contact_data){
-		echo "<tr><td>".$contact_data["contact_first_name"]." ".$contact_data["contact_last_name"]."</td>";
-		echo "<td>".$contact_data["contact_email"]."</td>";
-		echo "<td>".$contact_data["contact_phone"]."</td></tr>";
-	}
+
+	$tpl->assign('contacts', $contacts);
+	$tpl->displayFile('vw_contacts'); 
 ?>
-</table>
