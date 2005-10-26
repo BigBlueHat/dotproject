9968b662ca8757871efeae88f09e75ed97b1ff34
mosen <mosen>
2005-10-26 07:44:34 +0000

    Added smarty template for forums addedit page

--- dotproject/modules/forums/addedit.php
+++ dotproject/modules/forums/addedit.php
@@ -55,112 +55,25 @@ if ($canDelete) {
 	);
 }
 $titleBlock->show();
+
+$tpl->assign('forum_id', $forum_id);
+$tpl->assign('forum_unique_update', uniqid(""));
+
+if ($AppUI->user_id == $forum_info["forum_owner"] || $forum_id == 0)
+{
+	$tpl->assign('show_submit_button', TRUE);
+}
+else
+{
+	$tpl->assign('show_submit_button', FALSE);
+}
+
+$tpl->assign('projects', $projects);
+$tpl->assign('users', $users);
+
+$forum_owner_id = $forum_info['forum_owner'] ? $forum_info['forum_owner'] : $AppUI->user_id;
+$tpl->assign('forum_owner_id', $forum_owner_id);
+
+$tpl->displayAddEdit($forum_info);
+
 ?>
-<script language="javascript">
-function submitIt(){
-	var form = document.changeforum;
-	if(form.forum_name.value.search(/^\s*$/) >= 0 ) {
-		alert("<?php echo $AppUI->_('forumName', UI_OUTPUT_JS);?>");
-		form.forum_name.focus();
-	} else if(form.forum_project.selectedIndex < 1) {
-		alert("<?php echo $AppUI->_('forumSelectProject', UI_OUTPUT_JS);?>");
-		form.forum_project.focus();
-	} else if(form.forum_owner.selectedIndex < 1) {
-		alert("<?php echo $AppUI->_('forumSelectOwner', UI_OUTPUT_JS);?>");
-		form.forum_owner.focus();
-	} else {
-		form.submit();
-	}
-}
-
-function delIt(){
-	var form = document.changeforum;
-	if (confirm( "<?php echo $AppUI->_('forumDeleteForum', UI_OUTPUT_JS);?>" )) {
-		form.del.value="<?php echo $forum_id;?>";
-		form.submit();
-	}
-}
-</script>
-
-<table cellspacing="0" cellpadding="4" border="0" width="100%" class="std">
-<form name="changeforum" action="?m=forums" method="post">
-	<input type="hidden" name="dosql" value="do_forum_aed" />
-	<input type="hidden" name="del" value="0" />
-	<input type="hidden" name="forum_unique_update" value="<?php echo uniqid("");?>" />
-	<input type="hidden" name="forum_id" value="<?php echo $forum_id;?>" />
-
-<tr height="20">
-	<th valign="top" colspan="3">
-		<strong><?php
-		echo $AppUI->_( $forum_id ? 'Edit' : 'Add' ).' '.$AppUI->_( 'Forum' );
-		?></strong>
-	</th>
-</tr>
-<tr>
-	<td valign="top" width="50%">
-		<strong><?php echo $AppUI->_('Details');?></strong>
-		<table cellspacing="1" cellpadding="2" width="100%">
-		<tr>
-			<td align="right" width="100"><?php echo $AppUI->_('Forum Name');?>:</td>
-			<td>
-				<input type="text" class="text" size=25 name="forum_name" value="<?php echo @$forum_info["forum_name"];?>" maxlength="50" style="width:200px;" />
-			</td>
-		</tr>
-		<tr>
-			<td align="right"><?php echo $AppUI->_('Related Project');?></td>
-			<td>
-		<?php
-			echo arraySelect( $projects, 'forum_project', 'size="1" class="text"', $forum_info['forum_project'] );
-		?>
-			</td>
-		</tr>
-		<tr>
-			<td align="right"><?php echo $AppUI->_('Owner');?>:</td>
-			<td>
-		<?php
-			echo arraySelect( $users, 'forum_owner', 'size="1" class="text"', $forum_info['forum_owner'] ? $forum_info['forum_owner'] : $AppUI->user_id );
-		?>
-			</td>
-		</tr>
-		<tr>
-			<td align="right" nowrap><?php echo $AppUI->_('Moderator');?>:</td>
-			<td>
-		<?php
-			echo arraySelect( $users, 'forum_moderated', 'size="1" class="text"', $forum_info['forum_moderated'] );
-		?>
-			</td>
-		</tr>
-		<?php if ($forum_id) { ?>
-		<tr>
-			<td align="right"><?php echo $AppUI->_('Created On');?></td>
-			<td bgcolor="#ffffff"><?php echo @$forum_info["forum_create_date"];?></td>
-		</tr>
-		<tr>
-			<td align="right"><?php echo $AppUI->_('Last Post');?>:</td>
-			<td bgcolor="#ffffff"><?php echo @$forum_info["forum_last_date"];?></td>
-		</tr>
-		<tr>
-			<td align="right" nowrap><?php echo $AppUI->_('Message Count');?>:</td>
-			<td bgcolor="#ffffff"><?php echo @$forum_info["forum_message_count"];?></td>
-		</tr>
-		<?php } ?>
-		</table>
-	</td>
-	<td valign="top" width="50%">
-		<strong><?php echo $AppUI->_('Description');?></strong><br />
-		<textarea class="textarea" name="forum_description" style="height:150px;"><?php echo @$forum_info["forum_description"];?></textarea>
-	</td>
-</tr>
-
-
-<tr>
-	<td align="left">
-		<input type="button" value="<?php echo $AppUI->_('back');?>" class=button onclick="javascript:window.location='./index.php?m=forums';" />
-	</td>
-	<td align="right" colspan="2"><?php
-		if ($AppUI->user_id == $forum_info["forum_owner"] || $forum_id ==0) {
-			echo '<input type="button" value="'.$AppUI->_('submit').'" class=button onclick="submitIt()" />';
-		}?></td>
-</tr>
-</form>
-</table>
