0e8c43fc2dabae83fa8fccdaeedd6af040643540
mosen <mosen>
2005-10-26 07:03:21 +0000

    Added smarty templates for forums listing

--- dotproject/modules/forums/index.php
+++ dotproject/modules/forums/index.php
@@ -85,105 +85,52 @@ if ($canEdit) {
 	);
 }
 $titleBlock->show();
-?>
 
-<table width="100%" cellspacing="1" cellpadding="2" border="0" class="tbl">
-<form name="watcher" action="./index.php?m=forums&f=<?php echo $f;?>" method="post">
-<tr>
-	<th nowrap="nowrap">&nbsp;</th>
-	<th nowrap="nowrap" width="25"><a href="?m=forums&orderby=watch_user" class="hdr"><?php echo $AppUI->_( 'Watch' );?></a></th>
-	<th nowrap="nowrap"><a href="?m=forums&orderby=forum_name" class="hdr"><?php echo $AppUI->_( 'Forum Name' );?></a></th>
-	<th nowrap="nowrap" width="50" align="center"><a href="?m=forums&orderby=forum_topics" class="hdr"><?php echo $AppUI->_( 'Topics' );?></a></th>
-	<th nowrap="nowrap" width="50" align="center"><a href="?m=forums&orderby=forum_replies" class="hdr"><?php echo $AppUI->_( 'Replies' );?></a></th>
-	<th nowrap="nowrap" width="200"><a href="?m=forums&orderby=forum_last_date" class="hdr"><?php echo $AppUI->_( 'Last Post Info' );?></a></th>
-</tr>
-<?php
+$tpl->assign('f', $f);
+
 $p ="";
+$forum_rows_html = "";
 $now = new CDate();
 foreach ($forums as $row) {
+	$tpl_row = new CTemplate();
+	$tpl_row->assign('row', $row);
+	
 	$message_date = intval( $row['forum_last_date'] ) ? new CDate( $row['forum_last_date'] ) : null;
 
 	if($p != $row["forum_project"]) {
+		$tpl_row->assign('show_project_header', TRUE);
 		$create_date = intval( $row['forum_create_date'] ) ? new CDate( $row['forum_create_date'] ) : null;
-?>
-<tr>
-	<td colspan="6" style="background-color:#<?php echo $row["project_color_identifier"];?>">
-		<a href="?m=projects&a=view&project_id=<?php echo $row["forum_project"];?>">
-			<font color=<?php echo bestColor( $row["project_color_identifier"] );?>>
-			<strong><?php echo $row["project_name"];?></strong>
-			</font>
-		</a>
-	</td>
-</tr>
-	<?php
 		$p = $row["forum_project"];
-	}?>
-<tr>
-	<td nowrap="nowrap" align="center">
-	<?php if ( $row["forum_owner"] == $AppUI->user_id || $perms->checkModuleItem('forums', 'edit', $row['forum_id']) ) { ?>
-		<a href="?m=forums&a=addedit&forum_id=<?php echo $row["forum_id"];?>" title="<?php echo $AppUI->_('edit');?>">
-		<?php echo dPshowImage( './images/icons/stock_edit-16.png', 16, 16, '' );?>
-		</a>
-	<?php } 
-		if ( $row['visit_count'] != $row['message_count'] ) {
-			echo "&nbsp;" . dPshowImage('./images/icons/stock_new_small.png',false,false, "You have unread messages in this forum");
-		}
-	?>
-	</td>
+	}
+	if ( $row["forum_owner"] == $AppUI->user_id || $perms->checkModuleItem('forums', 'edit', $row['forum_id']) ) { 
+		$tpl_row->assign('show_edit_controls', TRUE); 
+	} 
 
-	<td nowrap="nowrap" align="center">
-		<input type="checkbox" name="forum_<?php echo $row['forum_id'];?>" <?php echo $row['watch_user'] ? 'checked' : '';?> />
-	</td>
+	$formatted_create_date = $create_date->format( $df );
+	$tpl_row->assign('create_date', $formatted_create_date);
 
-	<td>
-		<span style="font-size:10pt;font-weight:bold">
-			<a href="?m=forums&a=viewer&forum_id=<?php echo $row["forum_id"];?>"><?php echo $row["forum_name"];?></a>
-		</span>
-		<br /><?php echo $row["forum_description"];?>
-		<br /><font color="#777777"><?php echo $AppUI->_( 'Owner' ).' '.$row["user_username"];?>,
-		<?php echo $AppUI->_( 'Started' ).' '.$create_date->format( $df );?>
-		</font>
-	</td>
-	<td nowrap="nowrap" align="center"><?php echo $row["forum_topics"];?></td>
-	<td nowrap="nowrap" align="center"><?php echo $row["forum_replies"];?></td>
-	<td width="225">
-<?php
 	if ($message_date !== null) {
-		echo $message_date->format( "$df $tf" );
+		$tpl_row->assign('show_last_post', TRUE);	
+		$formatted_last_message_date = $message_date->format( "$df $tf" );
+		$tpl_row->assign('message_date', $formatted_last_message_date);
 
 		$last = new Date_Span();
 		$last->setFromDateDiff( $now, $message_date );
 
-		echo '<br /><font color=#999966>(' . $AppUI->_('Last post').' ';
-		printf( "%.1f", $last->format( "%d" ) );
-		echo ' '.$AppUI->_('days ago') . ') </font>';
+		$last_post_days = sprintf( "%.1f", $last->format( "%d" ) );
+		$tpl_row->assign('last_post_days', $last_post_days);
 
 		$id = $row['message_parent'] < 0 ? $row['message_id'] : $row['message_parent'];
-
-		echo '<br />&gt;&nbsp;<a href="?m=forums&a=viewer&forum_id='.$row['forum_id'].'&message_id='.$id.'">';
-		echo '<font color=#777777>'.$row['message_body'];
-		echo $row['message_length'] > $max_msg_length ? '...' : '';
-		echo '</font></a>';
+		$tpl_row->assign('last_message_id', $id); 
+		$tpl_row->assign('max_msg_length', $max_msg_length);
 	} else {
-		echo $AppUI->_('No posts');
+		$tpl_row->assign('show_last_post', FALSE);
 	}
+
+	$forum_rows_html .= $tpl_row->fetchFile('index.row');
+	unset($tpl_row);	
+}
+
+$tpl->assign('forum_rows', $forum_rows_html);
+$tpl->displayFile('index');
 ?>
-	</td>
-</tr>
-
-<?php } ?>
-</table>
-
-<table width="100%" cellspacing="1" cellpadding="0" border="0">
-	<input type="hidden" name="dosql" value="do_watch_forum" />
-	<input type="hidden" name="watch" value="forum" />
-<tr>
-	<td>&nbsp;</td>
-</tr>
-<tr>
-	<td align="left">
-		<input type="submit" class="button" value="<?php echo $AppUI->_( 'update watches' );?>" />
-	</td>
-</tr>
-</form>
-</table>
