8a119ea20599a0a59f11790c317fdd7eda26817d
mosen <mosen>
2005-10-30 23:23:18 +0000

    Added Template for Forums topic view

--- dotproject/modules/forums/view_topics.php
+++ dotproject/modules/forums/view_topics.php
@@ -47,86 +47,42 @@ $topics = $q->loadList();
 
 $crumbs = array();
 $crumbs["?m=forums"] = "forums list";
-?>
-<table width="100%" cellspacing="1" cellpadding="2" border="0">
-<tr>
-	<td><?php echo breadCrumbs( $crumbs );?></td>
-	<td align="right">
-	<?php if ($canEdit) { ?>
-		<input type="button" class=button style="width:120;" value="<?php echo $AppUI->_( 'start a new topic' );?>" onClick="javascript:window.location='./index.php?m=forums&a=viewer&forum_id=<?php echo $forum_id;?>&post_message=1';">
-	<?php } ?>
-	</td>
-</tr>
-</table>
 
-<table width="100%" cellspacing="1" cellpadding="2" border="0" class="tbl">
-<form name="watcher" action="?m=forums&a=viewer&forum_id=<?php echo $forum_id;?>&f=<?php echo $f;?>" method="post">
-<tr>
-	<th><a href="?m=forums&a=viewer&forum_id=<?php echo $forum_id;?>&orderby=watch_user" class="hdr"><?php echo $AppUI->_('Watch');?></a></th>
-	<th><a href="?m=forums&a=viewer&forum_id=<?php echo $forum_id;?>&orderby=message_title" class="hdr"><?php echo $AppUI->_('Topics');?></a></th>
-	<th><a href="?m=forums&a=viewer&forum_id=<?php echo $forum_id;?>&orderby=user_username" class="hdr"><?php echo $AppUI->_('Author');?></a></th>
-	<th><a href="?m=forums&a=viewer&forum_id=<?php echo $forum_id;?>&orderby=replies" class="hdr"><?php echo $AppUI->_('Replies');?></a></th>
-	<th><a href="?m=forums&a=viewer&forum_id=<?php echo $forum_id;?>&orderby=latest_reply" class="hdr"><?php echo $AppUI->_('Last Post');?></a></th>
+$tpl->assign('breadCrumbs', breadCrumbs( $crumbs ));
+$tpl->assign('canEdit', $canEdit);
+$tpl->assign('forum_id', $forum_id);
+$tpl->assign('f', $f);
 
-</tr>
-<?php
+$topic_rows = "";
 
 $now = new CDate();
 
 foreach ($topics as $row) {
+	$tpl_row = new CTemplate();
+
+	$tpl_row->assign('forum_id', $forum_id);
 	$last = intval( $row["latest_reply"] ) ? new CDate( $row["latest_reply"] ) : null;
 	
-//JBF limit displayed messages to first-in-thread
-	if ($row["message_parent"] < 0) { ?>
-<tr>
-	<td nowrap="nowrap" align="center" width="1%">
-		<input type="checkbox" name="forum_<?php echo $row['message_id'];?>" <?php echo $row['watch_user'] ? 'checked' : '';?> />
-	</td>
-	<td>
-		<?php
-			if ($row['visit_user'] != $AppUI->user_id || $row['reply_visits'] != $row['replies']) {
-				echo dPshowImage("images/icons/stock_new_small.png", false, false, "You have unread posts in this topic");
-			}
-		?>
-		<span style="font-size:10pt;">
-		<a href="?m=forums&a=viewer&forum_id=<?php echo $forum_id . "&message_id=" . $row["message_id"];?>"><?php echo $row["message_title"];?></a>
-		</span>
-	</td>
-	<td bgcolor="#dddddd" width="10%"><?php echo $row["user_username"];?></td>
-	<td align="center" width="10%"><?php echo  $row["replies"];?></td>
-	<td bgcolor="#dddddd" width="150" nowrap="nowrap">
-<?php if ($row["latest_reply"]) {
-		echo $last->format( "$df $tf" ).'<br /><font color=#999966>(';
+	//JBF limit displayed messages to first-in-thread
+	$tpl_row->assign('row', $row);
+	$tpl_row->assign('user_id', $AppUI->user_id);
 
-		$span = new Date_Span();
-		$span->setFromDateDiff( $now, $last );
+	$formatted_date = ($last != null) ? $last->format( "$df $tf" ) : null;
+	$tpl_row->assign('formatted_date', $formatted_date);
 
-		printf( "%.1f", $span->format( "%d" ) );
-		echo ' '.$AppUI->_('days ago');
+	$span = new Date_Span();
+	$span->setFromDateDiff( $now, $last );
 
-		echo ')</font>';
-	} else {
-		echo $AppUI->_("No replies");
-	}
+	$date_diff = sprintf( "%.1f", $span->format( "%d" ) );
+	$tpl_row->assign('date_diff_now_last', $date_diff);
+
+	$topic_rows .= $tpl_row->fetchFile('view_topics.row');
+}
+
+$tpl->assign('topic_rows', $topic_rows);
+
+$tpl->displayFile('view_topics');
+
+
+
 ?>
-	</td>
-</tr>
-<?php
-//JBF
-	}
-}?>
-</table>
-
-<table width="100%" border="0" cellpadding="0" cellspacing="1">
-<input type="hidden" name="dosql" value="do_watch_forum" />
-<input type="hidden" name="watch" value="topic" />
-<tr>
-	<td>&nbsp;</td>
-</tr>
-<tr>
-	<td align="left">
-		<input type="submit" class="button" value="<?php echo $AppUI->_( 'update watches' );?>" />
-	</td>
-</tr>
-</form>
-</table>
