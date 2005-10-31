84c9c4eb6605675c6ae4b86926279c6eff884197
mosen <mosen>
2005-10-31 01:19:40 +0000

    Added smarty template for Files checkout

--- dotproject/modules/files/co.php
+++ dotproject/modules/files/co.php
@@ -54,61 +54,11 @@ $extra = array(
 $project = new CProject();
 $projects = $project->getAllowedRecords( $AppUI->user_id, 'project_id,project_name', 'project_name', null, $extra );
 $projects = arrayMerge( array( '0'=>$AppUI->_('All') ), $projects );
+
+$tpl->assign('file_id', $file_id);
+$tpl->assign('user_id', $AppUI->user_id);
+$tpl->assign('obj', $obj);
+$tpl->assign('file_owner', $obj->getOwner());
+
+$tpl->displayFile('co');
 ?>
-
-<table width="100%" border="0" cellpadding="3" cellspacing="3" class="std">
-
-<form name="coFrm" action="?m=files" method="post">
-	<input type="hidden" name="dosql" value="do_file_co" />
-	<input type="hidden" name="del" value="0" />
-	<input type="hidden" name="file_id" value="<?php echo $file_id;?>" />
-        <input type="hidden" name="file_checkout" value="<?php echo $AppUI->user_id; ?>">
-        <input type="hidden" name="file_version_id" value="<?php echo $obj->file_version_id; ?>">
-        
-
-<tr>
-	<td width="100%" valign="top" align="center">
-		<table cellspacing="1" cellpadding="2" width="60%">
-	<?php if ($file_id) { ?>
-		<tr>
-			<td align="right" nowrap="nowrap"><?php echo $AppUI->_( 'File Name' );?>:</td>
-			<td align="left" class="hilite"><?php echo strlen($obj->file_name)== 0 ? "n/a" : $obj->file_name;?></td>
-		</tr>
-		<tr valign="top">
-			<td align="right" nowrap="nowrap"><?php echo $AppUI->_( 'Type' );?>:</td>
-			<td align="left" class="hilite"><?php echo $obj->file_type;?></td>
-		</tr>
-		<tr>
-			<td align="right" nowrap="nowrap"><?php echo $AppUI->_( 'Size' );?>:</td>
-			<td align="left" class="hilite"><?php echo $obj->file_size;?></td>
-		</tr>
-		<tr>
-			<td align="right" nowrap="nowrap"><?php echo $AppUI->_( 'Uploaded By' );?>:</td>
-			<td align="left" class="hilite"><?php echo $obj->getOwner();?></td>
-		</tr>
-	<?php } ?>
-		<tr>
-			<td align="right" nowrap="nowrap"><?php echo $AppUI->_( 'CO Reason' );?>:</td>
-			<td align="left">
-				<textarea name="file_co_reason" class="textarea" rows="4" style="width:270px"><?php echo $obj->file_co_reason;?></textarea>
-			</td>
-		</tr>
-
-		<tr>
-			<td align="right" nowrap="nowrap">&nbsp;</td>
-			<td align="left"><input type="checkbox" name="notify" checked="checked"><?php echo $AppUI->_('Notify Assignees of Task or Project Owner by Email'); ?></td>		
-		</tr>
-		
-		</table>
-	</td>
-</tr>
-<tr>
-	<td>
-		<input class="button" type="button" name="cancel" value="<?php echo $AppUI->_('cancel');?>" onClick="javascript:if(confirm('<?php echo $AppUI->_('Are you sure you want to cancel?', UI_OUTPUT_JS); ?>')){location.href = './index.php?m=files';}" />
-	</td>
-	<td align="right">
-		<input type="submit" class="button" value="<?php echo $AppUI->_( 'submit' );?>" />
-	</td>
-</tr>
-</form>
-</table>
