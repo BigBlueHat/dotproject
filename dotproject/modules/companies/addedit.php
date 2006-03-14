4ce61f21d98c1c16b8acf6e36cfa6377aadb844c
cyberhorse <cyberhorse>
2006-03-14 08:25:16 +0000

    removed blank lines every second line (windows/linux issue)

--- dotproject/modules/companies/addedit.php
+++ dotproject/modules/companies/addedit.php
@@ -1,177 +1,91 @@
 <?php /* COMPANIES $Id$ */
-
 $company_id = intval( dPgetParam( $_GET, "company_id", 0 ) );
 
-
-
 // check permissions for this company
-
 $perms =& $AppUI->acl();
-
 // If the company exists we need edit permission,
-
 // If it is a new company we need add permission on the module.
-
 if ($company_id)
-
   $canEdit = $perms->checkModuleItem($m, "edit", $company_id);
-
 else
-
   $canEdit = $perms->checkModule($m, "add");
 
-
-
 if (!$canEdit) {
-
 	$AppUI->redirect( "m=public&a=access_denied" );
-
 }
-
-
 
 // load the company types
-
 $types = dPgetSysVal( 'CompanyType' );
 
-
-
 // load the record data
-
 $q  = new DBQuery;
-
 $q->addTable('companies');
-
 $q->addQuery('companies.*');
-
 $q->addQuery('con.contact_first_name');
-
 $q->addQuery('con.contact_last_name');
-
 $q->addJoin('users', 'u', 'u.user_id = companies.company_owner');
-
 $q->addJoin('contacts', 'con', 'u.user_contact = con.contact_id');
-
 $q->addWhere('companies.company_id = '.$company_id);
-
 $sql = $q->prepare();
-
 $q->clear();
 
-
-
 $obj = null;
-
 if (!db_loadObject( $sql, $obj ) && $company_id > 0) {
-
 	// $AppUI->setMsg( '	$qid =& $q->exec(); Company' ); // What is this for?
-
 	$AppUI->setMsg( "invalidID", UI_MSG_ERROR, true );
-
 	$AppUI->redirect();
-
 }
 
-
-
 // collect all the users for the company owner list
-
 $q  = new DBQuery;
-
 $q->addTable('users','u');
-
 $q->addTable('contacts','con');
-
 $q->addQuery('user_id');
-
 $contact_full_name = ("CONCAT(contact_last_name, ', ' , contact_first_name)");
-
 $q->addQuery($contact_full_name);
-
 $q->addOrder('contact_last_name');
-
 $q->addWhere('u.user_contact = con.contact_id');
-
 $owners = $q->loadHashList();
 if ($company_id != 0)
     $owner_id = $obj->company_owner;
 else
     $owner_id = $AppUI->user_id;
 
-
-
 // setup the title block
-
 $ttl = $company_id > 0 ? "Edit Company" : "Add Company";
-
 $titleBlock = new CTitleBlock( $ttl, 'handshake.png', $m, "$m.$a" );
-
 $titleBlock->addCrumb( "?m=companies", "companies list" );
-
 if ($company_id != 0)
-
   $titleBlock->addCrumb( "?m=companies&a=view&company_id=$company_id", "view this company" );
-
 $titleBlock->show();
 
-
-
 require_once("./classes/CustomFields.class.php");
-
 $custom_fields = New CustomFields( $m, $a, $obj->company_id, "edit" );
-
 $tpl->assign('customFields', $custom_fields->getHTML());
-
 $tpl->assign('company_id', $company_id);
-
 $tpl->assign('types', $types);
-
 $tpl->assign('owners', $owners);
-
 $tpl->assign('owner_id', $owner_id);
 
-
-
 $tpl->displayAddEdit($obj);
-
 ?>
 
-
-
 <script language="javascript">
-
 function submitIt() {
-
 	var form = document.changeclient;
-
 	if (form.company_name.value.length < 3) {
-
 		alert( "<?php echo $AppUI->_('companyValidName', UI_OUTPUT_JS);?>" );
-
 		form.company_name.focus();
-
 	} else {
-
 		form.submit();
-
 	}
-
 }
-
-
 
 function testURL( x ) {
-
 	var test = "document.changeclient.company_primary_url.value";
-
 	test = eval(test);
-
 	if (test.length > 6) {
-
 		newwin = window.open( "http://" + test, 'newwin', '' );
-
 	}
-
 }
-
 </script>
