75b774c4288354219b62b3982705564f6312f6a9
theideaman <theideaman>
2006-04-11 12:54:11 +0000

    added cacheDir and modified PHP/XHTML to conform to Coding Standards

--- dotproject/install/index.php
+++ dotproject/install/index.php
@@ -35,50 +35,59 @@ if (is_file( "../includes/config.php" )) {
 <html>
 <head>
 	<title>dotProject Installer</title>
-	<meta name="Description" content="dotProject Installer">
- 	<link rel="stylesheet" type="text/css" href="../style/default/main.css">
+	<meta name="Description" content="dotProject Installer" />
+	<link rel="stylesheet" type="text/css" href="../style/default/main.css" />
 </head>
 <body>
-<h1><img src="dp.png" align="middle" alt="dotProject Logo"/>&nbsp;dotProject Installer</h1>
+<h1><img src="dp.png" align="middle" alt="dotProject Logo" />&nbsp;dotProject Installer</h1>
 <table cellspacing="0" cellpadding="3" border="0" class="tbl" width="90%" align="center">
 <tr>
-        <td class="item" colspan="2">Welcome to the dotProject Installer! It will setup the database for dotProject and create an appropriate config file.
+	<td class="item" colspan="2">Welcome to the dotProject Installer! It will setup the database for dotProject and create an appropriate config file.
 	In some cases a manual installation cannot be avoided.
-        </td>
+	</td>
 </tr>
 <tr>
-        <td colspan="2">&nbsp;</td>
+	<td colspan="2">&nbsp;</td>
 </tr>
 <tr>
-        <td class="title" colspan="2">There is an initial Check for (minimal) Requirements appended down below for troubleshooting. At least a database connection
+	<td class="title" colspan="2">There is an initial Check for (minimal) Requirements appended down below for troubleshooting. At least a database connection
 	must be available and ../includes/config.php must be writable for the webserver!</td>
 </tr>
 <?php
-	if ($mode == 'upgrade') {
+	if ($mode == 'upgrade'):
 ?>
 <tr>
-	<td class='title' colspan='2'><p class='error'>It would appear that you already have a dotProject installation. The installer will attempt to upgrade your system, however it is a good idea to take a full backup first!</p></td>
+	<td class="title" colspan="2">
+		<p class="error">It would appear that you already have a dotProject installation. The installer will attempt
+		to upgrade your system, however it is a good idea to take a full backup first!</p>
+	</td>
 <?php
-	}
+	endif;
 ?>
+</tr>
 <tr>
-        <td colspan="2" align="center"><br /><form action="db.php" method="post" name="form" id="form">
-	<input class="button" type="submit" name="next" value="Start <?php echo $mode == 'install' ? "Installation" : "Upgrade" ?>" />
-	<input type="hidden" name="mode" value="<?php echo $mode; ?>" /></form></td>
+	<td colspan="2" align="center">
+		<br />
+		<form action="db.php" method="post" name="form" id="form">
+			<input class="button" type="submit" name="next" value="Start <?php echo $mode == 'install' ? "Installation" : "Upgrade" ?>" />
+			<input type="hidden" name="mode" value="<?php echo $mode; ?>" />
+		</form>
+	</td>
 </tr>
 </table>
 <br />
 <?php
 // define some necessary variables for check inclusion
-$failedImg = '<img src="../images/icons/stock_cancel-16.png" width="16" height="16" align="middle" alt="Failed"/>';
-$okImg = '<img src="../images/icons/stock_ok-16.png" width="16" height="16" align="middle" alt="OK"/>';
+$failedImg = '<img src="../images/icons/stock_cancel-16.png" width="16" height="16" align="middle" alt="Failed" />';
+$okImg = '<img src="../images/icons/stock_ok-16.png" width="16" height="16" align="middle" alt="OK" />';
 $tblwidth = '90%';
 $cfgDir = "../includes";
 $cfgFile = "../includes/config.php";
 $filesDir = "../files";
-$locEnDir = "../locales/en";
+$cacheDir = "../files/cache";
 $tmpDir = "../files/temp";
-include_once("vw_idx_check.php");
+$locEnDir = "../locales/en";
+include_once "vw_idx_check.php";
 ?>
 </body>
 </html>
