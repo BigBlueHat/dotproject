2efa19c1dede32de6313853a048a82e23ccb328a
cyberhorse <cyberhorse>
2006-04-23 05:06:36 +0000

    - replaced double quotes with single quotes - some other cleanups

--- dotproject/modules/forums/viewer.php
+++ dotproject/modules/forums/viewer.php
@@ -1,17 +1,17 @@
 <?php /* $Id$ */
 //view posts
-$forum_id = isset($_GET["forum_id"]) ? $_GET["forum_id"] : 0;
-$message_id = isset($_GET["message_id"]) ? $_GET["message_id"] : 0;
-$post_message = isset($_GET["post_message"]) ? $_GET["post_message"] : 0;
-$f = dpGetParam( $_POST, 'f', 0 );
+$forum_id 		= dPgetParam($_GET, 'forum_id', 0);
+$message_id 	= dPgetParam($_GET, 'message_id', 0);
+$post_message = dPgetParam($_GET, 'post_message', 0);
+
+$f = dPgetParam( $_POST, 'f', 0 );
 
 // check permissions
 $canRead = !getDenyRead( $m, $forum_id );
 $canEdit = !getDenyEdit( $m, $forum_id );
 
-if (!$canRead || ($post_message & !$canEdit)) {
-	$AppUI->redirect( "m=public&a=access_denied" );
-}
+if (!$canRead || ($post_message & !$canEdit)) 
+	$AppUI->redirect( 'm=public&a=access_denied' );
 
 $df = $AppUI->getPref('SHDATEFORMAT');
 $tf = $AppUI->getPref('TIMEFORMAT');
@@ -25,16 +25,16 @@ $q->addQuery('forum_id, forum_project,	forum_description, forum_owner, forum_nam
 	user_username, contact_first_name, contact_last_name,
 	project_name, project_color_identifier');
 $q->addJoin('contacts', 'con', 'contact_id = user_contact');
-$q->addWhere("user_id = forum_owner");
-$q->addWhere("forum_id = $forum_id");
-$q->addWhere("forum_project = project_id");
+$q->addWhere('user_id = forum_owner');
+$q->addWhere('forum_id = '.$forum_id);
+$q->addWhere('forum_project = project_id');
 $q->exec(ADODB_FETCH_ASSOC);
 $forum = $q->fetchRow();
-$forum_name = $forum["forum_name"];
+$forum_name = $forum['forum_name'];
 echo db_error();
 $q->clear();
 
-$start_date = intval( $forum["forum_create_date"] ) ? new CDate( $forum["forum_create_date"] ) : null;
+$start_date = intval( $forum['forum_create_date'] ) ? new CDate( $forum['forum_create_date'] ) : null;
 
 // setup the title block
 $titleBlock = new CTitleBlock( 'Forum', 'support.png', $m, "$m.$a" );
@@ -50,10 +50,10 @@ $tpl->assign('formatted_start_date', $start_date_formatted);
 $tpl->displayFile('viewer');
 
 if($post_message){
-	include("./modules/forums/post_message.php");
+	include($baseDir . '/modules/forums/post_message.php');
 } else if($message_id == 0) {
-	include("./modules/forums/view_topics.php");
+	include($baseDir . '/modules/forums/view_topics.php');
 } else {
-	include("./modules/forums/view_messages.php");
+	include($baseDir . '/modules/forums/view_messages.php');
 }
 ?>
