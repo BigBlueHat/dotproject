fca28534eab18a159d3439d924424910fef5b3d2
cyberhorse <cyberhorse>
2006-05-03 14:18:10 +0000

    - fixed html errors - now it validates as xhtml

--- dotproject/style/default/login.php
+++ dotproject/style/default/login.php
@@ -6,7 +6,6 @@
 <head>
 	<title><?php echo $dPconfig['page_title'];?></title>
 	<meta http-equiv="Content-Type" content="text/html;charset=<?php echo isset( $locale_char_set ) ? $locale_char_set : 'UTF-8';?>" />
-       	<title><?php echo $dPconfig['company_name'];?> :: dotProject Login</title>
 	<meta http-equiv="Pragma" content="no-cache" />
 	<meta name="Version" content="<?php echo @$AppUI->getVersion();?>" />
 	<link rel="stylesheet" type="text/css" href="./style/<?php echo $uistyle;?>/main.css" media="all" />
@@ -19,24 +18,24 @@
 <?php //please leave action argument empty ?>
 <!--form action="./index.php" method="post" name="loginform"-->
 <form method="post" action="<?php echo $loginFromPage; ?>" name="loginform">
-<table align="center" border="0" width="250" cellpadding="6" cellspacing="0" class="std">
 <input type="hidden" name="login" value="<?php echo time();?>" />
 <input type="hidden" name="lostpass" value="0" />
 <input type="hidden" name="redirect" value="<?php echo $redirect;?>" />
+<table align="center" border="0" width="250" cellpadding="6" cellspacing="0" class="std">
 <tr>
 	<th colspan="2"><em><?php echo $dPconfig['company_name'];?></em></th>
 </tr>
 <tr>
-	<td align="right" nowrap><?php echo $AppUI->_('Username');?>:</td>
-	<td align="left" nowrap><input type="text" size="25" maxlength="20" name="username" class="text" /></td>
+	<td align="right" nowrap="nowrap"><?php echo $AppUI->_('Username');?>:</td>
+	<td align="left" nowrap="nowrap"><input type="text" size="25" maxlength="20" name="username" class="text" /></td>
 </tr>
 <tr>
-	<td align="right" nowrap><?php echo $AppUI->_('Password');?>:</td>
-	<td align="left" nowrap><input type="password" size="25" maxlength="32" name="password" class="text" /></td>
+	<td align="right" nowrap="nowrap"><?php echo $AppUI->_('Password');?>:</td>
+	<td align="left" nowrap="nowrap"><input type="password" size="25" maxlength="32" name="password" class="text" /></td>
 </tr>
 <tr>
-	<td align="left" nowrap><a href="http://www.dotproject.net/"><img src="./style/default/images/dp_icon.gif" border="0" alt="dotProject logo" /></a></td>
-	<td align="right" valign="bottom" nowrap><input type="submit" name="login" value="<?php echo $AppUI->_('login');?>" class="button" /></td>
+	<td align="left" nowrap="nowrap"><a href="http://www.dotproject.net/"><img src="./style/default/images/dp_icon.gif" border="0" alt="dotProject logo" /></a></td>
+	<td align="right" valign="bottom" nowrap="nowrap"><input type="submit" name="login" value="<?php echo $AppUI->_('login');?>" class="button" /></td>
 </tr>
 <tr>
 	<td colspan="2"><a href="#" onclick="f=document.loginform;f.lostpass.value=1;f.submit();"><?php echo $AppUI->_('forgotPassword');?></a></td>
