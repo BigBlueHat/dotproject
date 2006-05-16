7518dd9a11cd430ea38ebc9c0b8790358b007ecb
cyberhorse <cyberhorse>
2006-05-16 14:29:04 +0000

    - fixed bug in highlighting (when keyword was empty)

--- dotproject/includes/smarty/modifier.dPhighlight.php
+++ dotproject/includes/smarty/modifier.dPhighlight.php
@@ -2,6 +2,9 @@
 function smarty_modifier_dPhighlight($string, $key)
 {
 	global $locale_char_set;
+
+  if (empty($key))
+    return $string;
 	
 	return eregi_replace('('.quotemeta($key).')', '<span style="background: yellow">\\1</span>', $string);
 }
