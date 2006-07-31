27d11e927214383ff57718a90f6b063840059fc4
merlinyoda <merlinyoda>
2006-07-31 21:37:18 +0000

    Added hidden form fields for "config_group" and "config_type" fields for system config page. Add/Edit page for system config takes new values from POST and assings them to the proper object variable to be stored. Previous setup would "forget" these values and assign blank values into many of the database fields, causing a loss of needed information.
    Also fixed checkbox functionality on system config page. Value
    attributes for checkboxes were being set to their previous values instead
    of "true" when checked. Since POST data is only passed along by *checked*
    checkbox items, newly uncheck items remained unchanged (the values
    weren't posted to be updated) and newly checked items were passing their
    old value of "false" (old value passed to POST instead of "true", new
    value). Unchenged valued were unaffected.

--- dotproject/modules/system/do_systemconfig_aed.php
+++ dotproject/modules/system/do_systemconfig_aed.php
@@ -16,12 +16,17 @@ $rs = $q->exec();
 
 foreach ($_POST['dPcfg'] as $name => $value) {
 	$obj->config_name = $name;
-	$obj->config_value =  get_magic_quotes_gpc() ? $value : addslashes( $value ) ;
-
+    $obj->config_value = (get_magic_quotes_gpc() ? $value : addslashes( $value ));
+    
+    // get the group and type from hidden fields to preserver their values
+    // previous setup caused group and type to be lost.
+    $obj->config_group = $_POST["{$name}___group"];
+    $obj->config_type = $_POST["{$name}___type"];
+    
 	// grab the appropriate id for the object in order to ensure
 	// that the db is updated well (config_name must be unique)
 	$obj->config_id = $_POST['dPcfgId'][$name];
-
+    
 	// prepare (and translate) the module name ready for the suffix
 	$AppUI->setMsg( 'System Configuration' );
 	if (($msg = $obj->store())) {
