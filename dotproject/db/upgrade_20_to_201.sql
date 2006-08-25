2aa490b580304b9abd2b7ae5b2aee9a3c3664c83
caseydk <caseydk>
2006-08-25 15:41:23 +0000

    updated for mysql 5 compliance

--- dotproject/db/upgrade_20_to_201.sql
+++ dotproject/db/upgrade_20_to_201.sql
@@ -7,7 +7,7 @@
 #
 
 # 20050404 - minical config var restaured 
-INSERT INTO `config` VALUES ('', 'cal_day_view_show_minical', 'true', '', 'checkbox');
+INSERT INTO `config` VALUES (0, 'cal_day_view_show_minical', 'true', '', 'checkbox');
 
 # 20050405 - temporarily reset the memory limit for gantt charts
 INSERT INTO `config` ( `config_id` , `config_name` , `config_value` , `config_group` , `config_type` )
