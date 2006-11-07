2aaec1abb925d88137c36e7524bcb20b24df8da9
cyberhorse <cyberhorse>
2006-11-07 11:53:50 +0000

    cleanup

--- dotproject/modules/calendar/day_view.php
+++ dotproject/modules/calendar/day_view.php
@@ -1,19 +1,20 @@
 <?php /* CALENDAR $Id$ */
 global $tab, $locale_char_set, $date;
+
 $AppUI->savePlace();
 
-require_once( $AppUI->getModuleClass( 'tasks' ) );
+require_once($AppUI->getModuleClass('tasks'));
 
 // retrieve any state parameters
 if (isset( $_REQUEST['company_id'] )) {
-	$AppUI->setState( 'CalIdxCompany', intval( $_REQUEST['company_id'] ) );
+	$AppUI->setState('CalIdxCompany', intval($_REQUEST['company_id']));
 }
-$company_id = $AppUI->getState( 'CalIdxCompany', $AppUI->user_company);
+$company_id = $AppUI->getState('CalIdxCompany', $AppUI->user_company);
 
 $event_filter = $AppUI->checkPrefState('CalIdxFilter', @$_REQUEST['event_filter'], 'EVENTFILTER', 'my');
 
-$AppUI->setState( 'CalDayViewTab', dPgetParam($_GET, 'tab', $tab) );
-$tab = $AppUI->getState( 'CalDayViewTab' ,'0');
+$AppUI->setState('CalDayViewTab', dPgetParam($_GET, 'tab', $tab));
+$tab = $AppUI->getState('CalDayViewTab' ,'0');
 
 // get the prefered date format
 $df = $AppUI->getPref('SHDATEFORMAT');
@@ -30,49 +31,49 @@ $mm = $this_day->getMonth();
 $yy = $this_day->getYear();
 
 // get current week
-$this_week = Date_calc::beginOfWeek ($dd, $mm, $yy, FMT_TIMESTAMP_DATE, LOCALE_FIRST_DAY );
+$this_week = Date_calc::beginOfWeek ($dd, $mm, $yy, FMT_TIMESTAMP_DATE, LOCALE_FIRST_DAY);
 
 // prepare time period for 'events'
 $first_time = new CDate( $date);
 $first_time->setTime( 0, 0, 0 );
 $phpver = phpversion();
-if ($phpver < "5") // fix a bug in php4.
-	$first_time->subtractSeconds( 1 );
+if ($phpver < '5') // fix a bug in php4.
+	$first_time->subtractSeconds(1);
 
-$last_time = new CDate( $date );
-$last_time->setTime( 23, 59, 59 );
+$last_time = new CDate($date);
+$last_time->setTime(23, 59, 59);
 
-$prev_day = new CDate( Date_calc::prevDay( $dd, $mm, $yy, FMT_TIMESTAMP_DATE ) );
-$next_day = new CDate( Date_calc::nextDay( $dd, $mm, $yy, FMT_TIMESTAMP_DATE ) );
+$prev_day = new CDate(Date_calc::prevDay($dd, $mm, $yy, FMT_TIMESTAMP_DATE));
+$next_day = new CDate(Date_calc::nextDay($dd, $mm, $yy, FMT_TIMESTAMP_DATE));
 
 // setup the title block
 $titleBlock = new CTitleBlock( 'Day View', 'myevo-appointments.png', $m, "$m.$a" );
-$titleBlock->addCrumb( '?m=calendar&amp;date='.$this_day->format( FMT_TIMESTAMP_DATE ), 'month view' );
-$titleBlock->addCrumb( '?m=calendar&amp;a=week_view&amp;date='.$this_week, 'week view' );
+$titleBlock->addCrumb('?m=calendar&amp;date='.$this_day->format(FMT_TIMESTAMP_DATE), 'month view');
+$titleBlock->addCrumb('?m=calendar&amp;a=week_view&amp;date='.$this_week, 'week view');
 $titleBlock->addCell('
-<form action="?m=calendar&amp;a=addedit&amp;date=' . $this_day->format( FMT_TIMESTAMP_DATE )  . '" method="post">
+<form action="?m=calendar&amp;a=addedit&amp;date=' . $this_day->format(FMT_TIMESTAMP_DATE)  . '" method="post">
 	<input type="submit" class="button" value="'.$AppUI->_('new event').'" />
 </form>', '', '', '');
 $titleBlock->show();
 
 
-$minical = new CMonthCalendar( $this_day );
-$minical->setStyles( 'minititle', 'minical' );
+$minical = new CMonthCalendar($this_day);
+$minical->setStyles('minititle', 'minical');
 $minical->showArrows = false;
 $minical->showWeek = false;
 $minical->clickMonth = true;
-$minical->setLinkFunctions( 'clickDay' );
+$minical->setLinkFunctions('clickDay');
 
-$minical->setDate( $minical->prev_month );
+$minical->setDate($minical->prev_month);
 $tpl->assign('minical_prev', $minical->show());
-$minical->setDate( $minical->next_month );
+$minical->setDate($minical->next_month);
 $tpl->assign('minical', $minical->show());
-$minical->setDate( $minical->next_month );
+$minical->setDate($minical->next_month);
 $tpl->assign('minical_next', $minical->show());
 
-$tabBox = new CTabBox( "?m=calendar&amp;a=day_view&amp;date=" . $this_day->format( FMT_TIMESTAMP_DATE ), "", $tab );
-$tabBox->add( $dPconfig['root_dir'].'/modules/calendar/vw_day_events', 'Events' );
-$tabBox->add( $dPconfig['root_dir'].'/modules/calendar/vw_day_tasks', 'Tasks' );
+$tabBox = new CTabBox('?m=calendar&amp;a=day_view&amp;date=' . $this_day->format(FMT_TIMESTAMP_DATE), '', $tab);
+$tabBox->add($dPconfig['root_dir'].'/modules/calendar/vw_day_events', 'Events');
+$tabBox->add($dPconfig['root_dir'].'/modules/calendar/vw_day_tasks', 'Tasks');
 $tabBox->loadExtras('calendar', 'day_view');
 
 //$tabBox->show();
@@ -81,10 +82,9 @@ $tpl->assign('show_minical', $dPconfig['cal_day_view_show_minical']);
 
 $tpl->assign('prev_day', $prev_day);
 $tpl->assign('next_day', $next_day);
-$tpl->assign('today', htmlentities($this_day->format( "%A" ), ENT_COMPAT, $locale_char_set).', '.$this_day->format( $df ));
+$tpl->assign('today', htmlentities($this_day->format('%A'), ENT_COMPAT, $locale_char_set).', '.$this_day->format($df));
 
 $tpl->displayFile('view.day');
-
 ?>
 <script language="javascript" type="text/javascript">
 <!--
