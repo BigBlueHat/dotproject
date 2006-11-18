2b11ab408268c604edbea340f3ce0b5151cd7d09
caseydk <caseydk>
2006-11-18 19:05:37 +0000

    changed to denote this as 2.0.4-head as opposed to 2.0.4-5

--- dotproject/includes/version.php
+++ dotproject/includes/version.php
@@ -3,5 +3,5 @@
 $dp_version_major = 2;
 $dp_version_minor = 0;
 $dp_version_patch = 4;	// Can be set to null if not used
-$dp_version_prepatch = .5;	// Set to null if a release version.
+$dp_version_prepatch = 'head';	// Set to null if a release version.
 ?>
