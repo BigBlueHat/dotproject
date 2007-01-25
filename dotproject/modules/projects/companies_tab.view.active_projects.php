c1182c603e77f902aa239ca37fcc99dcaa55351f
caseydk <caseydk>
2007-01-25 03:05:37 +0000

    refactored these to be essentially identical since they functioned the same

--- dotproject/modules/projects/companies_tab.view.active_projects.php
+++ dotproject/modules/projects/companies_tab.view.active_projects.php
@@ -1,27 +1,31 @@
 <?php /* PROJECTS $Id$ */
+##
+##  Companies: View Active Projects sub-table
+##
 GLOBAL $AppUI, $company_id, $pstatus, $dPconfig, $tpl, $m;
 
 $pstatus = dPgetSysVal( 'ProjectStatus' );
 
 $sort = dPgetParam($_GET, 'orderby', 'project_name');
-if ($sort == 'project_priority')
-        $sort .= ' DESC';
-
+if ($sort == 'project_priority') {
+  $sort .= ' DESC';
+}
 $df = $AppUI->getPref('SHDATEFORMAT');
+
+$page = isset($_REQUEST['page'])?$_REQUEST['page']:1;
 
 $q  = new DBQuery;
 $q->addTable('projects');
 $q->addQuery('project_id, project_name, project_start_date, project_status, project_target_budget,
-	project_start_date,
-        project_priority,
-	contact_first_name, contact_last_name');
+	           project_start_date, project_priority, contact_first_name, contact_last_name');
 $q->addJoin('users', 'u', 'u.user_id = projects.project_owner');
 $q->addJoin('contacts', 'con', 'u.user_contact = con.contact_id');
 $q->addWhere('projects.project_company = '.$company_id);
 $q->addWhere('projects.project_status != 7');
 $q->addOrder($sort);
-$q->setPageLimit();
 $rows = $q->loadList();
+
+//TODO:  $rows = getProjectsByCompanyAndStatus($company_id, ' != 7 ', $sort);
 
 $q->addTable('projects');
 $q->addQuery('count(*)');
@@ -29,8 +33,11 @@ $q->addWhere('projects.project_company = '.$company_id);
 $q->addWhere('projects.project_status != 7');
 $count_rows = $q->loadResult();
 
+//TODO:  $count_rows = getProjectCountByStatus($company_id, ' != 7 ', $sort);
+
 $tpl->assign('current_url', 'index.php?m=companies&a=view&company_id=' . $company_id);
 $tpl->assign('pstatus', $pstatus);
+$tpl->assign('msg', $AppUI->getMsg());
 
 $show = array('project_priority', 'project_name', 'project_owner', 'project_start_date', 'project_status', 'project_target_budget');
 
--- dotproject/modules/projects/companies_tab.view.archived_projects.php
+++ dotproject/modules/projects/companies_tab.view.archived_projects.php
@@ -2,37 +2,43 @@
 ##
 ##	Companies: View Archived Projects sub-table
 ##
-GLOBAL $AppUI, $company_id, $tpl; 
+GLOBAL $AppUI, $company_id, $pstatus, $dPconfig, $tpl, $m;
+
+$pstatus = dPgetSysVal( 'ProjectStatus' );
 
 $sort = dPgetParam($_GET, 'orderby', 'project_name');
+if ($sort == 'project_priority') {
+  $sort .= ' DESC';
+}
+$df = $AppUI->getPref('SHDATEFORMAT');
 
 $page = isset($_REQUEST['page'])?$_REQUEST['page']:1;
 
 $q  = new DBQuery;
 $q->addTable('projects');
 $q->addQuery('project_id, project_name, project_start_date, project_status, project_target_budget,
-	project_start_date,
-        project_priority,
-	contact_first_name, contact_last_name');
+	           project_start_date, project_priority, contact_first_name, contact_last_name');
 $q->addJoin('users', 'u', 'u.user_id = projects.project_owner');
 $q->addJoin('contacts', 'con', 'u.user_contact = con.contact_id');
 $q->addWhere('projects.project_company = '.$company_id);
 $q->addWhere('projects.project_status = 7');
 $q->addOrder($sort);
-$q->setPageLimit();
 $rows = $q->loadList();
+
+//TODO:  $rows = getProjectsByCompanyAndStatus($company_id, ' = 7 ', $sort);
 
 $q->addTable('projects');
 $q->addQuery('count(*)');
 $q->addWhere('projects.project_company = '.$company_id);
 $q->addWhere('projects.project_status = 7');
-$q->addOrder($sort);
 $count_rows = $q->loadResult();
 
-$tpl->assign('msg', $AppUI->getMsg());
-$tpl->assign('current_url', 'index.php?m=companies&a=view&company_id=' . $company_id);
+//TODO:  $count_rows = getProjectCountByStatus($company_id, ' = 7 ', $sort);
 
-//$smarty->assign('pstatus', $pstatus);
+$tpl->assign('current_url', 'index.php?m=companies&a=view&company_id=' . $company_id);
+$tpl->assign('pstatus', $pstatus);
+$tpl->assign('msg', $AppUI->getMsg());
+
 $show = array('project_priority', 'project_name', 'project_owner');
 
 $tpl->displayList('projects', $rows, $count_rows, $show);
