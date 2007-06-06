942d689ab9fbb32e4a3bd0c2f38ec86ecadb72b5
cyberhorse <cyberhorse>
2007-06-06 14:16:51 +0000

    Fixed constructor circular dependency

--- dotproject/classes/date.class.php
+++ dotproject/classes/date.class.php
@@ -105,7 +105,7 @@ class CDate {
 		if (empty($date)) {
 			$this->setDate(date('Y-m-d H:i:s'));
 		} elseif (is_object($date) && (get_class($this) == get_class($date))) {
-			$this->setDate($date->getDate());
+			$this->copy($date);
 		} elseif (preg_match('/\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}/', $date)) {
     	$this->setTZ('UTC');
     	$d = new CDate();
