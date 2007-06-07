3310006417d1816082a2b0565d7b3b17d66f99dd
cyberhorse <cyberhorse>
2007-06-07 12:53:43 +0000

    FEATURE Made date picker easier to use, by allowing to click anywhere in the date square to pick a date, rather then exactly on the number.

--- dotproject/modules/calendar/calendar.class.php
+++ dotproject/modules/calendar/calendar.class.php
@@ -389,7 +389,7 @@ class CMonthCalendar {
 				if($this->showHighlightedDays && isset($this->highlightedDays[$day])){
 					$html .= " style=\"border: 1px solid ".$this->highlightedDays[$day]."\"";
 				}
-				$html .= ">";
+				$html .= " onclick=\"$this->dayFunc('$day','".$this_day->format( $df )."')\">";
 				if (isset($week[$i])) {
 					if ($this->dayFunc) {
 						$html .= "<a href=\"javascript:$this->dayFunc('$day','".$this_day->format( $df )."')\" class=\"$class\">";
