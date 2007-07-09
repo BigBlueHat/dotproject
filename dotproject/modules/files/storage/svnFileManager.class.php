18193edb8768b2d41e376e23aa189ff8843a004b
caseydk <caseydk>
2007-07-09 13:37:49 +0000

    finishing out the svn stub;

--- dotproject/modules/files/storage/svnFileManager.class.php
+++ dotproject/modules/files/storage/svnFileManager.class.php
@@ -3,26 +3,28 @@
 include_once DP_BASE_DIR.'/modules/files/storage/iFileManager.class.php';
 
 class SVNFileManager implements iFileManager {
-
-	public function createFile() {
+	public function createFile($file, $upload) {
 		return true;
 	}
-	public function retrieveFile() {
+	public function retrieveFile($fileRow) {
 		return true;
 	}
-	public function updateFile() {
+	public function updateFile($file) {
 		return true;
 	}
-	public function deleteFile() {
+	public function deleteFile($fileObject) {
+		return true;
+	}
+	public function moveFile($file, $oldProject, $realname) {
 		return true;
 	}
 	public function summarizeFile() {
 		return true;
 	}
-	public function checkinFile() {
+	public function checkinFile($file) {
 		return true;
 	}
-	public function checkoutFile() {
+	public function checkoutFile($file) {
 		return true;
 	}
 }
