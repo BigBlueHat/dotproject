c55cedf8b8dced6ac31e187d50e674fd28ece18d
mosen <mosen>
2007-12-11 11:49:42 +0000

    Added detail to phpdoc tags

--- dotproject3/code/modules/default/controllers/LoginController.php
+++ dotproject3/code/modules/default/controllers/LoginController.php
@@ -2,12 +2,24 @@
 require_once 'DP/Controller/Action.php';
 
 /**
+ * This is the file containing the definition of the login controller
+ * @author ajdonnison
+ * @version 3.0
+ * @package dotproject
+ */
+
+/**
  * The LoginController handles the login page and its acceptance
+ * @package dotproject
+ * @subpackage default
  */
 class LoginController extends DP_Controller_Action
 {
 	/**
-	 * Default is to display the login page.
+	 * Displays the login page.
+	 * 
+	 * Stores the URL requested before the login page was displayed, so that the user can be
+	 * redirected to their originally requested location after they log in.
 	 */
 	public function indexAction()
 	{
@@ -21,8 +33,10 @@ class LoginController extends DP_Controller_Action
 	}
 
 	/**
-	 * On save we need to validate the login and either kick back
-	 * or redirect to the new URL
+	 * Save and process the submitted login details.
+	 * 
+	 * If the user authenticates successfully then redirect to their originally requested URL,
+	 * otherwise display 'failed' message.
 	 */
 	public function saveAction()
 	{
