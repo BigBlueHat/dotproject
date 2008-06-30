c4dfe1999e529346497cdfdea0e0f3444ccb1e5a
ajdonnison <ajdonnison>
2008-06-30 22:50:53 +0000

    Validation support.

---
+++ dotproject3/code/lib/DP/Validator/PasswordConfirm.php
@@ -0,0 +1,29 @@
+<?php
+class DP_Validator_PasswordConfirm extends Zend_Validate_Abstract
+{
+    const NOT_MATCH = 'notMatch';
+
+    protected $_messageTemplates = array(
+        self::NOT_MATCH => 'Password confirmation does not match'
+    );
+
+    public function isValid($value, $context = null)
+    {
+        $value = (string) $value;
+        $this->_setValue($value);
+
+        if (is_array($context)) {
+            if (isset($context['confirmpassword'])
+                && ($value == $context['confirmpassword']))
+            {
+                return true;
+            }
+        } elseif (is_string($context) && ($value == $context)) {
+            return true;
+        }
+
+        $this->_error(self::NOT_MATCH);
+        return false;
+    }
+}
+?>
